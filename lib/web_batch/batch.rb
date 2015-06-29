6584c3095e70f4b64f50b4901933b4842f3bfdae
robcruz <irobcruz@yahoo.com>
2015-06-29 09:54:03 -0700

    Fixed Service Cost

--- lib/web_batch/batch.rb
+++ lib/web_batch/batch.rb
@@ -141,11 +141,11 @@ module Batch
     public
 
     def sign_out
-      10.times {
+      3.times {
         begin
           click username_field, "userNameText" unless sign_out_link.present?
           click sign_out_link, "signOutLink"
-          sign_in_button.wait_until_present
+          sign_in_button.wait_until_present(3)
           break field_present? sign_in_button
         rescue
           #ignore
