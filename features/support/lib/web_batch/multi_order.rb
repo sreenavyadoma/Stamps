41adeb5422deb62338dcec9f007ea6f760414183
robcruz <irobcruz@yahoo.com>
2015-07-10 10:28:12 -0700

    Changes for Print Costs tests

--- features/support/lib/web_batch/multi_order.rb
+++ features/support/lib/web_batch/multi_order.rb
@@ -2,18 +2,13 @@ module Batch
   class MultiOrder < BatchPage
     private
     def order_count_label
-      ps = @browser.ps :css => 'b'
-      p = ps.last
-      present = p.present?
-      p
+      @browser.ps(:css => 'b').last
     end
 
     public
     def order_count
-      raise "" unless browser_helper.field_present? order_count_label
-      text = browser_helper.text order_count_label
-      text = text.gsub(/\d+/).first
-      text
+      raise "Multi-order Count Label does not exist or Multi-order form not opened." unless browser_helper.field_present? order_count_label
+      browser_helper.text(order_count_label).gsub(/\d+/).first
     end
 
   end
