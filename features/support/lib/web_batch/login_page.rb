dde7e04f5b5a727464138161cc20f3eadeb15e05
Eli Ihejirika <eihejirika@stamps.com>
2015-07-14 12:27:51 -0700

    Eli's changes --updated login for hardcoded usernames in cucumber.yml

--- features/support/lib/web_batch/login_page.rb
+++ features/support/lib/web_batch/login_page.rb
@@ -41,8 +41,8 @@ module Batch
         when 0
           # user default sign in credentials
           credentials = batch_helper.rand_login_credentials
-          username = log_param "username", credentials["username"]
-          password = log_param "password", credentials["password"]
+          username = log_param "username", ENV['USR']
+          password = log_param "password", ENV['PW']
         when 1
           if args[0].is_a? Hash
             username = args[0]['username']
