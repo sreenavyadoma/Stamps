6669f137e8a80dce9532550324dadcac85935540
Eli Ihejirika <eihejirika@stamps.com>
2015-07-16 14:59:02 -0700

    Eli's changes -- added code to test all postage purchase combinations

--- features/support/lib/web_batch/buy_postage.rb
+++ features/support/lib/web_batch/buy_postage.rb
@@ -7,29 +7,94 @@ class BuyPostage < BrowserObject
     span
   end
 
-  public
-  def buy_10
+  def confirm_purchase_button
+    span_array = @browser.spans :text => 'Purchase'
+    span = span_array.last
+    present = span.present?
+    text = span.text
+    span
+  end
 
+  def ok_button
+    span_array = @browser.spans :text => 'OK'
+    span = span_array.last
+    present = span.present?
+    text = span.text
+    span
+  end
+
+  def buy_10_button
+    input = @browser.input :id => "sdc-purchasewin-10dradio"
+    present = input.present?
+    input
+  end
+
+  def buy_25_button
+    input = @browser.input :id => "sdc-purchasewin-25dradio"
+    present = input.present?
+    input
+  end
+
+  def buy_50_button
+    input = @browser.input :id => "sdc-purchasewin-50dradio"
+    present = input.present?
+    input
+  end
+
+  def buy_100_button
+    input = @browser.input :id => "sdc-purchasewin-100dradio"
+    present = input.present?
+    input
+  end
+
+  def buy_other_button
+    input = @browser.input :id => "sdc-purchasewin-otherdradio"
+    present = input.present?
+    input
+  end
+
+  def buy_other_textbox
+    input = @browser.text_field :id => "sdc-purchasewin-otheramount"
+    present = input.present?
+    input
+  end
+
+  def purchase_processing
+    div = @browser.div :css => "div[id^=processingpurchasewindow][id$=innerCt]"
+    present = div.present?
+    div
+  end
+
+  public
+
+
+  def buy_10
+    buy_10_button.click
   end
 
   def buy_25
-
+    buy_25_button.click
   end
 
   def buy_50
-
+    buy_50_button.click
   end
 
-  def buy100
-
+  def buy_100
+    buy_100_button.click
   end
 
-  def buy_other value
-
+  def buy_other=(value)
+    buy_other_button.click
+    browser_helper.set_text buy_other_textbox, value, 'Buy'
   end
 
   def purchase
     purchase_button.click
+  end
+
+  def confirm_purchase
+    confirm_purchase_button.click
   end
 
   def edit_payment_method
@@ -38,5 +103,14 @@ class BuyPostage < BrowserObject
 
   def autobuy
 
+  end
+
+  def wait_for_purchase_confirmation
+    purchase_processing.wait_until_present
+    purchase_processing.wait_while_present
+  end
+
+  def complete_purchase
+    ok_button.click
   end
 end
