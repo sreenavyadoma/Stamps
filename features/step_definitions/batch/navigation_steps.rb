ed8103066a21c35e8be58734e02b1b26a47e051d
Eli Ihejirika <eihejirika@stamps.com>
2015-07-16 14:15:11 -0700

    Eli's changes -- added code to buy postage

--- features/step_definitions/batch/navigation_steps.rb
+++ features/step_definitions/batch/navigation_steps.rb
@@ -1,14 +1,50 @@
 
 # batch.navigation_bar.sign_out
-#* Buy $10 postage
-#* Expect $10 is added to customer balance
+#* Buy postage
+#* Expect value is added to customer balance
 
  Then /^Buy \$(\d+) postage$/ do |amount|
+
+   log "Storing old postage amount"
    @old_balance = batch.navigation_bar.balance
-   batch.navigation_bar.buy_more.buy_other amount
+   log "Buy More link selected"
+   batch.navigation_bar.select_buy_more
+
+   log "$#{amount} postage radio button selected"
+
+   case amount
+     when "10"
+       batch.navigation_bar.buy_more.buy_10
+     when "25"
+       batch.navigation_bar.buy_more.buy_25
+     when "50"
+       batch.navigation_bar.buy_more.buy_50
+     when "100"
+       batch.navigation_bar.buy_more.buy_100
+     else
+       batch.navigation_bar.buy_more.buy_other = amount
+   end
+
+   log "Purchase button clicked"
+   batch.navigation_bar.buy_more.purchase
+   log "Purchase confirmed"
+   batch.navigation_bar.buy_more.confirm_purchase
+   log "Waiting for purchase processing to complete"
+   batch.navigation_bar.buy_more.wait_for_purchase_confirmation
+   log "Purchase Complete, clicking OK"
+   batch.navigation_bar.buy_more.complete_purchase
  end
 
 
 Then /^Expect \$(\d+) is added to customer balance$/ do |balance|
-  pending
+  log "Store new balance"
+  @new_balance = batch.navigation_bar.balance
+  log "Compare old and new balance"
+  match = @new_balance - @old_balance
+  match_float = match.to_f
+  balance_float = balance.to_f
+  log "Verify correct postage amount added to balance"
+  match_float.should eql  balance_float
+  log "Postage purchase verified"
 end
+
