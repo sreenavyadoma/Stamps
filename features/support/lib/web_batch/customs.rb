f2af6804bd36b06df52ac5b074049d1b13898d1f
robcruz <irobcruz@yahoo.com>
2015-09-17 11:48:33 -0700

    Refactored Stamps

--- features/support/lib/web_batch/customs.rb
+++ features/support/lib/web_batch/customs.rb
@@ -215,7 +215,8 @@ module Batch
       divs = @browser.divs :css => "div[id^=displayfield]>div[id^=displayfield]>div[id^=displayfield]"
       div = divs[divs.size-2]
       weight_label = Label.new div
-      log "Total Weight: #{weight_label.text}" # 0 lbs. 0 oz.
+      present = browser_helper.present? div
+      log "Total Weight: #{weight_label.text div}" # 0 lbs. 0 oz.
       weight_label
     end
 
