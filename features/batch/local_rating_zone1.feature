fc1a9fa9c26fdb1bfa538ebd5e980c58d6d3c599
robcruz <irobcruz@yahoo.com>
2015-10-13 22:29:32 -0700

    local rating

--- features/batch/local_rating_zone1.feature
+++ features/batch/local_rating_zone1.feature
@@ -10,7 +10,7 @@ Feature: Local Rating Zone 1
   Scenario:
     Then I Add a new order
     Then Verify Local Rating
-      |ship_from|ship_to|service|weight_lbs|weight_oz|length|height|width|tracking|rate0|
+      |ship_from|ship_to|service|weight_lbs|weight_oz|length|height|width|tracking|total|
       |default|Zone1 Test, Priority Mail, 1339 W Willow St, Long Beach CA 90810|Priority Mail Large/Thick Envelope|1|0|1|1|1|Signature Required|7.50|
       |default|Zone1 Test, Priority Mail, 1339 W Willow St, Long Beach CA 90810|Priority Mail Large/Thick Envelope|0|8|2|2|2|USPS Tracking|5.05|
       |default|Zone1 Test, Priority Mail, 1339 W Willow St, Long Beach CA 90810|Priority Mail Large/Thick Envelope|1|0|2|2|2|USPS Tracking|5.05|
--- features/step_definitions/batch/local_rating_steps.rb
+++ features/step_definitions/batch/local_rating_steps.rb
@@ -1,42 +1,44 @@
 Then /^Verify Local Rating$/ do |table|
-  results_file = "local_rating_results.csv"
+  #results_file = "local_rating_results.csv"
   @single_order_form = batch.single_order_form
   parameter_array = table.hashes
   results = Hash.new
 
-  CSV.open(results_file, "wb") do |csv|
-    log "| test_result | ship_from | ship_to | service | weight_lbs | weight_oz | length | height | width | tracking | total |"
-    csv << ["test_number", "ship_from", "ship_to", "service", "weight_lbs", "weight_oz", "length", "height", "width", "tracking", "total"]
-    parameter_array.each_with_index { |element, index|
-      step "Set single-order form Ship-From to #{element["ship_from"]}"
-      step "Set single-order form Ship-To address to #{element["ship_to"]}"
-      step "Set single-order form Service to \"#{element["service"]}\""
-      step "Set single-order form Ounces to #{element["weight_oz"]}"
-      step "Set single-order form Pounds to #{element["weight_lbs"]}"
-      step "Set single-order form Length to #{element["length"]}"
-      step "Set single-order form Height to #{element["height"]}"
-      step "Set single-order form Width to #{element["width"]}"
-      step "Set single-order form Tracking to #{element["tracking"]}"
+  #CSV.open(results_file, "wb") do |csv|
+  #end
 
-      10.times { |counter|
-        @single_order_form.click_form
-        total = @single_order_form.total
-        if total.eql? element["total"]
-          results[index] = total.eql? element["total"]
-          break
-        else
-          results[index] = total.eql? element["total"]
-        end
-      }
+
+  #log "| test_result | ship_from | ship_to | service | weight_lbs | weight_oz | length | height | width | tracking | total |"
+  #csv << ["test_number", "ship_from", "ship_to", "service", "weight_lbs", "weight_oz", "length", "height", "width", "tracking", "total"]
+  parameter_array.each_with_index { |element, index|
+    step "Set single-order form Ship-From to #{element["ship_from"]}"
+    step "Set single-order form Ship-To address to #{element["ship_to"]}"
+    step "Set single-order form Service to \"#{element["service"]}\""
+    step "Set single-order form Ounces to #{element["weight_oz"]}"
+    step "Set single-order form Pounds to #{element["weight_lbs"]}"
+    step "Set single-order form Length to #{element["length"]}"
+    step "Set single-order form Height to #{element["height"]}"
+    step "Set single-order form Width to #{element["width"]}"
+    step "Set single-order form Tracking to #{element["tracking"]}"
+
+    10.times { |counter|
+      @single_order_form.click_form
       total = @single_order_form.total
-
-      log "| #{index} | #{(results[index])?"Passed":"Failed"} |Expectation=#{element["total"]},Actual=#{total}| | ##{index} | |#{element["ship_from"]} | #{element["ship_to"]} | #{element["service"]} | #{element["weight_lbs"]} | #{element["weight_oz"]} | #{element["length"]} | #{element["height"]} | #{element["width"]} | #{element["tracking"]} | #{element["total"]} |"
-
-      csv << [index, (results[index])?"Passed":"Failed", "Expectation=#{element["total"]},Actual=#{total}", element["ship_from"], element["ship_to"], element["service"], element["weight_lbs"], element["weight_oz"], element["length"], element["height"], element["width"], element["tracking"], element["total"]]
-
-      step "Expect single-order form Total to be $#{element["total"]}"
+      if total.eql? element["total"]
+        results[index] = total.eql? element["total"]
+        break
+      else
+        results[index] = total.eql? element["total"]
+      end
     }
-  end
+    total = @single_order_form.total
+
+    log "| #{index} | #{(results[index])?"Passed":"Failed"} |Expectation=#{element["total"]},Actual=#{total}| | ##{index} | |#{element["ship_from"]} | #{element["ship_to"]} | #{element["service"]} | #{element["weight_lbs"]} | #{element["weight_oz"]} | #{element["length"]} | #{element["height"]} | #{element["width"]} | #{element["tracking"]} | #{element["total"]} |"
+
+    #csv << [index, (results[index])?"Passed":"Failed", "Expectation=#{element["total"]},Actual=#{total}", element["ship_from"], element["ship_to"], element["service"], element["weight_lbs"], element["weight_oz"], element["length"], element["height"], element["width"], element["tracking"], element["total"]]
+
+    step "Expect single-order form Total to be $#{element["total"]}"
+  }
 
 end
 
