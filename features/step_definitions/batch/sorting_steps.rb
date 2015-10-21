20ebc2537c410af99ffee077b752756fef2b371f
robcruz <irobcruz@yahoo.com>
2015-10-21 09:52:21 -0700

    Sorting algorithm for Elie.

--- features/step_definitions/batch/sorting_steps.rb
+++ features/step_definitions/batch/sorting_steps.rb
@@ -1,7 +1,7 @@
 Then /^Sort Order Grid (.*) in (.*) order$/ do |column, sort_order|
   case column.downcase
     when "order id"
-      batch.filter_panel.awaiting_shipment.grid.order_id.sort_order(sort_order).should be true
+      batch.filter.awaiting_shipment.grid.column.order_id.sort_order(sort_order).should be true
     else
 
   end
