bdcc6be140cc15ce6881bee7d4e40b4795837875
robcruz <irobcruz@yahoo.com>
2015-10-21 09:59:18 -0700

    Sorting algorithm for Elie.

--- features/step_definitions/batch/sorting_steps.rb
+++ features/step_definitions/batch/sorting_steps.rb
@@ -3,7 +3,6 @@ Then /^Sort Order Grid (.*) in (.*) order$/ do |column, sort_order|
     when "order id"
       batch.filter.awaiting_shipment.grid.column.order_id.sort_order(sort_order).should be true
     else
-
+      raise "The error lay between the chair and the keyboard... know how to use the API pls."
   end
-
 end
