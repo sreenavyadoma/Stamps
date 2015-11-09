c104c0b6244b0f289e4a09c0bced818b1c1aec4e
robcruz <irobcruz@yahoo.com>
2015-11-09 10:48:43 -0800

    abstracted Stamps::Test

--- features/step_definitions/wonder/wonder_steps.rb
+++ features/step_definitions/wonder/wonder_steps.rb
@@ -1,5 +1,5 @@
 Then /^Sign-in to (\w+) webpostage as (.*)\/(.*) using (\w+)$/ do |environment, username, password, browser|
-  @browser = Stamps.setup browser
+  @browser = Stamps::Test.setup browser
   print_postage.sign_in_page.visit(environment).sign_in username, password
 end
 
