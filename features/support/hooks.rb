da0fc2aac130824d7c09be3c942e636e109faed1
robcruz <irobcruz@yahoo.com>
2015-11-09 21:05:55 -0800

    Changees to Ship-To 7

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -8,11 +8,11 @@ include RSpec::Matchers
 include DataMagic
 
 Before do  |scenario|
-  log "Begin Test:  #{scenario.name}"
+  log "Begin Test Scenario:  --------------------------------------------------------  #{scenario.name}"
 end
 
 After do |scenario|
-  log "End Test:  #{scenario.name}"
+  log "End Test Scenario:  --------------------------------------------------------  #{scenario.name}"
   Stamps::Test.teardown
   $start = false
 end
