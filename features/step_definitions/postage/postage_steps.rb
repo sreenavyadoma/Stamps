59108e4838ad751a9bed38a4afe79f4cc0bc3cbf
robcruz <irobcruz@yahoo.com>
2015-11-16 13:14:05 -0800

    PrintPostage changes

--- features/step_definitions/postage/postage_steps.rb
+++ features/step_definitions/postage/postage_steps.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 Then /^Create page objects for Print On Shipping Label$/ do
   print_postage.print_on.select "Shipping Label - 8 ½” x 11” Paper"
   log print_postage.print_on.tooltip "Shipping Label - 8 ½” x 11” Paper"
