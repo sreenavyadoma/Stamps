865057fdd6644ec7ca339e10f38525a92c42fd27
Eli Ihejirika <eihejirika@stamps.com>
2015-11-17 16:14:26 -0800

    Print Postage Print on - 4

--- features/support/lib/postage/print_postage.rb
+++ features/support/lib/postage/print_postage.rb
@@ -1,25 +1,40 @@
+# encoding: utf-8
 module Postage
 
   class EmailTracking < PostageObject
-    def email_tracking
+    def checkbox
+      checkbox_field = @browser.input :id => "sdc-mainpanel-emailcheckbox-inputEl"
+      verify_field = checkbox_field.parent.parent.parent.parent
+      attribute = "class"
+      verify_field_attrib = "checked"
+      Stamps::Browser::Checkbox.new checkbox_field, verify_field, attribute, verify_field_attrib
+    end
+    def text_box
+      Textbox.new @browser.text_field :name => "recipientEmail"
+    end
 
+    def tooltip
+      Label.new(@browser.label :id => "sdc-mainpanel-emailcheckbox-boxLabelEl").attribute_value "data-qtip"
+    end
+
+    def data_error_tooltip
+      tooltip = browser.div :css => "div[id=sdc-mainpanel-emailtextfield-errorEl]"
     end
   end
 
   class ShipToBase < PostageObject
     def country selection
-
+      PostageShipToDomestic.new @browser
     end
   end
 
   class PostageShipToDomestic < ShipToBase
-    def delivery_address address
-      textbox = Textbox.new "textbox object goes here"
-      textbox.set address
+    def delivery_address
+      Textbox.new @browser.text_field :name => "sdc-mainpanel-shiptotextarea-inputEl"
     end
 
     def data_error_tooltip
-
+      tooltip = browser.div :css => "div[id=sdc-mainpanel-shiptotextarea-errorEl]"
     end
   end
 
@@ -45,13 +60,29 @@ module Postage
     end
   end
 
-  class ShippingLabel < PostageObject
-    def email_tracking
-      EmailTracking.new @browser
+  class Country < PostageObject
+    def drop_down
+      Button.new (@browser.divs :css => "div[class*=x-form-trigger]")[2]
     end
 
-    def ship_to
-      PostageShipTo.new @browser
+    def text_box
+      Textbox.new (@browser.text_field :name => "mailToCountry")
+    end
+
+    def select selection
+      box = text_box
+      button = drop_down
+      selection_label = Label.new @browser.div :text => selection
+      5.times {
+        begin
+          button.safe_click unless selection_label.present?
+          selection_label.scroll_into_view
+          selection_label.safe_click
+          break if box.text.include? selection
+        rescue
+          #ignore
+        end
+      }
     end
   end
 
@@ -80,38 +111,103 @@ module Postage
         end
       }
 
-      if selection.downcase.includes? "shipping label"
-        return ShippingLabel.new @browser
-      else
-        raise "Print On #{selection} not yet supported."
-      end
+    end
 
-      def tooltip selection
-        drop_down = Button.new (@browser.divs :css => "div[class*=x-form-trigger]")[0]
-        text_box = Textbox.new (@browser.text_field :name => "media")
-        selection_field = Label.new @browser.div :text => selection
+    def tooltip selection
+      drop_down = Button.new (@browser.divs :css => "div[class*=x-form-trigger]")[0]
+      text_box = Textbox.new (@browser.text_field :name => "media")
+      selection_field = Label.new @browser.div :text => selection
 
-        10.times {
-          drop_down.safe_click unless selection_field.present?
-          return selection_field.attribute_value "data-qtip" if selection_field.present?
-        }
+      10.times {
+        drop_down.safe_click unless selection_field.present?
+        return selection_field.attribute_value "data-qtip" if selection_field.present?
+      }
 
-      end
     end
   end
 
-  class PrintPostage < PostageObject
+  class PostageBase < PostageObject
+
+    # all common fields goes here including service drop down
+
+    def print_on selection
+      PrintOn.new(@browser).select selection
+    end
+
+  end
+
+  class ShippingLabel < PostageBase
+    def email_tracking
+      EmailTracking.new @browser
+    end
+
+    def ship_to
+      PostageShipTo.new @browser
+    end
+  end
+
+  class NetStamps < PostageBase
+
+  end
+
+  class Envelope < PostageBase
+
+  end
+
+  class CertifiedMail < PostageBase
+
+  end
+
+  class Roll < PostageBase
+
+  end
+
+  class PrintPostage < PostageBase
 
     def sign_in_page
-      @sign_in ||= Postage::SignInPage.new @browser
+      @sign_in ||= PostageBase::SignInPage.new @browser
     end
 
     def navigation
-      @navigation ||= Postage::Navigation.new @browser
+      @navigation ||= PostageBase::Navigation.new @browser
     end
 
-    def print_on
-      PrintOn.new @browser
+    def toolbar
+      #we'll get to this when it comes time to buy stamps and prefs
+    end
+
+    def print_on selection
+      PrintOn.new(@browser).select selection
+
+      if selection.downcase.include? "shipping label"
+        shipping_label
+      elsif selection.downcase.include? "stamps"
+        stamps
+      elsif selection.downcase.include? "envelope"
+        envelope
+      else
+        raise "Print On #{selection} not yet supported."
+      end
+    end
+
+    def shipping_label
+      ShippingLabel.new @browser
+    end
+
+    def stamps
+      NetStamps.new @browser
+    end
+
+    def envelope
+      Envelope.new @browser
+    end
+
+    def certified_mail
+      CertifiedMail.new @browser
+    end
+
+    def roll
+      Roll.new @browser
     end
 
   end
