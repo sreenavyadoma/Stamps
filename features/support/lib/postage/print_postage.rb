032c8904e49039f48f959a92c7d006848e2c222e
robcruz <irobcruz@yahoo.com>
2015-11-20 14:46:12 -0800

    Ship Date tests

--- features/support/lib/postage/print_postage.rb
+++ features/support/lib/postage/print_postage.rb
@@ -4,11 +4,7 @@ module Postage
 
   ######Class for Print Postage page, Incl. toolbars and navigation. Instantiates postage form objects for Print On selections
 
-  class PrintPostage
-
-    def initialize browser
-      @browser = browser
-    end
+  class PrintPostage < PostageObject
 
     def sign_in_page
       @sign_in ||= PostageBase::SignInPage.new @browser
