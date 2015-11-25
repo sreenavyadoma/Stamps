1ee96656961c112247ffa72ed632895d9a84b30a
Eli Ihejirika <eihejirika@stamps.com>
2015-11-25 15:26:01 -0800

    testing buy_more browser move

--- features/support/lib/batch/navigation.rb
+++ features/support/lib/batch/navigation.rb
@@ -42,8 +42,7 @@ module Batch
 
     def select_buy_more
       @browser.window.move_to 0, 0
-      @browser.window.resize_to 1500, 850
-      @browser.window.move_to 800, 500
+
 
       balance_label.focus
       browser_helper.click balance_label, "Balance"
