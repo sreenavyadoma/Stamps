83e357dc3dd399112cf2255cff33b25ea9d14829
robcruz <irobcruz@yahoo.com>
2016-01-21 15:53:08 -0800

    Fixed signout

--- features/support/lib/orders/navigation_bar.rb
+++ features/support/lib/orders/navigation_bar.rb
@@ -67,7 +67,7 @@ module Orders
           drop_down = username
           sign_out_link = Label.new (@browser.a :text => "Sign Out")
           20.times do
-            drop_down.safe_click if sign_out_link.present?
+            drop_down.safe_click unless sign_out_link.present?
             drop_down.hover
             sign_out_link.safe_click if sign_out_link.present?
             return if @browser.url.include? "SignIn"
