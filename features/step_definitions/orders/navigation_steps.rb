5d0f6c68a8afde9f01d6f7c08033e1d229d350bc
robcruz <irobcruz@yahoo.com>
2016-01-26 14:41:51 -0800

    Changes to General Settings Tests

--- features/step_definitions/orders/navigation_steps.rb
+++ features/step_definitions/orders/navigation_steps.rb
@@ -1,4 +1,5 @@
- Then /^Purchase Postage for \$(\d+)$/ do |amount|
+
+Then /^Purchase Postage for \$(\d+)$/ do |amount|
    @old_balance = orders.navigation_bar.balance.amount
    log.info "Buy More link selected"
    buy_postage_modal = orders.navigation_bar.balance.buy_more
@@ -19,16 +20,18 @@
  end
 
 
-Then /^Expect \$(\d+) is added to customer balance$/ do |balance|
-  log.info "Step: Expect \$#{balance} is added to customer balance"
-  log.info "Store new balance"
-  sleep 4
-  @new_balance = orders.navigation_bar.balance.amount
-  log.info "Compare old and new balance"
-  match = @new_balance.to_f - @old_balance.to_f
-  balance_float = balance.to_f
-  log.info "Verify correct postage amount added to balance"
-  match.should eql  balance_float
-  log.info "Postage purchase verified"
+Then /^Expect \$(\d+) is added to customer balance$/ do |purchase_amount|
+  log.info "Step: Expect \$#{purchase_amount} is added to customer balance"
+  20.times do
+    sleep 1
+    new_balance = orders.navigation_bar.balance.amount
+    actual_purchased_amount = new_balance.to_f - @old_balance.to_f
+    break if actual_purchased_amount ==  purchase_amount.to_f
+  end
+  new_balance = orders.navigation_bar.balance.amount
+  actual_purchased_amount = new_balance.to_f - @old_balance.to_f
+
+  log.info "Test #{(actual_purchased_amount ==  purchase_amount.to_f)?"Passed":"Failed"}"
+  actual_purchased_amount.should eql  purchase_amount.to_f
 end
 
