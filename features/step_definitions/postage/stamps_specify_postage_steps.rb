8f2731b835ab6f01372f246cccb220a6a07f70e3
Eli Ihejirika <eihejirika@stamps.com>
2016-02-10 13:20:04 -0800

    Updated step definitions

--- features/step_definitions/postage/stamps_specify_postage_steps.rb
+++ features/step_definitions/postage/stamps_specify_postage_steps.rb
@@ -2,13 +2,19 @@
 
 Then /^Stamps: Select Specify Postage Amount/ do
   log.info "Step:  Select Stamps Specify Postage Amount"
-  @postage_form = postage.stamps if @postage_form.nil?
-  @postage_form.specify_postage_amount
+  @stamps = postage.stamps if @postage_form.nil?
+  @stamps.specify_postage_amount
 end
 
 Then /^Stamps: Set Stamp Amount to \$(.*)$/ do |amount|
   log.info "Step:  Set Stamps Stamp Amount to $#{amount}"
-  @postage_form = postage.stamps if @postage_form.nil?
-  @postage_form.specify_postage_amount.stamp_amount.set amount
+  @stamps = postage.stamps if @postage_form.nil?
+  @stamps.specify_postage_amount.stamp_amount.set amount
 end
+
+Then /^Stamps: Set Specify Postage Service to \"(.*)\"/ do |service|
+  log.info "Step: Envelopes: Set Print Postage Service to: \n #{service}"
+  @stamps.specify_postage_service.select service
+end
+
 
