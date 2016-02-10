310341ba8d272d125f030d688d2975da6b6209b7
Eli Ihejirika <eihejirika@stamps.com>
2016-02-10 13:20:37 -0800

    New class for Contacts Modal

---
+++ features/support/lib/print/contacts_modal.rb
@@ -0,0 +1,77 @@
+
+
+module Print
+  module Postage
+    class ContactsModal < Print::Postage::PrintObject
+      def close
+        browser_helper.safe_click (@browser.img :css => "img[class*='x-tool-img x-tool-close']")
+      end
+
+      def cancel
+
+        #button = Button.new @browser.span :text => "Cancel"
+        #@browser.frame(:index => 1).button.click
+
+        frame1 = @browser.iframe(:css => 'iframe[class*=sdc-addressbookiframe]')
+        frame1.a(:text => 'View Contacts').click
+
+        sleep 3
+      end
+
+      def wait_until_present
+        window_title.wait_until_present
+      end
+
+      def present?
+        window_title.present?
+      end
+
+      def select_contact
+
+      end
+
+      def window_title
+        Label.new (@browser.span :text => "Search Contacts")
+      end
+
+      def search
+        Search.new @browser
+      end
+
+      def search_by_group
+        SearchByGroup.new @browser
+      end
+
+      def search_by_cost_code
+        SearchByCostCode.new @browser
+      end
+
+      def view_contacts_page
+
+      end
+
+      def view_by
+        ViewBy.new @browser
+      end
+
+    end
+
+    class Search < Print::Postage::PrintObject
+
+    end
+
+    class SearchByGroup < Print::Postage::PrintObject
+
+    end
+
+    class SearchByCostCode < Print::Postage::PrintObject
+
+    end
+
+    class ViewBy < Print::Postage::PrintObject
+
+    end
+
+
+  end
+end
