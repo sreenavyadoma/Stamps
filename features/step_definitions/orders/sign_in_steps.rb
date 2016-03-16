0d105dc02d993d97ac41116cd2e312009ee48b09
robcruz <irobcruz@yahoo.com>
2016-03-16 12:13:24 -0700

    Included health check info before all tests

--- features/step_definitions/orders/sign_in_steps.rb
+++ features/step_definitions/orders/sign_in_steps.rb
@@ -23,6 +23,7 @@ end
 Given /^I sign-in to Orders as (.*)\/(.*)/ do |username, password|
   log.info "Step: I am signed in to Orders as #{username}/#{password}"
   step "I launch browser default"
+  step "Get Healthcheck Info"
   step "Orders: Load Sign-in page"
   orders.sign_in_page.sign_in_with_credentials username, password
 end
@@ -30,6 +31,7 @@ end
 Given /^I am signed in to Orders$/ do
   log.info "Step: I am signed in to Orders"
   step "I launch browser default"
+  step "Get Healthcheck Info"
   step "Orders: Load Sign-in page"
   orders.sign_in_page.sign_in_with_credentials
 end
@@ -37,6 +39,7 @@ end
 Given /^I am signed in to Orders as (.*)\/(.*)/ do |username, password|
   log.info "Step: I am signed in to Orders as #{username}/#{password}"
   step "I launch browser default"
+  step "Get Healthcheck Info"
   step "Orders: Load Sign-in page"
   orders.sign_in_page.sign_in_with_credentials username, password
 end
@@ -44,6 +47,7 @@ end
 Given /^I am signed in to Orders using (.*)/ do |browser|
   log.info "Step: I am signed in to Orders as #{browser}"
   step "I launch browser #{browser}"
+  step "Get Healthcheck Info"
   step "Orders: Load Sign-in page"
   orders.sign_in_page.sign_in_with_credentials
 end
@@ -51,6 +55,7 @@ end
 Given /^I am signed in to Orders as (.*)\/(.*)\/(.*)/ do |browser, username, password|
   log.info "Step: I am signed in to Orders as #{browser}/#{username}/#{password}"
   step "I launch browser #{browser}"
+  step "Get Healthcheck Info"
   step "Orders: Load Sign-in page"
   orders.sign_in_page.sign_in_with_credentials username, password
 end
@@ -58,6 +63,7 @@ end
 Given /^I am signed in to Orders as (.*)\/(.*)\/(.*)\/(.*)/ do |browser, url, username, password|
   log.info "Step: I am signed in to Orders as #{browser}/#{url}/#{username}/#{password}"
   step "I launch browser #{browser}"
+  step "Get Healthcheck Info"
   step "Orders: Load Sign-in page #{url}"
   orders.sign_in_page.sign_in_with_credentials username, password
 end
