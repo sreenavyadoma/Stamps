981a99cb8394fcb149cbbe637e18ac03e6c6372e
robcruz <irobcruz@yahoo.com>
2016-03-24 15:18:56 -0700

    Then Navigation Bar:  Customer Balance

---
+++ features/step_definitions/orders/navigation_bar_steps.rb
@@ -0,0 +1,7 @@
+Then /^Navigation Bar:  Customer Balance$/ do
+  log.info "Navigation Bar:  Customer Balance"
+  log.message " ############## CUSTOMER BALANCE "
+  log.message " ############## #{orders.navigation_bar.balance.amount}"
+  log.message " ############## #{orders.navigation_bar.balance.amount}"
+  log.message " ############## CUSTOMER BALANCE "
+end
