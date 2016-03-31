3e056ebd2022a1238cbe91b22ad1c3535cc8bdd4
robcruz <irobcruz@yahoo.com>
2016-03-31 12:19:19 -0700

    updated healthcheck

--- features/support/lib/healthcheck/health.rb
+++ features/support/lib/healthcheck/health.rb
@@ -13,30 +13,36 @@ module Stamps
         when /qasc/
           @browser.goto "https://printext.qasc.stamps.com/orders/healthcheck.aspx"
           log.message "HealthCheck: QASC"
-          log.message "#{@browser.pre.text}"
+          sleep 1
+          log.message "#{@browser.text}"
           @browser.goto "https://printext.qasc.stamps.com/webpostage/healthcheck.aspx"
           log.message "HealthCheck: QASC"
-          log.message "#{@browser.pre.text}"
+          sleep 1
+          log.message "#{@browser.text}"
         when /qacc/
           @browser.goto "https://printext.qacc.stamps.com/orders/healthcheck.aspx"
           log.message "HealthCheck: QACC Healthcheck:"
-          log.message "#{@browser.pre.text}"
+          sleep 1
+          log.message "#{@browser.text}"
           @browser.goto "https://printext.qacc.stamps.com/webpostage/healthcheck.aspx"
           log.message "HealthCheck: QACC Healthcheck:"
-          log.message "#{@browser.pre.text}"
+          sleep 1
+          log.message "#{@browser.text}"
         when /staging/
           @browser.goto "https://print.testing.stamps.com/orders/healthcheck.aspx"
           log.message "HealthCheck: Staging Healthcheck:"
-          log.message "#{@browser.pre.text}"
+          sleep 1
+          log.message "#{@browser.text}"
           @browser.goto "https://print.testing.stamps.com/webpostage/healthcheck.aspx"
           log.message "HealthCheck: Staging Healthcheck:"
-          log.message "#{@browser.pre.text}"
+          sleep 1
+          log.message "#{@browser.text}"
         else
           log.info "Invalid environment type!"
           log.info "Teardown: Begin tearing down test"
           Stamps::Test.teardown
           log.info "Teardown: Done!"
-          stop_test "Raise an exception"
+          stop_test "#{ENV['URL']} is not a valid URL selection"
       end
       log.message "HealthCheck:"
       log.message "HealthCheck: --------------------------- End"
