ca141cf2e9b0b25b8a1d37533428d35e347d8dc1
robcruz <irobcruz@yahoo.com>
2016-04-18 10:55:03 -0700

    Refactored postage

--- features/step_definitions/print/stamps_specify_postage_steps.rb
+++ features/step_definitions/print/stamps_specify_postage_steps.rb
@@ -2,13 +2,11 @@
 
 Then /^Postage Stamps: Select Specify Postage Amount/ do
   log.info "Step:  Select Stamps Specify Postage Amount"
-  @print_postage = postage.print_postage if @postage_form.nil?
   @print_postage.specify_postage_amount
 end
 
 Then /^Postage Stamps: Set Stamp Amount to \$(.*)$/ do |amount|
   log.info "Step:  Set Stamps Stamp Amount to $#{amount}"
-  @print_postage = postage.print_postage if @postage_form.nil?
   @print_postage.specify_postage_amount.stamp_amount.set amount
 end
 
