1d95ace385012a1706dfa8955d898c987dd0daeb
Eli Ihejirika <eihejirika@stamps.com>
2016-04-22 13:24:50 -0700

    added steps and page objects for print sample

--- features/step_definitions/print/print_postage_modal_steps.rb
+++ features/step_definitions/print/print_postage_modal_steps.rb
@@ -9,6 +9,11 @@ When /^Postage: Open Print Modal$/ do
   @print_window = postage.footer.print
 end
 
+When /^Postage: Open Print Sample Modal$/ do
+  log.info "Postage: Open Print Sample Modal"
+  @print_window = postage.footer.print_sample
+end
+
 Then /^Postage Print Modal: Select Printer \"(.*)\"$/ do |printer|
   log.info "Step: Select Printer #{printer}"
   @print_window.printer.select printer
@@ -17,6 +22,14 @@ end
 When /^Postage Print Modal: Print$/ do
   log.info "Step: Print"
   print_postage_modal = postage.footer.print
+  @printer = print_postage_modal.printer.text_box.text
+  @paper_tray = print_postage_modal.paper_tray.text_box.text
+  @printing_error = print_postage_modal.print
+end
+
+When /^Postage Print Modal: Print Sample$/ do
+  log.info "Step: Print Sample"
+  print_postage_modal = postage.footer.print_sample
   @printer = print_postage_modal.printer.text_box.text
   @paper_tray = print_postage_modal.paper_tray.text_box.text
   @printing_error = print_postage_modal.print
