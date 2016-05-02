a86cd9204f78209331dfd968d915394e70b73d01
Eli Ihejirika <eihejirika@stamps.com>
2016-05-02 13:03:15 -0700

    fixed typo in reference number

--- features/support/lib/print/print_roll.rb
+++ features/support/lib/print/print_roll.rb
@@ -1,6 +1,38 @@
 # encoding: utf-8
 module Print
   module Postage
+
+    class RollFormView < Print::Postage::PrintPostageObject
+
+      def preview_image
+        image = StampsLabel.new @browser.img :css => "dimg[src*='Labelsample.gif']"
+      end
+
+      def hide_postage_value
+        checkbox_field = @browser.input :css => "input[id=hidePostageCheckbox]"
+        verify_fields = @browser.inputs :css => "table[id^=checkboxfield][class*=x-form-type-checkbox]"
+        verify_field = verify_fields[5]
+
+        Stamps::Browser::StampsCheckbox.new checkbox_field, verify_field, "class", "checked"
+      end
+
+      def print_reference_number
+        checkbox_field = @browser.input :css => "input[id=printreferencecheckbox]"
+        verify_fields = @browser.inputs :css => "table[id^=checkboxfield][class*=x-form-type-checkbox]"
+        verify_field = verify_fields[6]
+
+        Stamps::Browser::StampsCheckbox.new checkbox_field, verify_field, "class", "checked"
+      end
+
+      def reference_number
+        StampsTextbox.new @browser.text_field :name => "referenceNumber"
+      end
+
+      def cost_code
+        Print::Postage::CostCode.new @browser
+      end
+    end
+
     class Roll < Print::Postage::PrintPostageObject
 
       def insure_for
@@ -9,6 +41,10 @@ module Print
 
       def ship_date
         Print::Postage::ShipDate.new @browser
+      end
+
+      def form_view
+        Print::Postage::RollFormView.new @browser
       end
     end
   end
