9761e05098e63e8cc6a899deb3360c0f81becede
robcruz <irobcruz@yahoo.com>
2016-05-16 15:42:37 -0700

    Removed Test connection from Rakuten Store

--- features/step_definitions/common/common_steps.rb
+++ features/step_definitions/common/common_steps.rb
@@ -24,27 +24,3 @@ end
 Then /^Pause for (\d+) seconds?$/ do |seconds|
   step "Wait in seconds #{seconds}"
 end
-
-=begin
-Then /^BROWSER:  CTRL-O$/ do
-  @browser.send_keys [:control, 'o']
-  @browser.send_keys [:control, 'o']
-  open_file = Windows::OpenFile.new
-  log.info open_file.present?
-  log.info open_file.present?
-  log.info open_file.present?
-  open_file.file_name "C:\Github\Stamps\config\data\import.csv"
-
-  open_file.file_name "C:\Github\Stamps\config\data\import.csv"
-
-end
-
-Then /^BROWSER:  Zoom Out$/ do
-  @browser.send_keys [:control, '-']
-end
-
-Then /^BROWSER:  Zoom In$/ do
-  @browser.send_keys [:control, '+']
-end
-=end
-
