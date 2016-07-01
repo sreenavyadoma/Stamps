fadc33929b87abd3f9c70261811c7c291dfeea64
robcruz <irobcruz@yahoo.com>
2016-07-01 13:18:11 -0700

    changes to message

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -11,24 +11,23 @@ include RAutomation
 
 Before do  |scenario|
   Stamps.init scenario.name
-  logger.info "Running Tests..."
-
-  logger.info "---------------- Feature: #{scenario.feature}"
-  logger.info "---------------- Scenario: #{scenario.name}"
-  logger.info "---------------- Tags:"
-  scenario.tags.each_with_index {|tag, index| logger.info "---------------- Tag #{index+1}: #{tag.name}" }
-  logger.info "---------------- Steps:"
-  scenario.test_steps.each_with_index { |test_step, index| logger.info "---------------- Step #{index}: #{test_step.source.last.name}" if index>0 }
+  logger.message "Running Tests..."
+  logger.message "---------------- Feature: #{scenario.feature}"
+  logger.message "---------------- Scenario: #{scenario.name}"
+  logger.message "---------------- Tags:"
+  scenario.tags.each_with_index {|tag, index| logger.message "---------------- Tag #{index+1}: #{tag.name}" }
+  logger.message "---------------- Steps:"
+  scenario.test_steps.each_with_index { |test_step, index| logger.message "---------------- Step #{index}: #{test_step.source.last.name}" if index>0 }
 end
 
 After do |scenario|
-  logger.info "Teardown Tests..."
-  logger.info "---------------- Feature: #{scenario.feature}"
-  logger.info "---------------- Scenario: #{scenario.name}"
-  logger.info "---------------- Tags:"
-  scenario.tags.each_with_index {|tag, index| logger.info "---------------- Tag #{index+1}: #{tag.name}" }
-  logger.info "---------------- Steps:"
-  scenario.test_steps.each_with_index { |test_step, index| logger.info "---------------- Step #{index}: #{test_step.source.last.name}" if index>0 }
+  logger.message "Teardown Tests..."
+  logger.message "---------------- Feature: #{scenario.feature}"
+  logger.message "---------------- Scenario: #{scenario.name}"
+  logger.message "---------------- Tags:"
+  scenario.tags.each_with_index {|tag, index| logger.message "---------------- Tag #{index+1}: #{tag.name}" }
+  logger.message "---------------- Steps:"
+  scenario.test_steps.each_with_index { |test_step, index| logger.message "---------------- Step #{index}: #{test_step.source.last.name}" if index>0 }
 
   Stamps.teardown
 
