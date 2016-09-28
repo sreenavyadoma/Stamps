13562f5085b75859c3a7d7896af310e1561261b5
robcruz <irobcruz@yahoo.com>
2016-09-28 08:07:13 -0700

    [PRINTAUTO-285] Update User Prints 1 FPO CN22 label on 8.5x11 - right side - with new workflow

--- features/support/lib/orders/sign_in.rb
+++ features/support/lib/orders/sign_in.rb
@@ -188,7 +188,7 @@ module Stamps
               break
             end
 
-            toolbar.wait_until_present 180
+            toolbar.wait_until_present 240
 
             logger.info "#{username} is #{(navbar.present?)?"signed-in!":"not signed-in."}"
 
