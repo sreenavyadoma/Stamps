64f4a6523ed0814bee406df693322748efdfee1a
robcruz <irobcruz@yahoo.com>
2016-10-28 07:36:20 -0700

    [PRINTAUTO-517] Investigate and fix Jenkins Server problems mail_authentication

--- features/step_definitions/healthcheck/healthcheck_steps.rb
+++ features/step_definitions/healthcheck/healthcheck_steps.rb
@@ -1,19 +1,19 @@
 Then /^Health Check: Print - Web Batch$/ do
-  #logger.step "Health Check: Print - Web Batch"
+  logger.step "Health Check: Print - Web Batch"
   health.health_check
 end
 
 Then /^Health Check: Print - Address Book$/ do
-  #logger.step "Health Check: Print - Address Book"
+  logger.step "Health Check: Print - Address Book"
   health.address_book
 end
 
 Then /^Health Check: OR - Reports$/ do
-  #logger.step "Health Check: OR - Reports"
+  logger.step "Health Check: OR - Reports"
   health.or_reports
 end
 
 Then /^Health Check: OR - Postage Tools$/ do
-  #logger.step "Health Check: OR - Postage Tools"
+  logger.step "Health Check: OR - Postage Tools"
   health.or_postage_tools
 end
