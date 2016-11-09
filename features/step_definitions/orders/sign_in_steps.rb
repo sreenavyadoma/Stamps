2b3e5d0aa8172d67999ccc1a7a181fbe7212c60f
robcruz <irobcruz@yahoo.com>
2016-11-09 13:24:48 -0800

    [PRINTAUTO-517] Investigate and fix Jenkins Server problems webreg_orders

--- features/step_definitions/orders/sign_in_steps.rb
+++ features/step_definitions/orders/sign_in_steps.rb
@@ -80,6 +80,8 @@ end
 
 Then /^Orders: Sign in using Jenkins web reg credentials$/ do
   logger.step "Orders: Sign in"
+  ENV['WEB_REG_USR'].should be_truthy
+  ENV['WEB_REG_PW'].should be_truthy
   web_apps.orders.landing_page.sign_in ENV['WEB_REG_USR'], ENV['WEB_REG_PW']
 end
 
