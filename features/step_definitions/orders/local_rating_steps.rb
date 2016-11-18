6aaf88d9ad302ab9e6b7792d412dc19a956fdfa8
robcruz <irobcruz@yahoo.com>
2016-11-18 07:07:01 -0800

    ORDERSAUTO-1238 Create Update ShipStation Orders Ship-From Tests Refactored web_apps/stamps

--- features/step_definitions/orders/local_rating_steps.rb
+++ features/step_definitions/orders/local_rating_steps.rb
@@ -1,7 +1,7 @@
 Then /^Verify Local Rating$/ do |table|
   logger.step "Verify Local Rating..."
   #results_file = "local_rating_results.csv"
-  @order_details = stamps.orders.details
+  order_details_form = stamps.orders.details
   parameter_array = table.hashes
   results = Hash.new
 
@@ -20,8 +20,8 @@ Then /^Verify Local Rating$/ do |table|
     step "Details: Set Tracking to \"#{element["tracking"]}\""
 
     10.times do
-      @order_details.blur_out
-      total = @order_details.footer.total_ship_cost
+      order_details_form.blur_out
+      total = order_details_form.footer.total_ship_cost
       if total.eql? element["total"]
         results[index] = total.eql? element["total"]
         break
@@ -29,18 +29,18 @@ Then /^Verify Local Rating$/ do |table|
         results[index] = total.eql? element["total"]
       end
     end
-    total = @order_details.footer.total_ship_cost
+    total = order_details_form.footer.total_ship_cost
 
     expected_total_amount = element["total"]
 
     5.times do
       stamps.orders.details.blur_out
       sleep 1
-      actual = stamps.orders.details.footer.total_ship_cost
+      total_ship_cost = stamps.orders.details.footer.total_ship_cost
       stamps.orders.details.blur_out
       stamps.orders.details.blur_out
       sleep 1
-      break if actual.eql? expected_total_amount
+      break if total_ship_cost.to_f == expected_total_amount.to_f
     end
 
     logger.step "  --------------------------------------------------------------------------- "
@@ -48,10 +48,9 @@ Then /^Verify Local Rating$/ do |table|
     logger.step "  --------------------------------------------------------------------------- "
 
     actual = stamps.orders.details.footer.total_ship_cost
-    actual.should == expected_total_amount
+    actual.should eql expected_total_amount
 
     "".should eql "| Test #{index} | #{(results[index])?"Passed":"Failed"} |Expectation=#{element["total"]},Actual=#{total}| | #{element["service"]} | #{element["weight_lbs"]} | #{element["weight_oz"]} | #{element["length"]} | #{element["height"]} | #{element["width"]} | #{element["tracking"]} | #{element["total"]} |" if actual != expected_total_amount
-
   end
 
 end
