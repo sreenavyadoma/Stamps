b3293feeb9627358be7dd869d14fdd11c9d3966a
robcruz <irobcruz@yahoo.com>
2017-01-09 11:25:11 -0800

    ORDERSAUTO-1565 - Drive rating tests from spreadsheet - rates_parcel_select_ground

--- features/step_definitions/orders/rates_sheet_steps.rb
+++ features/step_definitions/orders/rates_sheet_steps.rb
@@ -414,7 +414,7 @@ Then /^Rates: Test Sheet (.*) in Zone (\d+)$/ do |param_sheet, zone|
           @result_sheet[row_num, @columns[:results]] = "Expected #{@result_sheet[row_num, @columns[:expectation]]}, Got #{@result_sheet[row_num, @columns[:total_ship_cost]]}"
         end
         logger.step "#{"#"*10} Zone #{zone} - Row #{row_num}"
-        logger.step "#{"#"*10} Weight: #{@result_sheet[row_num, @columns[:weight]]}, Service: #{@result_sheet[row_num, @columns[:service]]}"
+        logger.step "#{"#"*10} Weight : #{@result_sheet[row_num, @columns[:weight_lb]]}, Selected Service: #{@result_sheet[row_num, @columns[:service_selected]]}"
         logger.step "#{"#"*10} Test #{@result_sheet[row_num, @columns[:status]] } - Expected #{@result_sheet[row_num, @columns[:expectation]]}, Got #{@result_sheet[row_num, @columns[:total_ship_cost]]}"
         logger.step "#{"#"*10} "
       end
