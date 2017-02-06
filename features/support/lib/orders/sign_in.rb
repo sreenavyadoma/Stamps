e3213b4a31bc3743e78d57725e9d4cc60783840e
robcruz <irobcruz@yahoo.com>
2017-02-06 13:10:51 -0800

    Sprint 50 - ORDERSAUTO-1663 Point BVT Tests to Mail Redesign URL and update element IDs.

--- features/support/lib/orders/sign_in.rb
+++ features/support/lib/orders/sign_in.rb
@@ -295,7 +295,7 @@ module Stamps
         username.safely_wait_until_present(8)
 
         signed_in_user = StampsElement.new(browser.span(text: usr))
-        6.times do
+        4.times do
           begin
             if username.present?
               username.set usr
@@ -317,7 +317,7 @@ module Stamps
               end
             end
 
-            8.times {sleep(1) if username.present?}
+            8.times {sleep(0.25) if username.present?}
 
             if invalid_username.present?
               logger.error invalid_username.text
@@ -352,6 +352,8 @@ module Stamps
             raise e
           end
         end
+
+        expect("Unable to sign-in with credentials usr=#{usr}, pw=#{pw}").to eql("Sign-in failed in #{param.test_env}") if username.present?
 
         logger.message "#"*15
         logger.message "Signed-in User: #{signed_in_user.text}"
