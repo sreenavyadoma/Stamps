bf9079da10d6887d20424d87b89af04bfb1cb46e
Eli Ihejirika <eihejirika@stamps.com>
2017-03-21 15:00:17 -0700

    ORDERSAUTO-1841 Update Import_orders test to import 100 orders

--- features/step_definitions/orders/import_orders_steps.rb
+++ features/step_definitions/orders/import_orders_steps.rb
@@ -1,36 +1,38 @@
 Then /^(?:I|i)n Orders Toolbar, Import$/ do
-  stamps.orders.orders_toolbar.import = stamps.orders.orders_toolbar.import
+  stamps.orders.orders_toolbar.import
 end
 
 Then /^Import Orders: Import$/ do
   import_time = stamps.orders.orders_toolbar.import.import
-
   logger.step "Success modal is present after #{import_time} seconds"
 
-  import_timer_filename = "\\\\rcruz-win7\\Public\\automation\\data\\import_times.csv"
-  @rate_file_loc = "#{data_for(:rates_test, {})['test_dir']}\\#{rate_file}"
+  #import_timer_filename = "\\\\rcruz-win7\\Public\\automation\\data\\import_times.csv"
 
+  import_time_file = data_for(:import_orders_test, {})['import_time_file']
+  import_time_loc = "#{data_for(:import_orders_test, {})['import_orders_dir']}\\#{import_time_file}"
 
-  csv_file = CSV.open(@import_timer_filename, "a")
+  logger.step "Import Orders File: #{import_time_loc}"
+  expect("Import Orders File: #{import_time_loc}").to eql "Import Time File does not exist!" unless File.exist?(import_time_loc)
+
+  csv_file = CSV.open(import_time_loc, "a")
   csv_file.add_row([Time.now,import_time])
   logger.step "Import Time Saved to CSV file"
   csv_file.close
 end
 
 Then /^Import Orders: Select CSV File$/ do
-  @open_file = stamps.orders.orders_toolbar.import.select_csv_file
-  expect(@open_file.present?).to be true
+  stamps.orders.orders_toolbar.import.select_csv_file
+  expect(stamps.orders.orders_toolbar.import.select_csv_file.present?).to be true
 end
 
 Then /^Import Orders: Expect Import is successful$/ do
-  stamps.orders.orders_toolbar.import.confirm_success = stamps.orders.orders_toolbar.import.confirm_success
-  expect(stamps.orders.orders_toolbar.import.confirm_success).to be_truthy
-  expect(stamps.orders.orders_toolbar.import.confirm_success.window_title).to eql "Success"
+  expect(stamps.orders.orders_toolbar.import_orders_modal.confirm_success).to be_truthy
+  expect(stamps.orders.orders_toolbar.import_orders_modal.confirm_success.window_title).to eql "Success"
 end
 
 Then /^Import Orders: Success: OK$/ do
-  expect(stamps.orders.orders_toolbar.import.confirm_success).to be_truthy
-  stamps.orders.orders_toolbar.import.confirm_success.ok
+  expect(stamps.orders.orders_toolbar.import_orders_modal.confirm_success).to be_truthy
+  stamps.orders.orders_toolbar.import_orders_modal.confirm_success.ok
 end
 
 Then /^Import Orders: Cancel$/ do
@@ -38,20 +40,23 @@ Then /^Import Orders: Cancel$/ do
 end
 
 Then /^Import Orders: Download sample file$/ do
-  expect(stamps.orders.orders_toolbar.import.confirm_success).to be_truthy
+  expect(stamps.orders.orders_toolbar.import_orders_modal.confirm_success).to be_truthy
   stamps.orders.orders_toolbar.import.orders.download_sample_file
 end
 
-Then /^Import Orders: File Upload: Set Filename to (.*)$/ do |filename| #import_orders_test.csv
+Then /^Import Orders: File Upload: Set Filename to (.*)$/ do |filename| #import_orders_test_100.csv
   step "In Orders Toolbar, Import" if stamps.orders.orders_toolbar.import.nil?
-  step "Import Orders: Select CSV File" if (@open_file.nil? || !(@open_file.present?))
-  @csv_import_filename = "\\\\rcruz-win7\\Public\\automation\\data\\#{filename}"
+  step "Import Orders: Select CSV File" if (stamps.orders.orders_toolbar.import.select_csv_file.nil? || !(stamps.orders.orders_toolbar.import.select_csv_file.present?))
+
+  import_orders_loc = "#{data_for(:import_orders_test, {})['import_orders_dir']}\\#{filename}"
+
+  #@csv_import_filename = "\\\\rcruz-win7\\Public\\automation\\data\\#{filename}"
   #logger.step "Import File:  #{@csv_import_filename}"
+  logger.step "Import File:  #{import_orders_loc}"
 
-  @csv_import_file = CSV.read(@csv_import_filename, :headers=>true)
-  logger.step @csv_import_file['Order ID (required)']
 
-  @open_file.file_name @csv_import_filename
+
+  stamps.orders.orders_toolbar.import.select_csv_file.file_name import_orders_loc
 end
 
 Then /^Import Orders: Expect Imported Filename is (.*)$/ do |expectation|
