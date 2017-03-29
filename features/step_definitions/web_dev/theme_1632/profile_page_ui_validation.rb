3805642dc265a9dbbc2e33e38ced5ad241bd2912
svinod <svinod@stamps.com>
2017-03-29 13:23:03 -0700

    WEBDEVAUTO-46 - Automate Test case WEBDEVAUTO-3 Verify UI of the Profile page

--- features/step_definitions/web_dev/theme_1632/profile_page_ui_validation.rb
+++ features/step_definitions/web_dev/theme_1632/profile_page_ui_validation.rb
@@ -6,11 +6,11 @@ Then /^[Vv]erify Stamps logo exists in navigation bar$/ do
    expect(registration.navigation.stamps_logo.present?).to be(true)
 end
 
-Then /^verify USPS logo exists on navigation bar$/ do
+Then /^[Vv]erify USPS logo exists on navigation bar$/ do
   expect(registration.navigation.usps_logo.present?).to be(true)
 end
 
-Then /^verify progress bar exists on the page$/ do
+Then /^[Vv]erify progress bar exists on the page$/ do
   registration.profile_ui_progressbar
 end
 
@@ -22,67 +22,67 @@ Then /^[Vv]erify Profile page header is (.*)$/ do |str|
   expect(registration.profile.header).to eql(str), "Profile page header is not #{str}"
 end
 
-Then /^verify Email textbox exists on profile page$/ do
+Then /^[Vv]erify Email textbox exists on profile page$/ do
   expect(registration.profile.email.present?).to be(true), "Email textbox does not exists on profile page"
 end
 
-Then /^verify username textbox exists on the page$/ do
+Then /^[Vv]erify username textbox exists on the page$/ do
   registration.profile_ui_username
 end
 
-Then /^verify PASSWORD textbox exists on the page$/ do
+Then /^[Vv]erify PASSWORD textbox exists on the page$/ do
   registration.profile_ui_password
 end
 
-Then /^verify RE-TYPE PASSWORD exists on the page$/ do
+Then /^[Vv]erify RE-TYPE PASSWORD exists on the page$/ do
   registration.profile_ui_retypepassword
 end
 
-Then /^How will you use Stamps.com drop-down includes (.*)$/ do |str|
+Then /^[Hh]ow will you use Stamps.com drop-down includes (.*)$/ do |str|
 
 end
 
-Then /^verify values in dropdown HOW WILL YOU USE STAMPS\.COM$/ do
+Then /^[Vv]erify values in dropdown HOW WILL YOU USE STAMPS\.COM$/ do
    registration.profile_dropdown
 end
 
-Then /^verify PROMO CODE field textbox exists on the page$/ do
+Then /^[Vv]erify PROMO CODE field textbox exists on the page$/ do
   registration.profile_ui_promocodefield
 end
 
-Then /^verify CONTINUE button exists on the page$/ do
+Then /^[Vv]erify CONTINUE button exists on the page$/ do
   registration.profile_continuebutton
 end
 
-Then /^verify content under Why do I need to create an account? exists on the page$/ do
+Then /^[Vv]erify content under Why do I need to create an account? exists on the page$/ do
     registration.profile_content_createanaccount
 end
 
-Then /^verify content under Money-saving offers and new products$/ do
+Then /^[Vv]erify content under Money-saving offers and new products$/ do
   registration.profile_content_moneysavingoffers
 end
 
-Then /^verify Money-saving offers and new products checkbox is checkmarked on the page$/ do
+Then /^[Vv]erify Money-saving offers and new products checkbox is checkmarked on the page$/ do
   registration.profile_moneysavingoffers_checkbox
 end
 
-Then /^verify Privacy Policy link exists on the page and is clickable$/ do
+Then /^[Vv]erify Privacy Policy link exists on the page and is clickable$/ do
   registration.profile_privacypolicylink
 end
 
-Then /^verify copyright link exists on the page and is clickable$/ do
+Then /^[Vv]erify copyright link exists on the page and is clickable$/ do
   registration.profile_copyrightlink
 end
 
-Then /^verify Norton logo exists on the page$/ do
+Then /^[Vv]erify Norton logo exists on the page$/ do
   registration.profile_nortonlogo
 end
 
-Then /^verify TRUSTe logo exists on the page$/ do
+Then /^[Vv]erify TRUSTe logo exists on the page$/ do
   registration.profile_trustelogo
 end
 
-Then /^verify LIVE chat button exists on the page$/ do
+Then /^[Vv]erify LIVE chat button exists on the page$/ do
   registration.profile_livechatbutton
 end
 
