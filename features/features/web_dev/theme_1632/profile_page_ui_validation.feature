4c2fbe00feeee9b8be418b5ed2a08f7bdcb86019
svinod <svinod@stamps.com>
2017-03-31 09:44:14 -0700

    WEBDEVAUTO-46 - Automate Test case WEBDEVAUTO-3 Verify UI of the Profile page

--- features/features/web_dev/theme_1632/profile_page_ui_validation.feature
+++ features/features/web_dev/theme_1632/profile_page_ui_validation.feature
@@ -5,13 +5,13 @@ Feature: WebReg Profile page validation theme_1632
 
   @profile_page_ui_validation
   Scenario:
-    Then navigate to WebReg Profile page
+    Then Load WebReg Profile page
     Then expect WebReg navigationbar Stamps logo exists
     Then expect WebReg navigation bar USPS logo exists
     Then expect WebReg Profile bread crumbs to contain Profile
     Then expect WebReg Profile bread crumbs to contain Membership
     Then expect WebReg Profile bread crumbs to contain Choose Supplies
-    Then expect WebReg Profile header is Sign up for a new account
+    Then expect WebReg Profile header contain Sign up for a new account
     Then expect WebReg Profile email textbox exists
     Then expect WebReg Profile username textbox exists
     Then expect WebReg Profile password textbox exists
@@ -22,17 +22,16 @@ Feature: WebReg Profile page validation theme_1632
     Then expect WebReg Profile Usage Type list of values to contain Individual/Home Office
     Then expect WebReg Profile promo code textbox exists
     Then expect WebReg Profile CONTINUE button exists
-    Then expect webReg Profile content under <h3>Why do I need to create an account?</h3> should contain:
+    Then expect WebReg Profile content under Why do I need to create an account should contain:
     """
     The USPS requires you to register your name, telephone number and physical address from which your postage will be printed. This information will be used to create your Stamps.com account.
     """
-    Then expect WebReg Profile content under<h3>Money-saving offers and new products</h3> should contain:
+    Then expect WebReg Profile content under Money-saving offers and new products should contain:
     """
     Send me special money-saving offers and information on new products and services from Stamps.com and its partners
     """
     Then expect WebReg Profile Money-saving offers and new products checkbox exist
       and is checkmarked
-    Then expect WebReg Profile CONTINUE button exists
     Then expect WebReg Profile Privacy Policy link exists
       and is clickable
     Then expect WebReg Profile copyright link exists
--- features/step_definitions/web_dev/theme_1632/profile_page_ui_validation.rb
+++ features/step_definitions/web_dev/theme_1632/profile_page_ui_validation.rb
@@ -1,93 +1,80 @@
-Then /^[Nn]avigate to WebReg page$/ do
+Then /^[Ll]oad WebReg Profile page$/ do
   webreg.load_page
 end
 
-Then /^[Vv]erify Stamps logo exists in navigation bar$/ do
+Then /^[Ee]xpect WebReg navigationbar Stamps logo exists$/ do
    expect(registration.navigation.stamps_logo.present?).to be(true)
 end
 
-Then /^[Vv]erify USPS logo exists on navigation bar$/ do
+Then /^[Ee]xpect WebReg navigation bar USPS logo exists$/ do
   expect(registration.navigation.usps_logo.present?).to be(true)
 end
 
-#begin
-Then /^[Vv]erify progress bar exists on the page$/ do
-  registration.profile_ui_progressbar
-end
-#end
-
-Then /^[Vv]erify bread crumb should contain (.*)$/ do |str|
-  expect(registration.bread_crumbs).to include("Profile","Membership","Choose Supplies"), "Bread crumb #{str} does not exist "
+Then /^[Ee]xpect WebReg Profile bread crumbs to contain (.*)$/ do |str|
+  expect(registration.bread_crumbs).to eql(str), "Bread crumb #{str} does not exist "
 end
 
-Then /^[Vv]erify Profile page header is (.*)$/ do |str|
-  expect(registration.profile.header).to eql(str), "Profile page header is not #{str}"
+Then /^[Ee]xpect WebReg Profile header contain (.*)$/ do |str|
+  expect(registration.profile.header).to eql(str), "Profile header #{str} does not exist "
 end
 
-Then /^[Vv]erify Email textbox exists on profile page$/ do
+Then /^[Ee]xpect WebReg Profile email textbox exists$/ do
   expect(registration.profile.email.present?).to be(true), "Email textbox does not exists on profile page"
 end
 
-Then /^[Vv]erify username textbox exists on the page$/ do
+Then /^[Ee]xpect WebReg Profile username textbox exists$/ do
   expect(registration.profile.username.present?).to be (true), "Username textbox does not exist on profile page"
-  registration.profile.username.set "myusername"
-  registration.profile.username.text
-  registration.profile.username.enabled?
-  registration.profile.username.disabled?
-  registration.profile.username.place_holder
-  registration.profile.username.visible?
-  registration.profile.username.data_qtip
 end
 
-Then /^[Vv]erify PASSWORD textbox exists on the page$/ do
+Then /^[Ee]xpect WebReg Profile password textbox exists$/ do
   expect(registration.profile.password.present?).to be (true), "Password textbox does not exist on profile page"
 end
 
-Then /^[Vv]erify RE-TYPE PASSWORD exists on the page$/ do
+Then /^[Ee]xpect WebReg Profile retype password textbox exists$/ do
   expect(registration.profile.retype_password.present?).to be (true), "Retype password textbox does not exist on profile page"
 end
 
-Then /^[Hh]ow will you use Stamps.com drop-down includes (.*)$/ do |str|
+Then /^[Ee]xpect WebReg Profile Usage Type list of values to contain (.*)$/ do |str|
   expect(registration.profile.dropdown).to eql(str)
 end
 
-Then /^[Vv]erify PROMO CODE field textbox exists on the page$/ do
+Then /^[Ee]xpect WebReg Profile promo code textbox exists$/ do
   expect(registration.profile.promocode.present?).to be (true)
 end
 
-  Then /^[Vv]erify CONTINUE button exists on the page$/ do
+  Then /^[Ee]xpect WebReg Profile CONTINUE button exists$/ do
   expect(registration.profile_continue_button.present?).to be (true)
 end
 
-Then /^[Vv]erify content under Why do I need to create an account\? includes (.*)$/ do |str|
+Then /^[Ee]xpect WebReg Profile content under Why do I need to create an account should contain (.*)$/ do |str|
     expect(registration.profile_content_createanaccount).to eql(str)
 end
 
-Then /^[Vv]erify content under Money-saving offers and new products includes (.*)$/ do |str|
-  excpect(registration.profile_content_money_saving_offers).to eql(str)
+Then /^[Ee]xpect WebReg Profile content under Money-saving offers and new products should contain (.*)$/ do |str|
+  expect(registration.profile_content_money_saving_offers).to eql(str)
 end
 
-Then /^[Vv]erify Money-saving offers and new products checkbox is checkmarked on the page$/ do
+Then /^[Ee]xpect WebReg Profile Money-saving offers and new products checkbox exist$/ do
   expect(registration.profile_moneysavingoffers_checkbox.present?).to be (true)
 end
 
-Then /^[Vv]erify Privacy Policy link exists on the page and is clickable$/ do
-  expect(registration.profile_privacypolicylink.present?).to be (true)
+Then /^[Ee]xpect WebReg Profile Privacy Policy link exists$/ do
+  expect(registration.profile_privacy_policy.present?).to be (true)
 end
 
-Then /^[Vv]erify copyright link exists on the page and is clickable$/ do
+Then /^[Ee]xpect WebReg Profile copyright link exists$/ do
   expect(registration.profile_copyrightlink.present?).to be (true)
 end
 
-Then /^[Vv]erify Norton logo exists on the page$/ do
+Then /^[Ee]xpect WebReg Profile Norton logo exists$/ do
   expect(registration.profile_nortonlogo.present?).to be (true)
 end
 
-Then /^[Vv]erify TRUSTe logo exists on the page$/ do
+Then /^[Ee]xpect WebReg Profile TRUSTe logo exists$/ do
   expect(registration.profile_trustelogo.present?).to be (true)
 end
 
-Then /^[Vv]erify LIVE chat button exists on the page$/ do
+Then /^[Ee]xpect WebReg Profile LIVE chat button exists$/ do
   expect(registration.profile_livechatbutton.present?).to be (true)
 end
 
