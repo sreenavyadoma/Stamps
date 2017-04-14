442823e43c8243cf65174857b885ee45bd2c2ba0
robcruz <irobcruz@yahoo.com>
2017-04-14 14:08:03 -0700

    WEBDEVAUTO-46 - In Progress4 sub-tasksAutomate Test case WEBDEVAUTO-3 Verify UI of the Profile page

--- features/support/lib/mail/print_form_panel.rb
+++ features/support/lib/mail/print_form_panel.rb
@@ -5,7 +5,10 @@ module Stamps
         def blur_out
           @blur_out = StampsElement.new(browser.label(text: 'Print On:')) if @blur_out.nil? || !@blur_out.present?
           expect(@blur_out).to be_present
-          3.times { @blur_out.double_click }
+          2.times do
+            @blur_out.double_click
+            @blur_out.click
+          end
         end
       end
 
