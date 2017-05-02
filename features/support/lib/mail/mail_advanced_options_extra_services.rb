fd789a919a698d67ffeac5d215bf1957946a4098
ksathavara <ksathavara@stamps.com>
2017-05-02 16:41:11 -0700

    Jenkins Test Execution

--- features/support/lib/mail/mail_advanced_options_extra_services.rb
+++ features/support/lib/mail/mail_advanced_options_extra_services.rb
@@ -10,7 +10,7 @@ module Stamps
           super
           @window_title = StampsElement.new(browser.div(text: "Extra Services"))
           text_boxes = browser.text_fields(id: "sdc-extraserviceswin-securitydroplist-inputEl")
-          drop_downs = browser.div(id: "sdc-extraserviceswin-securitydroplist-trigger-picker")
+          drop_downs = browser.divs(id: "sdc-extraserviceswin-securitydroplist-trigger-picker")
           @security = StampsComboBox.new(text_boxes, drop_downs, :li, 0)
 
           text_box = browser.text_field(id: "sdc-extraserviceswin-valuenumberfield-inputEl")
@@ -19,7 +19,7 @@ module Stamps
           @value = StampsNumberField.new(text_box, inc_btn, dec_btn)
 
           text_boxes = browser.text_fields(id: "sdc-extraserviceswin-contentdroplist-inputEl")
-          drop_downs = browser.div(id: "sdc-extraserviceswin-contentdroplist-trigger-picker")
+          drop_downs = browser.divs(id: "sdc-extraserviceswin-contentdroplist-trigger-picker")
           @handling = StampsComboBox.new(text_boxes, drop_downs, :li, 0)
 
           @save_element = StampsElement.new(browser.span(id: "sdc-extraservices-savebtn-btnInnerEl"))
