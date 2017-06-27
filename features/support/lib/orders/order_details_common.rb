0506636a70671a26d0fae1231a857bc66b4c9a19
robcruz <irobcruz@yahoo.com>
2017-06-27 12:54:29 -0700

    WEBDEVAUTO-121 - Registration + Orders Security Questions

--- features/support/lib/orders/order_details_common.rb
+++ features/support/lib/orders/order_details_common.rb
@@ -3,22 +3,35 @@ module Stamps
     module OrderDetailsCommon
 
       class ShipFromAddress < Browser::StampsModal
-        attr_reader :drop_down, :text_box, :manage_shipping_address
+        attr_reader :manage_shipping_address
 
         def initialize(param, form_type)
           super(param)
+          @manage_shipping_address = ShipFrom::ManageShippingAddresses.new(param)
+        end
+
+        def text_box
           case form_type
             when :single_order
               @text_box = StampsTextBox.new browser.text_fields(name: "ShipFrom")[0]
-              @drop_down = StampsElement.new browser.divs(css: "div[id^=shipfromdroplist][id$=trigger-picker]")[0]
             when :multi_order
               @text_box = StampsTextBox.new browser.text_fields(name: "ShipFrom")[1]
+            else
+              # invalid selection.
+          end
+        end
+
+        def drop_down
+          case form_type
+            when :single_order
+              @drop_down = StampsElement.new browser.divs(css: "div[id^=shipfromdroplist][id$=trigger-picker]")[0]
+            when :multi_order
               @drop_down = StampsElement.new browser.divs(css: "div[id^=shipfromdroplist][id$=trigger-picker]")[1]
             else
               # invalid selection.
               expect([:single_order, :multi_order]).to include(form_type)
           end
-          @manage_shipping_address = ShipFrom::ManageShippingAddresses.new(param)
+
         end
 
         def select(str)
