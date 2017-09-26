89ff6411ada9e5167ed145f1c3afb0c5847d5dc8
ksathavara <ksathavara@stamps.com>
2017-09-26 11:47:11 -0700

    Web Client after Web reg

--- features/support/lib/orders/security_questions_modal.rb
+++ features/support/lib/orders/security_questions_modal.rb
@@ -5,13 +5,25 @@ module Stamps
         attr_accessor :cont_btn, :window_title, :body, :first_security_question, :first_security_answer, :second_security_question, :second_security_answer
         def initialize(param)
           super
-          @cont_btn = StampsElement.new( browser.span(text: "Continue"))
-          @window_title = StampsElement.new( browser.div(text: "Security Questions"))
-          @body = StampsElement.new( browser.div(css: "div[class*='app-window'][class*='window-closable']>div[id$=body]>div>div>div>div>label"))
           @first_security_question = SecurityFirstQuestion.new(param)
           @first_security_answer = StampsTextBox.new(browser.text_fields(css: "input[type=password][name^=textfield-][name$=-inputEl]")[0])
           @second_security_question = SecuritySecondQuestion.new(param)
           @second_security_answer = StampsTextBox.new(browser.text_fields(css: "input[type=password][name^=textfield-][name$=-inputEl]")[1])
+        end
+
+        def cont_btn
+          @cont_btn = StampsElement.new( browser.span(text: "Continue")) if @cont_btn.nil? || !@cont_btn.present?
+          @cont_btn
+        end
+
+        def window_title
+          @window_title = StampsElement.new( browser.div(text: "Security Questions")) if @window_title.nil? || !@window_title.present?
+          @window_title
+        end
+
+        def body
+          @body = StampsElement.new( browser.div(css: "div[class*='app-window'][class*='window-closable']>div[id$=body]>div>div>div>div>label")) if @body.nil? || !@body.present?
+          @body
         end
 
         def present?
@@ -34,11 +46,14 @@ module Stamps
 
       class SecurityFirstQuestion < Browser::StampsModal
         def drop_down
-          StampsElement.new(browser.div(css: "div[id^='container-'][id$='-targetEl']>div>div>div>div[id^='combo-'][id$='-trigger-picker']"))
+          @drop_down = StampsElement.new(browser.div(css: "div[id^='container-'][id$='-targetEl']>div>div>div>div[id^='combo-'][id$='-trigger-picker']")) if @drop_down.nil? || !@drop_down.present?
+          @drop_down
         end
 
         def select(str)
-          selection = StampsElement.new(browser.lis(text: str).first)
+          @selection = StampsElement.new(browser.lis(text: str).first) if @selection.nil? || !@selection.present?
+          @selection
+
           30.times do
             drop_down.click unless selection.present?
             selection.scroll_into_view
@@ -49,11 +64,14 @@ module Stamps
 
       class SecuritySecondQuestion < Browser::StampsModal
         def drop_down
-          StampsElement.new(browser.div(css: "div[id^='container-'][id$='-targetEl']>div>div>div>div[id^='combo-'][id$='-trigger-picker']"))
+          @drop_down = StampsElement.new(browser.div(css: "div[id^='container-'][id$='-targetEl']>div>div>div>div[id^='combo-'][id$='-trigger-picker']")) if @drop_down.nil? || !@drop_down.present?
+          @drop_down
         end
 
         def select(str)
-          selection = StampsElement.new(browser.spans(text: str).last)
+          @selection = StampsElement.new(browser.lis(text: str).first) if @selection.nil? || !@selection.present?
+          @selection
+
           15.times do
             drop_down.click unless selection.present?
             selection.scroll_into_view
