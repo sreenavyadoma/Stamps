095ff89a3368f0950f4a79abf4fb6c3541be7c9d
Eli Ihejirika <eihejirika@stamps.com>
2017-10-18 10:52:01 -0700

    ORDERSAUTO-2915 New Sprint 10/25/17 WEBAPPS-6367 Hide Label Value is Sent for Certified Mail CreateIndicium

--- features/support/lib/mail/mail_toolbar.rb
+++ features/support/lib/mail/mail_toolbar.rb
@@ -22,7 +22,7 @@ module Stamps
       end
 
       def print_button
-        expect([:envelopes, :stamps, :labels, :rolls, :certified_mails]).to include(param.print_media)
+        expect([:envelopes, :stamps, :labels, :rolls, :certified_mails, :certified_mails_3910_3930, :certified_mails_3810]).to include(param.print_media)
         10.times do
           case param.print_media
             when :envelopes
