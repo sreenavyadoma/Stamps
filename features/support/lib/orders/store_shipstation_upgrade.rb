33c1870b6bb1e1b4b54e30fa3bca420e46e09504
ksathavara <ksathavara@stamps.com>
2017-11-01 12:22:06 -0700

    Ship Station upgrade

--- features/support/lib/orders/store_shipstation_upgrade.rb
+++ features/support/lib/orders/store_shipstation_upgrade.rb
@@ -38,6 +38,88 @@ module Stamps
           @close
         end
       end
+
+      class AddAdvancedShippingFeatures < StoreSettings
+
+        # To-Do by Kaushal
+        def dialog_header
+          @dialog_header = StampsElement.new(browser.img(css: "")) if @dialog_header.nil? || !@dialog_header.present?
+          expect(@dialog_header).to be_present
+          @dialog_header
+        end
+
+        def present?
+          dialog_header.present?
+        end
+
+        def body
+          @body = StampsElement.new(browser.img(css: "")) if @body.nil? || !@body.present?
+          expect(@body).to be_present
+          @body
+        end
+
+        def shipstation_logo
+          @shipstation_logo = StampsElement.new(browser.img(css: "")) if @shipstation_logo.nil? || !@shipstation_logo.present?
+          expect(@shipstation_logo).to be_present
+          @shipstation_logo
+        end
+
+        def add_features_now
+          @add_features_now = StampsElement.new(browser.img(css: "")) if @add_features_now.nil? || !@add_features_now.present?
+          expect(@add_features_now).to be_present
+          @add_features_now
+        end
+
+        def close_by_x
+          @close_by_x = StampsElement.new(browser.img(css: "")) if @close_by_x.nil? || !@close_by_x.present?
+          expect(@close_by_x).to be_present
+          @close_by_x
+        end
+      end
+
+      class ActivateYourNewFeatures < StoreSettings
+
+        # To-Do by Kaushal
+        def body
+          @body = StampsElement.new(browser.img(css: "")) if @body.nil? || !@body.present?
+          expect(@body).to be_present
+          @body
+        end
+
+        def present?
+          header.present?
+        end
+
+        def header
+          @header = StampsElement.new(browser.img(css: "")) if @header.nil? || !@header.present?
+          expect(@header).to be_present
+          @header
+        end
+
+        def username
+          @username = StampsElement.new(browser.img(css: "")) if @username.nil? || !@username.present?
+          expect(@username).to be_present
+          @username
+        end
+
+        def password
+          @password = StampsElement.new(browser.img(css: "")) if @password.nil? || !@password.present?
+          expect(@password).to be_present
+          @password
+        end
+
+        def check_box
+          @check_box = StampsElement.new(browser.img(css: "")) if @check_box.nil? || !@check_box.present?
+          expect(@check_box).to be_present
+          @check_box
+        end
+
+        def login
+          @login = StampsElement.new(browser.img(css: "")) if @login.nil? || !@login.present?
+          expect(@login).to be_present
+          @login
+        end
+      end
     end
   end
 end
