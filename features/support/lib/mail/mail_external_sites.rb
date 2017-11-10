3c69dab46ce4401b50e2df06a4a5f436166c0439
ksathavara <ksathavara@stamps.com>
2017-11-10 14:35:01 -0800

    ORDERSAUTO-3274
    Buy more labels Automation

--- features/support/lib/mail/mail_external_sites.rb
+++ features/support/lib/mail/mail_external_sites.rb
@@ -3,7 +3,9 @@ module Stamps
     class MailExternalSites < Browser::StampsModal
       def net_stamps_home_btn
         browser.windows.last.use
+        15.times do
         (cache[:net_stamps_home_btn].nil?||!cache[:net_stamps_home_btn].present?)?cache[:net_stamps_home_btn] = StampsField.new(browser.img(css: "img[src*=home_logo]")):cache[:net_stamps_home_btn]
+        end
       end
 
       private
