7e9e04774ec254cde70e2188140778502dc0c439
abaranovskyi <abaranovskyi@stamps.com>
2017-11-22 17:16:17 -0800

    Fixes for bvt_mail_piece_validation

--- features/step_definitions/mail/mail_forgot_credentials_steps.rb
+++ features/step_definitions/mail/mail_forgot_credentials_steps.rb
@@ -6,7 +6,7 @@ end
 
 Then /^[Oo]n Print form, set forgotten email to (.*)$/ do |email|
   #test_config.logger.step "on Print form, enter forgotten email: \n #{email}"
-  @forgot_username=stamps.mail.sign_in_modal.forgot_username if @forgot_username.nil?
+  #@forgot_username=stamps.mail.sign_in_modal.forgot_username if @forgot_username.nil?
   @forgot_username.email.set email
   @forgot_username.continue
   @forgot_username.ok
@@ -19,26 +19,28 @@ end
 
 Then /^[Oo]n Print form, set forgotten username to (.*)$/ do |username|
   #test_config.logger.step "on Print form, enter forgotten username: \n #{username}"
-  @forgot_password=stamps.mail.sign_in_modal.forgot_password if @forgot_password.nil?
+  #@forgot_password=stamps.mail.sign_in_modal.forgot_password if @forgot_password.nil?  #Alex
   @forgot_password.username.set username
   @forgot_password.continue
 end
 
 Then /^[Oo]n Print form, set secret answer 1 to (.*)$/ do |answer|
+#Then /^[Oo]n Print form, set secret answer (\d+) to (.*)$/ do |number, answer|
   #test_config.logger.step "on Print form, set secret answer 1 to: \n #{answer}"
-  @forgot_password=stamps.mail.sign_in_modal.forgot_password if @forgot_password.nil?
-  @forgot_password.secret_answer_1.set answer
+  #@forgot_password=stamps.mail.sign_in_modal.forgot_password if @forgot_password.nil?  #Alex
+  @forgot_password.secret_answer_1.set answer   #Alex test
+  #@forgot_password.secret_answer(number).set(answer)
 end
 
 Then /^[Oo]n Print form, set secret answer 2 to (.*)$/ do |answer|
   #test_config.logger.step "on Print form, set secret answer 1 to: \n #{answer}"
-  @forgot_password=stamps.mail.sign_in_modal.forgot_password if @forgot_password.nil?
+  #@forgot_password=stamps.mail.sign_in_modal.forgot_password if @forgot_password.nil?  #Alex
   @forgot_password.secret_answer_2.set answer
 end
 
 Then /^Mail: Confirm password reset/ do
   #test_config.logger.step "Mail: Confirm password reset"
-  @forgot_password=stamps.mail.sign_in_modal.forgot_password if @forgot_password.nil?
+  #@forgot_password=stamps.mail.sign_in_modal.forgot_password if @forgot_password.nil?  #Alex
   @forgot_password.continue
   @forgot_password.confirm_reset
 end
