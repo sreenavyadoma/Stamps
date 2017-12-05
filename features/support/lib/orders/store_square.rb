99d905a53dfd6f274ffe5db5de83170717cb9ca4
ksathavara <ksathavara@stamps.com>
2017-12-05 12:19:56 -0800

    Import square store

--- features/support/lib/orders/store_square.rb
+++ features/support/lib/orders/store_square.rb
@@ -12,6 +12,76 @@ module Stamps
         end
       end
 
+      module GeneralSettings
+        def text
+          (cache[:text].nil?||!cache[:text].present?)?cache[:text]= StampsField.new(browser.h3(text: "General Settings")):cache[:text]
+        end
+
+        def store_nickname
+          (cache[:store_nickname].nil?||!cache[:store_nickname].present?)?cache[:store_nickname]=StampsTextbox.new(iframe.text_field(id: "storeName")):cache[:store_nickname]
+        end
+
+        def checkbox
+          input=iframe.input(css: 'input[id=importOrders]')
+          verify=iframe.input(css: 'input[id=importOrders]')
+          Stamps::Browser::StampsCheckbox.new(input, verify, "class", "ng_not_empty")
+        end
+      end
+
+      module ServiceMapping
+
+        def text
+          (cache[:text].nil?||!cache[:text].present?)?cache[:text]= StampsField.new(browser.h3(text: "Service Mapping")):cache[:text]
+        end
+
+        def requested_service
+          (cache[:requested_service].nil?||!cache[:requested_service].present?)?cache[:requested_service]= StampsTextbox.new(iframe.text_field(css: "div[id^='serviceName-'][name='serviceName']]")) :cache[:requested_service]
+        end
+
+        def shipping_serv
+          (cache[:shipping_serv].nil?||!cache[:shipping_serv].present?)?cache[:shipping_serv]=StampsTextbox.new(iframe.text_field(id: "storeName")):cache[:shipping_serv]
+        end
+
+        def checkbox
+          input=iframe.input(css: 'input[id=importOrders]')
+          verify=iframe.input(css: 'input[id=importOrders]')
+          Stamps::Browser::StampsCheckbox.new(input, verify, "class", "ng_not_empty")
+        end
+      end
+
+      class Settings < Browser::StampsModal
+
+        def window_title
+          StampsField.new browser.div(text: "Settings")
+        end
+
+        def present?
+          window_title.present?
+        end
+
+        def wait_until_present
+          window=window_title
+          3.times do
+            break if window.present?
+            sleep(0.35)
+          end
+        end
+
+        def close
+          button=StampsField.new(browser.img(css: "img[src*='data:image/gif']"))
+          5.times do
+            button.click
+            sleep(0.35)
+            break unless button.present?
+          end
+        end
+
+        def save
+          (cache[:save].nil?||!cache[:save].present?)?cache[:save]=StampsField.new(browser.button(id: "saveSettings")):cache[:save]
+        end
+      end
+
+
       class Square < Browser::StampsModal
         include SqaureWindowTitle
 
