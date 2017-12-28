5c6d45ae5ba3c3aee1c402b426b46f2b4fcf4897
Eli Ihejirika <eihejirika@stamps.com>
2017-12-28 12:08:00 -0800

    ORDERSAUTO-3405 New Sprint 11/22/17 WEBAPPS-6706 Split Order: Automatically Select New Order After Split

--- features/support/lib/orders/split_order.rb
+++ features/support/lib/orders/split_order.rb
@@ -2,22 +2,25 @@ module Stamps
   module Orders
 
       class SplitOrderModal < Browser::Base
-        attr_reader :window_title
 
-        def initialize(param)
-          super
-
-          @window_title=StampsField.new(browser.div(css: "div[id^=splitorderwindow-][id$=innerCt]"))
-
-        end
-
-        def original_order_id
-          id=StampsField.new(browser.div(css: "div[id^=splitorderwindow-][id$=targetEl]>div>div"))
-          /\d+/.match(id.text).to_s
+        def window_title
+          (cache[:window_title].nil?||!cache[:window_title].present?)?cache[:window_title]=StampsField.new(browser.div(css: "div[id^=splitorderwindow-][id$=innerCt]")):cache[:window_title]
         end
 
         def new_order_id
-          StampsTextbox.new(browser.text_field(css: "div[id^=splitorderwindow-][class*=dialogue-content]>div>div>div>div>div>div>div>div>div>div>div>input"))
+          (cache[:new_order_id].nil?||!cache[:new_order_id].present?)?cache[:new_order_id]=StampsTextbox.new(browser.text_field(css: "div[id^=splitorderwindow-][class*=dialogue-content]>div>div>div>div>div>div>div>div>div>div>div>input")):cache[:new_order_id]
+        end
+
+        def confirm
+          (cache[:confirm].nil?||!cache[:confirm].present?)?cache[:confirm]=StampsField.new(browser.span(text: "Confirm")):cache[:confirm]
+        end
+
+        def id
+          (cache[:id].nil?||!cache[:id].present?)?cache[:id]=StampsField.new(browser.div(css: "div[id^=splitorderwindow-][id$=targetEl]>div>div")):cache[:id]
+        end
+
+        def original_order_id
+          /\d+/.match(id.text).to_s
         end
 
         def present?
@@ -25,7 +28,7 @@ module Stamps
         end
 
         def item(number)
-          SplitItemDetails.new(param, number)
+          (cache[:item].nil?||!cache[:item].present?)?cache[:item]=SplitItemDetails.new(param, number):cache[:item]
         end
 
         def confirm_order_split
@@ -33,24 +36,44 @@ module Stamps
           sleep 5
         end
 
-        def confirm
-          StampsField.new(browser.span(text: "Confirm"))
-        end
+
 
         class SplitItemDetails < Browser::Base
-          attr_reader :index, :original_item_qty, :original_item_id, :original_item_description, :new_item_qty
+
+          attr_reader :index
           def initialize(param, number)
             super(param)
             @index=number
-            inc_btn=browser.divs(css: "div[id^=singlesplitorderitem-][id$=-targetEl]>div>div>div>div>div[class*=up]")[@index]
-            dec_btn=browser.divs(css: "div[id^=singlesplitorderitem-][id$=-targetEl]>div>div>div>div>div[class*=down]")[@index]
-
-            @original_item_id=(StampsField.new(browser.divs(css: "div[id^=singlesplitorderitem-][id$=targetEl]>div>div>div>label")[@index*3 + 1])).text
-            @original_item_description=(StampsField.new(browser.divs(css: "div[id^=singlesplitorderitem-][id$=targetEl]>div>div>div>label")[@index*3])).text
-            @original_item_qty=(StampsField.new(browser.labels(css: "div[id^=singlesplitorderitem-][id$=targetEl]>div>div>div>label")[@index*3 + 2])).text
-            @new_item_qty=StampsTextbox.new(browser.text_fields(css: "div[id^=singlesplitorderitem-][id$=targetEl]>div>div>div>div>input")[@index])
           end
 
+          def inc_btn
+            (cache[:inc_btn].nil?||!cache[:inc_btn].present?)?cache[:inc_btn]=browser.divs(css: "div[id^=singlesplitorderitem-][id$=-targetEl]>div>div>div>div>div[class*=up]")[@index]:cache[:inc_btn]
+
+          end
+
+          def dec_btn
+            (cache[:dec_btn].nil?||!cache[:dec_btn].present?)?cache[:dec_btn]=browser.divs(css: "div[id^=singlesplitorderitem-][id$=-targetEl]>div>div>div>div>div[class*=down]")[@index]:cache[:dec_btn]
+
+          end
+
+          def original_item_id
+            (cache[:original_item_id].nil?||!cache[:original_item_id].present?)?cache[:original_item_id]=(StampsField.new(browser.divs(css: "div[id^=singlesplitorderitem-][id$=targetEl]>div>div>div>label")[@index*3 + 1]).text):cache[:original_item_id]
+
+          end
+
+          def original_item_description
+            (cache[:original_item_description].nil?||!cache[:original_item_description].present?)?cache[:original_item_description]=(StampsField.new(browser.divs(css: "div[id^=singlesplitorderitem-][id$=targetEl]>div>div>div>label")[@index*3]).text):cache[:original_item_description]
+
+          end
+
+          def original_item_qty
+            (cache[:original_item_qty].nil?||!cache[:original_item_qty].present?)?cache[:original_item_qty]=(StampsField.new(browser.labels(css: "div[id^=singlesplitorderitem-][id$=targetEl]>div>div>div>label")[@index*3 + 2]).text):cache[:original_item_qty]
+
+          end
+
+          def new_item_qty
+            (cache[:new_item_qty].nil?||!cache[:new_item_qty].present?)?cache[:new_item_qty]=StampsTextbox.new(browser.text_fields(css: "div[id^=singlesplitorderitem-][id$=targetEl]>div>div>div>div>input")[@index]):cache[:new_item_qty]
+          end
         end
 
 
