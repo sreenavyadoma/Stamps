e6da15a097c3a8178844c2c1bd798843d585b43f
Eli Ihejirika <eihejirika@stamps.com>
2017-12-28 14:25:09 -0800

    ORDERSAUTO-3405 New Sprint 11/22/17 WEBAPPS-6706 Split Order: Automatically Select New Order After Split

--- features/support/lib/orders/split_order.rb
+++ features/support/lib/orders/split_order.rb
@@ -1,25 +1,25 @@
 module Stamps
   module Orders
 
-      class SplitOrderModal < Browser::Base
+      class SplitOrderModal < Browser::Base #this class represents the Split Order Modal
 
-        def window_title
+        def window_title #the window title is used the verify that the modal has appeared
           (cache[:window_title].nil?||!cache[:window_title].present?)?cache[:window_title]=StampsField.new(browser.div(css: "div[id^=splitorderwindow-][id$=innerCt]")):cache[:window_title]
         end
 
-        def new_order_id
+        def new_order_id #The default text in the New Order ID field, usually the existing orders ID with -1 appended to the end
           (cache[:new_order_id].nil?||!cache[:new_order_id].present?)?cache[:new_order_id]=StampsTextbox.new(browser.text_field(css: "div[id^=splitorderwindow-][class*=dialogue-content]>div>div>div>div>div>div>div>div>div>div>div>input")):cache[:new_order_id]
         end
 
-        def confirm
+        def confirm #the Confirm button in the Split Order Modal
           (cache[:confirm].nil?||!cache[:confirm].present?)?cache[:confirm]=StampsField.new(browser.span(text: "Confirm")):cache[:confirm]
         end
 
-        def id
+        def id #the id of the original order, as shown in the Split Order Modal window title
           (cache[:id].nil?||!cache[:id].present?)?cache[:id]=StampsField.new(browser.div(css: "div[id^=splitorderwindow-][id$=targetEl]>div>div")):cache[:id]
         end
 
-        def original_order_id
+        def original_order_id #the id of the original order, formatted to remove all other text
           /\d+/.match(id.text).to_s
         end
 
@@ -27,11 +27,11 @@ module Stamps
           window_title.present?
         end
 
-        def item(number)
-          (cache[:item].nil?||!cache[:item].present?)?cache[:item]=SplitItemDetails.new(param, number):cache[:item]
+        def item(number) #instantiate an object that represents the item details in the Split Order Modal
+          SplitItemDetails.new(param, number)
         end
 
-        def confirm_order_split
+        def confirm_order_split #click the Confirm button
           confirm.click
           sleep 5
         end
@@ -46,29 +46,28 @@ module Stamps
             @index=number
           end
 
-          def inc_btn
+          def inc_btn #button to increment the value of the associated field in the item details form (identified by its index)
             browser.divs(css: "div[id^=singlesplitorderitem-][id$=-targetEl]>div>div>div>div>div[class*=up]")[@index]
           end
 
-          def dec_btn
+          def dec_btn #button to dencrement the value of the associated field in the item details form (identified by its index)
             browser.divs(css: "div[id^=singlesplitorderitem-][id$=-targetEl]>div>div>div>div>div[class*=down]")[@index]
           end
 
-          def original_item_id #item id for order that is getting split
+          def original_item_id #id for each item that is listed in original order (identified by its index)
             StampsField.new(browser.divs(css: "div[id^=singlesplitorderitem-][id$=targetEl]>div>div>div>label")[@index*3 + 1]).text
-
           end
 
-          def original_item_description
+          def original_item_description #description for each item that is listed in original order (identified by its index)
             StampsField.new(browser.divs(css: "div[id^=singlesplitorderitem-][id$=targetEl]>div>div>div>label")[@index*3]).text
 
           end
 
-          def original_item_qty
+          def original_item_qty #quantity for each item that is listed in original order (identified by its index)
             StampsField.new(browser.labels(css: "div[id^=singlesplitorderitem-][id$=targetEl]>div>div>div>label")[@index*3 + 2]).text
           end
 
-          def new_item_qty
+          def new_item_qty #description for each item that is listed in original order (identified by its index)
             StampsTextbox.new(browser.text_fields(css: "div[id^=singlesplitorderitem-][id$=targetEl]>div>div>div>div>input")[@index])
           end
         end
