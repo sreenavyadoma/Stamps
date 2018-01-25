a18d8a2e9e886f98e018683805293222b12fff3f
robcruz <rcruz@stamps.com>
2018-01-25 07:23:55 -0800

    ORDERSAUTO-3595 Fix to Bulk & Order Details International Services

--- features/support/lib/orders/orders_modals.rb
+++ features/support/lib/orders/orders_modals.rb
@@ -12,17 +12,17 @@ module Stamps
     # Orders floating modals
     module StampsOrdersModals
       def orders_print_modal
-        cache[:orders_print_modal] = Printing::OrdersPrintModal.new(param) if cache[:orders_print_modal].nil? || !cache[:orders_print_modal].present?
+        cache[:orders_print_modal] = Printing::OrdersPrintModal.new(param) if cache[:orders_print_modal].nil?
         cache[:orders_print_modal]
       end
 
       def orders_settings_modal
-        cache[:orders_settings] = OrdersSettings::OrdersSettingsModal.new(param) if cache[:orders_settings].nil? || !cache[:orders_settings].present?
+        cache[:orders_settings] = OrdersSettings::OrdersSettingsModal.new(param) if cache[:orders_settings].nil?
         cache[:orders_settings]
       end
 
       def split_order_modal
-        cache[:split_order] = SplitOrder::Modal.new(param) if cache[:split_order].nil? || !cache[:split_order].present?
+        cache[:split_order] = SplitOrder::Modal.new(param) if cache[:split_order].nil?
         cache[:split_order]
       end
 
@@ -77,7 +77,7 @@ module Stamps
       end
 
       def terms_conditions
-        cache[:terms_conditions] = TermsAndConditionsModal.new(param) if cache[:terms_conditions].nil? || !cache[:terms_conditions].present?
+        cache[:terms_conditions] = TermsAndConditionsModal.new(param) if cache[:terms_conditions].nil?
         cache[:terms_conditions]
       end
     end
