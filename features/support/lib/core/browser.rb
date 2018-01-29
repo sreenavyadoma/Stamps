5d7748cf347d90bb41aa9a05d4df5acfbe89f7e1
robcruz <rcruz@stamps.com>
2018-01-29 12:09:20 -0800

    ORDERSAUTO-3595 Fix to Bulk & Order Details International Services

--- features/support/lib/core/browser.rb
+++ features/support/lib/core/browser.rb
@@ -18,7 +18,7 @@ module Stamps
           end
 
           def cache
-            @cache.nil? ? raise(ArgumentError, "Missing #{self.name.split('::').last}.assign({}) statement, cache not set for #{self}") : @cache
+            @cache.nil? ? raise(ArgumentError, "Cache not set for class #{self.to_s.split('::').last}") : @cache
           end
         end
       end
