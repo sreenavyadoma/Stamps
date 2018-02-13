d0c761bf241cd0f2c3b21bcdb1fc586469c78d5e
mhossain <mhossain@stamps.com>
2018-02-13 11:52:48 -0800

    Added fire event for checkboxes

--- features/support/lib/web_dev/registration/bread_crumbs.rb
+++ features/support/lib/web_dev/registration/bread_crumbs.rb
@@ -3,12 +3,12 @@ module Stamps
     class RegistrationBreadCrumbs < Browser::Base
 
       def bread_crumb_profile
-        cache[:bread_crumb_profile] = StampsField.new(browser.span(css: "li[id=breadcrumb_Profile]>span")) if cache[:bread_crumb_profile].nil? || cache[:bread_crumb_profile].present?
+        cache[:bread_crumb_profile] = StampsField.new(browser.span(css: "li[id=breadcrumb_Profile]>span")) if cache[:bread_crumb_profile].nil? || !cache[:bread_crumb_profile].present?
         cache[:bread_crumb_profile]
       end
 
       def bread_crumb_membership
-        cache[:bread_crumb_membership] = StampsField.new(browser.span(css: "li[id=breadcrumb_Membership]>span")) if cache[:bread_crumb_membership].nil? || cache[:bread_crumb_membership].present?
+        cache[:bread_crumb_membership] = StampsField.new(browser.span(css: "li[id=breadcrumb_Membership]>span")) if cache[:bread_crumb_membership].nil? || !cache[:bread_crumb_membership].present?
         cache[:bread_crumb_membership]
       end
 
