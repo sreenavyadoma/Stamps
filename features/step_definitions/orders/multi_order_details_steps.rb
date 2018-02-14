c3d323784f5508857144916cc21db093dfdee79d
Eli Ihejirika <eihejirika@stamps.com>
2018-02-13 17:24:24 -0800

    ORDERSAUTO-3714 Fix BVT Orders tests for Shipstation

--- features/step_definitions/orders/multi_order_details_steps.rb
+++ features/step_definitions/orders/multi_order_details_steps.rb
@@ -173,7 +173,7 @@ end
 
 Then /^[Cc]lick [Bb]ulk [Uu]pdate [Uu]pdate [Oo]rder [Bb]utton$/ do
   stamps.orders.bulk_update.update_orders.click
-  sleep 30
+  sleep 7
 end
 
 Then /^[Ee]xpect [Bb]ulk [Uu]pdate is present$/ do
