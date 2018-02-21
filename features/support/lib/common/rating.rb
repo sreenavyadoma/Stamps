de31e7b05b0f13645d10320aa80f913080081ed6
abaranovskyi <nhfdrf1989ptktyttn!>
2018-02-21 13:27:55 -0800

    ORDERSAUTO-3743 - Automate copying of rates parameter sheets onto Jenkins VM nodes.

---
+++ features/support/lib/common/rating.rb
@@ -0,0 +1,36 @@
+module Stamps
+  class Rating < Browser::Base
+    def spreadsheet
+      cache[:spreadsheet].nil? ? cache[:spreadsheet] = SpreadSheet.new(param) : cache[:spreadsheet]
+    end
+  end
+
+  class SpreadSheet < Browser::Base
+    def source_file
+      "#{data_for(:rates_test, {})['source_dir']}\\#{data_for(:rates_test, {})['rate_file']}"
+    end
+
+    def test_file
+      "#{data_for(:rates_test, {})['test_dir']}\\#{data_for(:rates_test, {})['rate_file']}"
+    end
+
+    def update
+      update_file unless updated?
+      updated?
+    end
+
+    def updated?
+      return false unless File.file?(test_file)
+      FileUtils.compare_file(source_file, test_file)
+    end
+
+    def update_file
+      begin
+        FileUtils.copy_file(source_file, test_file)
+      rescue
+        false
+      end
+      true
+    end
+  end
+end
