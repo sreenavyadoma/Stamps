eeec240fe97e2d3a9ee963771682f01bc48dbc2d
abaranovskyi <nhfdrf1989ptktyttn!>
2018-02-27 14:54:56 -0800

    AB_ORDERSAUTO_3690 - Adding OS version parsing

--- features/support/lib/core/test_setup.rb
+++ features/support/lib/core/test_setup.rb
@@ -43,7 +43,8 @@ module Stamps
               profile['network.http.phishy-userpass-length'] = 255
               driver = Watir::Browser.new(:firefox, :profile => profile)
             end
-            self.browser_version = /Firefox\/.+/.match(driver.execute_script("return navigator.userAgent;"))
+            # self.browser_version = /Firefox\/.+/.match(driver.execute_script("return navigator.userAgent;"))
+            get_versions(driver.execute_script("return navigator.userAgent;"))
             driver.window.resize_to 1560, 1020
             driver.window.move_to 0, 0
           when :chrome
@@ -55,7 +56,8 @@ module Stamps
               #ignore
             end
             driver = Watir::Browser.new(:chrome, switches: %w(--ignore-certificate-errors --disable-popup-blocking --disable-translate))
-            self.browser_version = /Chrome\/.+/.match(driver.execute_script("return navigator.userAgent;"))
+            # self.browser_version = /Chrome\/.+/.match(driver.execute_script("return navigator.userAgent;"))
+            get_versions(driver.execute_script("return navigator.userAgent;"))
             driver.window.maximize
             #switches: ['--ignore-certificate-errors --disable-popup-blocking --disable-translate']
           when :ie # Launch Internet Explorer
@@ -146,7 +148,8 @@ module Stamps
       # match = /([\d.]+) (Safari)/.match(info)
       # self.browser_version = "#{match[2]} #{match[1]}"
       self.browser_version = /[\d.]+ Safari|Edge\/.+|Firefox\/.+|Chrome\/.+/.match(info)
-      self.os_version = /(Mac OS.+[\d_]+)\)/.match(info)[1]
+      # self.os_version = /(Mac OS.+[\d_]+|Windows.*?[\d\.])\)/.match(info)[1]
+      self.os_version = /(Mac OS.+[\d_]+|Windows.+?[\d]{1,})/.match(info)
     end
 
 
