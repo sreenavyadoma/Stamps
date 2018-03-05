93ba073c4f42d055c709a9b33234e0eb056a1928
robcruz <rcruz@stamps.com>
2018-03-05 11:15:52 -0800

    ORDERSAUTO-3830 Create Mobile test framework

--- features/support/lib/_mobile_and_web_core/element_finder.rb
+++ features/support/lib/_mobile_and_web_core/element_finder.rb
@@ -1,23 +1,25 @@
 module Stamps
-  class ElementFinder
-    ABSOLUTE_MAX_WAIT_TIME_SECONDS = 12
+  module Core
+    class ElementFinder
+      ABSOLUTE_MAX_WAIT_TIME_SECONDS = 12
 
-    attr_reader :driver
-    def initialize(driver)
-      @driver = driver
-    end
+      attr_reader :driver
+      def initialize(driver)
+        @driver = driver
+      end
 
-    def element(locator, max_time_in_sec)
-      if max_time_in_sec < 0 || max_time_in_sec > ABSOLUTE_MAX_WAIT_TIME_SECONDS
-        max_time_in_sec = ABSOLUTE_MAX_WAIT_TIME_SECONDS
+      def element(locator, max_time_in_sec)
+        if max_time_in_sec < 0 || max_time_in_sec > ABSOLUTE_MAX_WAIT_TIME_SECONDS
+          max_time_in_sec = ABSOLUTE_MAX_WAIT_TIME_SECONDS
+        end
+        begin
+          element = driver.element(locator).to_subtype
+          element.wait_until_present(max_time_in_sec)
+        rescue
+          element = nil
+        end
+        element
       end
-      begin
-        element = driver.element(locator).to_subtype
-        element.wait_until_present(max_time_in_sec)
-      rescue
-        element = nil
-      end
-      element
     end
   end
 end
