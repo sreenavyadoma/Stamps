6df5e91e8856804d1b2e63aa5526628f7fcaa26a
robcruz <rcruz@stamps.com>
2018-03-05 11:51:26 -0800

    ORDERSAUTO-3830 Create Mobile test framework

--- features/support/lib/_mobile_and_web_core/action_methods.rb
+++ features/support/lib/_mobile_and_web_core/action_methods.rb
@@ -1,30 +1,45 @@
 module Stamps
-  class ActionMethods
-    attr_reader :driver, :finder
-    def initialize(driver, finder)
-      @driver = driver
-      @finder = finder
+  module Core
+    class ActionMethods
+      attr_reader :driver, :finder
+      def initialize(driver, finder)
+        @driver = driver
+        @finder = finder
+      end
+
+      def set(text, locator, max_time_in_sec)
+        element = finder.element(locator, max_time_in_sec)
+        throw_or_log_error(locator, call[0]) if element.nil?
+
+        browser.text_field(name: "new_user_first_name").wait_until_present
+        # need helper class that will create element based on locator
+        browser.text_field(name: "new_user_first_name").wait_until(&:present?).click
+        browser.text_field(name: "new_user_first_name").wait_until(message: 'foo') { |field| field.present? }
+        browser.text_field(name: "new_user_first_name").wait_until(timeout: 60, &:present?)
+      end
+
+      def send_keys(text, locator, max_time_in_sec)
+
+      end
+
+      def click(locator, max_time_in_sec)
+        element = finder.element(locator, max_time_in_sec)
+        raise "Unable to locate element with locator #{locator.to_s}" if element.nil?
+        element.click
+      end
+
+      def wait_until_present
+
+      end
+
+      def wait_while_present
+
+      end
+
+      def throw_or_log_error(locator, method_name_from)
+        # set a flag that enables framework to throw an exception or log error. throwOrLogError
+        raise "#{method_name_from} - Unable to locate element with locator #{locator.to_s}"
+      end
     end
-
-    def set(text, locator, max_time_in_sec)
-      element = finder.element(locator, max_time_in_sec)
-      raise "Unable to locate element with locator #{locator.to_s}" if element.nil?
-      browser.text_field(name: "new_user_first_name").wait_until_present
-      # need helper class that will create element based on locator
-      browser.text_field(name: "new_user_first_name").wait_until(&:present?).click
-      browser.text_field(name: "new_user_first_name").wait_until(message: 'foo') { |field| field.present? }
-      browser.text_field(name: "new_user_first_name").wait_until(timeout: 60, &:present?)
-    end
-
-    def send_keys(text, locator, max_time_in_sec)
-
-    end
-
-    def click(locator, max_time_in_sec)
-      element = finder.element(locator, max_time_in_sec)
-      raise "Unable to locate element with locator #{locator.to_s}" if element.nil?
-      element.click
-    end
-
   end
 end
--- features/support/lib/_mobile_and_web_core/page_objects.rb
+++ features/support/lib/_mobile_and_web_core/page_objects.rb
@@ -1,9 +1,32 @@
 module Stamps
   module Core
+
+    ##
+    #
+    module Cache
+      class << self
+        def included(base)
+          base.extend ClassMethods
+        end
+
+        module ClassMethods
+          def assign(cache)
+            @cache = cache
+          end
+
+          def cache
+            raise(ArgumentError, "Cache not set for #{self.to_s.split('::').last}") if @cache.nil?
+            @cache
+          end
+        end
+      end
+    end
+
     class PageObject
-      attr_reader :driver
+      attr_reader :driver, :locators
       def initialize(driver)
         @driver = driver
+        @locators = {}
         @finder = ElementFinder.new(driver)
         @actions = StampsActionMethods.new(driver, finder, call[0])
       end
@@ -13,6 +36,7 @@ module Stamps
       end
     end
   end
+
 
 
 end
