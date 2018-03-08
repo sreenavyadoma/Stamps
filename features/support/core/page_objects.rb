450ccbe9f2f11d4d1a0684997f4c9354e04bc1f5
Rob Cruz <robert.cruz@gmail.com>
2018-03-07 16:16:19 -0800

    Mobile Testing

--- features/support/core/page_objects.rb
+++ features/support/core/page_objects.rb
@@ -1,11 +1,8 @@
 module Stamps
   module Core
-
     class PageObject
-      attr_reader :driver, :locators
       def initialize(driver)
         @driver = driver
-        @locators = {}
         @finder = ElementFinder.new(driver)
         @actions = StampsActionMethods.new(driver, finder, call[0])
       end
@@ -13,9 +10,10 @@ module Stamps
       def is_loaded?
         raise NotImplementedError, "#{self.class.name}#is_loaded? is an abstract method."
       end
+
+      protected
+      attr_reader :driver, :locators
     end
   end
-
-
 
 end
