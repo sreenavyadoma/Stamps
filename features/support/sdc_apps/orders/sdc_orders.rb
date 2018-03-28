fe40c0486d9d276a179b28b4212bd977f6b23acc
Rob Cruz <robert.cruz@gmail.com>
2018-03-27 21:24:00 -0700

    ORDERSAUTO-3994 Spike: iOS Automation

--- features/support/sdc_apps/orders/sdc_orders.rb
+++ features/support/sdc_apps/orders/sdc_orders.rb
@@ -2,13 +2,29 @@ module Stamps
   module Orders
     class SdcOrders < SdcPageObject
       element(:loading_orders) { SdcElement.new(browser.div(text: 'Loading orders...')) } # this stays here
-      element(:signed_in_user) {SdcElement.new(browser.span(id: 'userNameText'))} # this will need to move
+      element(:signed_in_user, required: true) {SdcElement.new(browser.span(id: 'userNameText'))} # this will need to move to Navigation > Signed-in Dropdown
 
       def order_details
 
       end
 
+      def navigation
+
+      end
+
       def toolbar
+
+      end
+
+      def orders_grid
+
+      end
+
+      def left_filter_panel
+
+      end
+
+      def paging_toolbar
 
       end
     end
