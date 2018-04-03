0d042f149a5b12cafa7ea99f1d5a75a4c5b22acd
Rob Cruz <robert.cruz@gmail.com>
2018-04-03 13:51:31 -0700

    ORDERSAUTO-3994 Spike: iOS Automation

--- features/step_definitions/common/web_apps_steps.rb
+++ features/step_definitions/common/web_apps_steps.rb
@@ -21,7 +21,7 @@ end
 
 Then /^sign-in to Orders as (.+), (.+)$/ do |usr, pw|
   SdcWebsite.landing_page.sign_in_with(TestData.store[:username] = usr, TestData.store[:password] = pw)
-  SdcWebsite.orders.signed_in_user.safe_wait_until_present(timeout: 10) if SdcEnv.browser
+  SdcWebsite.orders.signed_in_user.safe_wait_until_present(timeout: 10)
   expect(SdcWebsite.orders.signed_in_user.text).to eql(TestData.store[:username])
 end
 
