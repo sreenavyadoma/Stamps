8f285b31b4caffc7de118cd20868829561701b0c
Rob Cruz <robert.cruz@gmail.com>
2018-04-04 11:54:55 -0700

    ORDERSAUTO-3994 Spike: iOS Automation

--- features/support/sdc_apps/sdc_navigation.rb
+++ features/support/sdc_apps/sdc_navigation.rb
@@ -1,16 +1,16 @@
-module Stamps
-  class SignedInUser
-    element(:signed_in_user) { {id: 'userNameText'} }
-    element(:sign_out) { {id: "signOutLink"} }
-
-    def method_missing(method, *args, &block)
-      super unless signed_in_user.respond_to? method
-    end
-  end
-
-  class SdcNavigation
-    def user_drop_down
-
-    end
-  end
-end
+# module Stamps
+#   class SignedInUser
+#     #element(:signed_in_user) { {id: 'userNameText'} }
+#     element(:sign_out) { {id: "signOutLink"} }
+#
+#     def method_missing(method, *args, &block)
+#       super unless signed_in_user.respond_to? method
+#     end
+#   end
+#
+#   class SdcNavigation
+#     def user_drop_down
+#
+#     end
+#   end
+# end
