e76224bf42395641a7ee672310df2ad9d6ab927b
Rob Cruz <robert.cruz@gmail.com>
2018-04-04 09:46:19 -0700

    ORDERSAUTO-3994 Spike: iOS Automation

--- features/support/sdc_apps/orders/sdc_landing_page.rb
+++ features/support/sdc_apps/orders/sdc_landing_page.rb
@@ -1,12 +1,29 @@
 module Stamps
   module Orders
+    module LandPageInstHelper
+      def visit_env
+        case SdcEnv.env
+          when :qacc
+            'ext.qacc'
+          when :qasc
+            'ext.qasc'
+          when :stg
+            '.testing'
+          when :prod
+            ''
+          else
+            # ignore
+        end
+      end
+    end
 
     class LandingPage < SdcPageObject
+      include LandPageInstHelper
+
       text_field(:username, tag_name: :text_field) { {xpath: "//input[@placeholder='USERNAME']"} }
       text_field(:password, tag_name: :text_field) { {xpath: "//input[@placeholder='PASSWORD']"} }
       button(:sign_in, required: true) { {xpath: "//span[contains(text(), 'Sign In')]"} }
 
-      # Checkbox
       element(:rm_checkbox) { {xpath: "//*[contains(@class, 'remember-username-checkbox')]//span[contains(@id, 'displayEl')]"} }
       element(:rm_verify) { {xpath: "//*[contains(@class, 'remember-username-checkbox')]"} }
       checkbox(:remember_me, :rm_checkbox, :rm_verify, "class", "checked")
@@ -14,26 +31,10 @@ module Stamps
       page_url { |env| "https://print#{env}.stamps.com/SignIn/Default.aspx?env=Orders&" }
 
       def self.visit
-        super(case SdcEnv.env
-                when :qacc
-                  'ext.qacc'
-                when :qasc
-                  'ext.qasc'
-                when :stg
-                  '.testing'
-                when :prod
-                  ''
-                else
-                  # ignore
-              end)
+        super(visit_env)
       end
 
       def sign_in_with(usr, pwd)
-        SdcLog.info remember_me.checked?
-        remember_me.check
-        SdcLog.info remember_me.checked?
-        remember_me.uncheck
-        SdcLog.info remember_me.checked?
         username.set(usr)
         password.set(pwd)
         sign_in.safe_click(ctr: 2).send_keys_while_present(:enter, ctr: 2)
