62389a803cbfc8a7e460be5f84b9a211e73256b0
Rob Cruz <robert.cruz@gmail.com>
2018-04-06 08:00:18 -0700

    ORDERSAUTO-3994 Spike: iOS Automation

--- features/step_definitions/common/web_apps_steps.rb
+++ features/step_definitions/common/web_apps_steps.rb
@@ -25,6 +25,8 @@ end
 
 Then /^sign-in to Orders as (.+), (.+)$/ do |usr, pw|
   SdcWebsite.landing_page.sign_in_with(TestData.store[:username] = usr, TestData.store[:password] = pw)
+  SdcWebsite.orders.loading_orders.safe_wait_until_present if SdcEnv.browser
+  SdcWebsite.orders.loading_orders.safe_wait_while_present if SdcEnv.browser
   SdcWebsite.navigation.user_drop_down.signed_in_user.safe_wait_until_present(timeout: 8)
   expect(SdcWebsite.navigation.user_drop_down.signed_in_user.text).to eql(TestData.store[:username])
 end
--- features/support/sdc_apps/sdc_navigation.rb
+++ features/support/sdc_apps/sdc_navigation.rb
@@ -6,13 +6,8 @@ module Stamps
 
     def sign_out(iter: 3)
       signed_in_user.safe_wait_until_present
-
-      iter.to_i.times do
-        signed_in_user.hover unless sign_out_link.present?
-        sign_out_link.safe_click
-        sign_out_link.safe_send_keys(:enter)
-        break unless signed_in_user.present?
-      end
+      signed_in_user.hover unless sign_out_link.present?
+      sign_out_link.safe_click
     end
   end
 
