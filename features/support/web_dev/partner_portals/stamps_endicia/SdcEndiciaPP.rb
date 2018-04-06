a9a543348b2cfeabcfe4602abec2c1da914153b0
mhossain <mhossain@stamps.com>
2018-04-06 08:11:08 -0700

    mobile

--- features/support/web_dev/partner_portals/stamps_endicia/SdcEndiciaPP.rb
+++ features/support/web_dev/partner_portals/stamps_endicia/SdcEndiciaPP.rb
@@ -4,16 +4,6 @@ module Stamps
       module StampsEndicia
         class SdcEndiciaLanding < SdcPageObject
           page_url { |env| "https://#{env}.partner.stamps.com" }
-
-          # create sections aka classes having elements
-            class LoginPage
-              # LoginPage elements
-              text_field(:username, :text_field, required: true) { {xpath: "xpath "} }
-              text_field(:password, :text_field, required: true) { {xpath: "xpath "} }
-              button(:sign_in, required: true) { {xpath: "xpath "} }
-              link(:forgot_pw, required: true) {{id: "xxx"}}
-            end
-
           def self.visit
             super(case SdcEnv.env
                     when :qacc
@@ -28,17 +18,6 @@ module Stamps
                       # ignore
                   end)
           end
-
-          def sign_in_with(usr, pw)
-            username.set usr
-            username.set pw
-            sign_in.click
-            sign_in.send_keys :enter
-            username.safe_wait_while_present
-          end
-        end
-
-        class SdcEndiciaPort < SdcPageObject
         end
       end
     end
