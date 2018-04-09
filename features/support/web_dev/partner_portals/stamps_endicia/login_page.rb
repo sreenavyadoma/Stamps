6722fb95267e652eebce040e8c115211300baa12
Rob Cruz <robert.cruz@gmail.com>
2018-04-09 12:25:15 -0700

    ORDERSAUTO-3994 Spike: iOS Automation

--- features/support/web_dev/partner_portals/stamps_endicia/login_page.rb
+++ features/support/web_dev/partner_portals/stamps_endicia/login_page.rb
@@ -3,59 +3,77 @@ module Stamps
     module Portals
       module StampsEndicia
         module Fields
-            class Email < SdcPageObject
-              text_field(:username, tag_name: :text_field, required: true) { {id: "username"} }
-              element(:tooltip, required: true) { {xpath: "xpath "} }
-            end
+          class SurveyDropDown
+            element(:dd, tag_name: :text_field, required: true) { {id: "usageType"} }
 
-            class Password < SdcPageObject
-              text_field(:password, tag_name: :text_field, required: true) { {id: "password "} }
-              element(:tooltip, required: true) { {xpath: "xpath "} }
-            end
+            # Business Use - Mostly mailing (letters/postcards/flats)
+            def select(str, iter: 3)
+              # create the drop-down element
+              self.class.element(:selection) { {xpath: "//span[contains(text(), ' #{str} ')]"} }
 
-            class LogIn < SdcPageObject
-              button(:log_in, required: true) { {xpath: "xpath "} }
-            end
+              iter.to_i.times do
+                selection.present? unless dd.safe_click
+                selection.safe_click
+                break if dd.text_value.include str
+              end
 
-            class ForgotPassowrd < SdcPageObject
-              link(:forgot_pw, required: true) {{id: "forgotpassword"}}
-              element(:tooltip, required: true) { {xpath: "xpath "} }
+              dd.text_value
             end
-         end
+          end
 
-          class LoginPage < SdcPageObject
+          class Email < SdcPageObject
             text_field(:username, tag_name: :text_field, required: true) { {id: "username"} }
-            element(:username_tooltip, required: true) { {xpath: "xpath "} }
+            element(:tooltip, required: true) { {xpath: "xpath "} }
+          end
 
+          class Password < SdcPageObject
             text_field(:password, tag_name: :text_field, required: true) { {id: "password "} }
             element(:tooltip, required: true) { {xpath: "xpath "} }
+          end
 
-            def email
-              @email ||= Fields::Email.new()
-            end
+          class LogIn < SdcPageObject
+            button(:log_in, required: true) { {xpath: "xpath "} }
+          end
+
+          class ForgotPassowrd < SdcPageObject
+            link(:forgot_pw, required: true) {{id: "forgotpassword"}}
+            element(:tooltip, required: true) { {xpath: "xpath "} }
+          end
+        end
+
+        class LoginPage < SdcPageObject
+          text_field(:username, tag_name: :text_field, required: true) { {id: "username"} }
+          element(:username_tooltip, required: true) { {xpath: "xpath "} }
+
+          text_field(:password, tag_name: :text_field, required: true) { {id: "password "} }
+          element(:tooltip, required: true) { {xpath: "xpath "} }
+
+          def email
+            @email ||= Fields::Email.new()
+          end
 # login_page.password.set ''
 # login_page.username.set ''
 # login_page.sign_in.click
-            def password
-              Fields::Password.new()
-            end
+          def password
+            Fields::Password.new()
+          end
 
-            def forgot_password
-              Fields::ForgotPassowrd.new()
-            end
+          def forgot_password
+            Fields::ForgotPassowrd.new()
+          end
 
-            def login
-              Fields::LogIn.new()
-            end
+          def login
+            Fields::LogIn.new()
+          end
 
 
-            def sign_in_with(usr, pw)
-              self.username.set usr
-              username.set pw
-              log_in.click
-              log_in.send_keys :enter
-              username.safe_wait_while_present
-            end
+          def sign_in_with(usr, pw)
+            self.username.set usr
+            username.set pw
+            log_in.click
+            log_in.send_keys :enter
+            username.safe_wait_while_present
+          end
         end
       end
     end
