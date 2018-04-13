494b0a2c5878c35786bb832d7e0c0b2f7c8cef2b
Rob Cruz <robert.cruz@gmail.com>
2018-04-13 11:00:31 -0700

    Fixed sauce key

--- features/step_definitions/common/web_apps_steps.rb
+++ features/step_definitions/common/web_apps_steps.rb
@@ -30,6 +30,7 @@ Then /^sign-in to Orders as (.+), (.+)$/ do |usr, pw|
     SdcWebsite.navigation.user_drop_down.signed_in_user.safe_wait_until_present(timeout: 5)
     #expect(SdcWebsite.navigation.user_drop_down.signed_in_user.text_value).to include(TestData.store[:username])
   end
+  sleep 3
 end
 
 Then /^sign-in to Mail as (.+), (.+)$/ do |usr, pw|
