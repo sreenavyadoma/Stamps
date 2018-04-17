02316dca9431fc091149d5d4046637d48a69ae69
Rob Cruz <robert.cruz@gmail.com>
2018-04-17 09:54:29 -0700

    Browser, Android and IOS are all working v1

--- features/support/sdc_apps/orders/sdc_landing_page.rb
+++ features/support/sdc_apps/orders/sdc_landing_page.rb
@@ -5,9 +5,6 @@ module Stamps
     page_obj(:username, tag: :text_field) { {xpath: "//input[@placeholder='USERNAME']"} }
     page_obj(:password, tag: :text_field) { {xpath: "//input[@placeholder='PASSWORD']"} }
     page_obj(:sign_in) { {xpath: "//span[contains(text(), 'Sign In')]"} }
-    page_objs(:sign_ins) { {xpath: "//span[contains(text(), 'Sign In')]"} }
-    page_objs(:sign_in_elem) { {xpath: "//span[contains(text(), 'Sign In')]"} }
-    page_objs_index(:textbox, index: 0) { :sign_in_elem }
 
     page_url { |env| "https://print#{env}.stamps.com/SignIn/Default.aspx?env=Orders&" }
 
