8fb25dc36c1b5d9c3e130d3b6fbdcea352e62b8a
mhossain <mhossain@stamps.com>
2018-04-17 09:55:45 -0700

    mobile

--- features/support/web_dev/stamps_endicia/partner_portals/reset_password_page.rb
+++ features/support/web_dev/stamps_endicia/partner_portals/reset_password_page.rb
@@ -4,7 +4,15 @@ module Stamps
 
       class ResetPasswordPage < SdcPage
         #sdcEndica content
-        page_obj(:header, required: true) {{xpath: "//h1['Password Reset']"}}
+        page_obj(:header) {{xpath: "//h1['Password Reset']"}}
+
+        #Email
+        text_field(:email, tag: :text_field, required: true) {{xpath: "//input[@name='email']"}}
+
+        #submit
+        button(:submit) {{xpath: "//button[@label='Submit']/span" }}
+
+
 
       end
 
