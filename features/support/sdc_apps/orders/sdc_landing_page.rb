459767be24a263fa2cf001bf2b99373cc8aafaf5
Rob Cruz <robert.cruz@gmail.com>
2018-04-17 09:36:15 -0700

    Browser, Android and IOS are all working v1

--- features/support/sdc_apps/orders/sdc_landing_page.rb
+++ features/support/sdc_apps/orders/sdc_landing_page.rb
@@ -5,6 +5,9 @@ module Stamps
     page_obj(:username, tag: :text_field) { {xpath: "//input[@placeholder='USERNAME']"} }
     page_obj(:password, tag: :text_field) { {xpath: "//input[@placeholder='PASSWORD']"} }
     page_obj(:sign_in) { {xpath: "//span[contains(text(), 'Sign In')]"} }
+    page_objs(:sign_ins) { {xpath: "//span[contains(text(), 'Sign In')]"} }
+    page_objs(:sign_in_elem) { {xpath: "//span[contains(text(), 'Sign In')]"} }
+    _page_object(:textbox) { sign_in_elem[0] }
 
     page_url { |env| "https://print#{env}.stamps.com/SignIn/Default.aspx?env=Orders&" }
 
--- features/support/sdc_core/sdc_core.rb
+++ features/support/sdc_core/sdc_core.rb
@@ -65,6 +65,10 @@ module Stamps
       alias_method :selection, :page_obj
       alias_method :link, :page_obj
 
+      def page_obj_wrapper(name, required: false)
+        _page_object(name, required: required) { SdcElement.new(yield) }
+      end
+
       def page_objs(name, tag: nil, timeout: 30, &block)
         _page_objects(name) { SdcFinder.elements(browser, tag: tag, timeout: timeout, &block) }
       end
