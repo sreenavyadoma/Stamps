b3b8a44c1f11b7927d9f861629e06b81fdd4a15a
Rob Cruz <robert.cruz@gmail.com>
2018-04-19 14:06:18 -0700

    Browser, Android and IOS are all working v1

--- features/support/sdc_apps/mail/sdc_mail.rb
+++ features/support/sdc_apps/mail/sdc_mail.rb
@@ -1,34 +1,14 @@
 module Stamps
-  module CommonNavigation
-    def mail
 
-    end
-
-    def orders
-
-    end
-
-    def contacts
-
-    end
-
-    def history
-
-    end
-  end
-
-  module Mail
+  module SdcMail
     class SignInLink < SdcPage
+      page_url { |env| "https://print#{env}.stamps.com/SignIn/Default.aspx?env=Orders&" }
 
-      def sign_in_with(usr, pw)
 
-      end
     end
 
     class SdcMail < SdcPage
       include CommonNavigation
-
-      page_url { |env| "https://print#{env}.stamps.com/SignIn/Default.aspx?env=Orders&" }
 
       def sign_in_link
         # sign-in modal
