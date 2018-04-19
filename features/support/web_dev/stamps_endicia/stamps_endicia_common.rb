25e3706fd7d139c00f317b2c6125146f7670ab72
Rob Cruz <robert.cruz@gmail.com>
2018-04-19 08:19:53 -0700

    Browser, Android and IOS are all working v1

--- features/support/web_dev/stamps_endicia/stamps_endicia_common.rb
+++ features/support/web_dev/stamps_endicia/stamps_endicia_common.rb
@@ -2,9 +2,13 @@ module Stamps
   module StampsEndicia
     class Common < SdcPage
       #stamps and endicia logo
-      page_objs(:sdc_endicia_logos)  {{xpath: "//img[@alt='Stamps.com and Endicia']"}}
-      page_objs_index(:sdc_endicia_logo_browser, index: 0) {:sdc_endicia_logos}
-      page_objs_index(:sdc_endicia_logo_mobile, index: 0) {:sdc_endicia_logos}
+      #page_objs(:sdc_endicia_logos)  { {xpath: "//img[@alt='Stamps.com and Endicia']"} }
+      #page_objs_index(:sdc_endicia_logo_browser, index: 0) {:sdc_endicia_logos}
+      #page_objs_index(:sdc_endicia_logo_mobile, index: 0) {:sdc_endicia_logos}
+
+
+      page_objs(:sdc_endicia_logo_browser, index: 0)  { {xpath: "//img[@alt='Stamps.com and Endicia']"} }
+      page_objs(:sdc_endicia_logo_mobile, index: 0)  { {xpath: "//img[@alt='Stamps.com and Endicia']"} }
     end
 
   end
