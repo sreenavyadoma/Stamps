405dc36c225a37fb27ca339178d123a82fbc709b
Rob Cruz <robert.cruz@gmail.com>
2018-04-20 08:31:24 -0700

    Browser, Android and IOS are all working v1

--- features/step_definitions/common/sdc_web_apps_steps.rb
+++ features/step_definitions/common/sdc_web_apps_steps.rb
@@ -51,21 +51,25 @@ Then /^sign-in to Orders(?: as (.+), (.+)|)$/ do |usr, pw|
   landing_page.username.set(TestData.store[:username] = usr)
   landing_page.password.set(TestData.store[:password] = pw)
   if SdcEnv.browser
-    5.to_i.times do
-      begin
-        landing_page.sign_in.click
-        landing_page.sign_in.click
-        landing_page.sign_in.safe_click
-        landing_page.sign_in.send_keys(:enter)
-        landing_page.sign_in.send_keys(:enter)
-        #sign_in.send_keys_while_present(:enter, ctr: 2)
-        #sign_in.safe_wait_while_present
-        break if signed_in_user.present?
-      rescue
-        # ignore
+    if SdcEnv.sauce_device
+      landing_page.sign_in.click
+      landing_page.sign_in.send_keys(:enter)
+    else
+      5.to_i.times do
+        begin
+          landing_page.sign_in.click
+          landing_page.sign_in.click
+          landing_page.sign_in.safe_click
+          landing_page.sign_in.send_keys(:enter)
+          landing_page.sign_in.send_keys(:enter)
+          #sign_in.send_keys_while_present(:enter, ctr: 2)
+          #sign_in.safe_wait_while_present
+          break if signed_in_user.present?
+        rescue
+          # ignore
+        end
       end
     end
-    sleep(10)
     SdcWebsite.orders.loading_popup.safe_wait_until_present(timeout: 5)
     SdcWebsite.orders.loading_popup.safe_wait_while_present(timeout: 5)
     SdcWebsite.navigation.user_drop_down.signed_in_user.safe_wait_until_present(timeout: 5)
