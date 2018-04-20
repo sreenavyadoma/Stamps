d5470be1b550941da102387b762c13df67510821
Rob Cruz <robert.cruz@gmail.com>
2018-04-20 09:42:28 -0700

    Browser, Android and IOS are all working v1

--- features/support/sdc_core/sdc_test.rb
+++ features/support/sdc_core/sdc_test.rb
@@ -346,28 +346,31 @@ class SdcTest
     end
 
     def teardown
-      SdcPage.browser.quit
-      # begin
-      #
-      # #
-      # #   if SdcEnv.sauce_device
-      # #     sessionid = SdcPage.browser.send(:bridge).session_id
-      # #     jobname = "#{scenario.feature.name} - #{scenario.name}"
-      # #     if scenario.passed?
-      # #       SauceWhisk::Jobs.pass_job sessionid
-      # #     else
-      # #       SauceWhisk::Jobs.fail_job sessionid
-      # #     end
-      # #
-      # #     SdcLog.info "SauceOnDemandSessionID=#{sessionid} job-name=#{jobname}"
-      # #   end
-      # #
-      # #
-      # # rescue
-      # #   # ignore
-      # # end
-      #
-      # SdcUserCredentials.close
+      begin
+
+        SdcPage.browser.quit
+
+        if SdcEnv.sauce_device
+          sessionid = SdcPage.browser.send(:bridge).session_id
+          jobname = "#{scenario.feature.name} - #{scenario.name}"
+          if scenario.passed?
+            SauceWhisk::Jobs.pass_job sessionid
+          else
+            SauceWhisk::Jobs.fail_job sessionid
+          end
+
+          SdcLog.info "SauceOnDemandSessionID=#{sessionid} job-name=#{jobname}"
+        else
+
+          SdcUserCredentials.close
+
+          SdcLog.info "#{SdcPage.browser} closed."
+        end
+
+
+      rescue
+        # ignore
+      end
     end
 
     private
