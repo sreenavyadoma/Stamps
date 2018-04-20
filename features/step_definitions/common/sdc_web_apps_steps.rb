2ae783eff04d3f2634e1e6b4ee62f7fe21125bdf
Rob Cruz <robert.cruz@gmail.com>
2018-04-20 08:41:14 -0700

    Browser, Android and IOS are all working v1

--- features/step_definitions/common/sdc_web_apps_steps.rb
+++ features/step_definitions/common/sdc_web_apps_steps.rb
@@ -70,9 +70,9 @@ Then /^sign-in to Orders(?: as (.+), (.+)|)$/ do |usr, pw|
           # ignore
         end
       end
+      SdcWebsite.orders.loading_popup.safe_wait_until_present(timeout: 5)
+      SdcWebsite.orders.loading_popup.safe_wait_while_present(timeout: 5)
     end
-    SdcWebsite.orders.loading_popup.safe_wait_until_present(timeout: 5)
-    SdcWebsite.orders.loading_popup.safe_wait_while_present(timeout: 5)
     SdcWebsite.navigation.user_drop_down.signed_in_user.safe_wait_until_present(timeout: 5)
     expect(SdcWebsite.navigation.user_drop_down.signed_in_user.text_value).to include(TestData.store[:username])
 
@@ -80,7 +80,7 @@ Then /^sign-in to Orders(?: as (.+), (.+)|)$/ do |usr, pw|
     begin
       landing_page.sign_in.click
       landing_page.sign_in.send_keys(:enter)
-      landing_page.sign_in.safe_send_keys(:enter)
+      #landing_page.sign_in.safe_send_keys(:enter)
     rescue
       # ignore
     end
