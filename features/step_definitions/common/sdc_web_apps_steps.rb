59aa5fac49b87536ad50b84786513c1f32e6115f
Rob Cruz <robert.cruz@gmail.com>
2018-04-20 11:50:50 -0700

    Browser, Android and IOS are all working v1

--- features/step_definitions/common/sdc_web_apps_steps.rb
+++ features/step_definitions/common/sdc_web_apps_steps.rb
@@ -54,6 +54,7 @@ Then /^sign-in to Orders(?: as (.+), (.+)|)$/ do |usr, pw|
     if SdcEnv.sauce_device
       landing_page.sign_in.click
       SdcWebsite.navigation.user_drop_down.signed_in_user.safe_wait_until_present(timeout: 10)
+      sleep 5
     else
       5.to_i.times do
         begin
