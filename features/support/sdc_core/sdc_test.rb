0ed5d9b0ddc4131ab509b139818885f1bfcec079
Rob Cruz <robert.cruz@gmail.com>
2018-04-20 10:03:58 -0700

    Browser, Android and IOS are all working v1

--- features/support/sdc_core/sdc_test.rb
+++ features/support/sdc_core/sdc_test.rb
@@ -350,22 +350,22 @@ class SdcTest
 
         SdcPage.browser.quit
 
-        if SdcEnv.sauce_device
-          sessionid = SdcPage.browser.send(:bridge).session_id
-          jobname = "#{scenario.feature.name} - #{scenario.name}"
-          if scenario.passed?
-            SauceWhisk::Jobs.pass_job sessionid
-          else
-            SauceWhisk::Jobs.fail_job sessionid
-          end
-
-          SdcLog.info "SauceOnDemandSessionID=#{sessionid} job-name=#{jobname}"
-        else
-
-          SdcUserCredentials.close
-
-          SdcLog.info "#{SdcPage.browser} closed."
-        end
+        # if SdcEnv.sauce_device
+        #   sessionid = SdcPage.browser.send(:bridge).session_id
+        #   jobname = "#{scenario.feature.name} - #{scenario.name}"
+        #   if scenario.passed?
+        #     SauceWhisk::Jobs.pass_job sessionid
+        #   else
+        #     SauceWhisk::Jobs.fail_job sessionid
+        #   end
+        #
+        #   SdcLog.info "SauceOnDemandSessionID=#{sessionid} job-name=#{jobname}"
+        # else
+        #
+        #   SdcUserCredentials.close
+        #
+        #   SdcLog.info "#{SdcPage.browser} closed."
+        # end
 
 
       rescue
