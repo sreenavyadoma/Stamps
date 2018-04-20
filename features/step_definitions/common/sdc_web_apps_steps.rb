4775ea6b1383e953676816d7a5abb1a370ca6358
Rob Cruz <robert.cruz@gmail.com>
2018-04-20 10:49:10 -0700

    Browser, Android and IOS are all working v1

--- features/step_definitions/common/sdc_web_apps_steps.rb
+++ features/step_definitions/common/sdc_web_apps_steps.rb
@@ -53,7 +53,7 @@ Then /^sign-in to Orders(?: as (.+), (.+)|)$/ do |usr, pw|
   if SdcEnv.browser
     if SdcEnv.sauce_device
       landing_page.sign_in.click
-      landing_page.sign_in.safe_send_keys(:enter)
+      landing_page.sign_in.send_keys(:enter)
       sleep 2
     else
       5.to_i.times do
