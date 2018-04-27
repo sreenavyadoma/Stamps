7b70c4ac3993af377de6c9da17b4a347f6cacddf
mhossain <mhossain@stamps.com>
2018-04-27 15:20:33 -0700

    PP UI validation

--- features/step_definitions/web_dev/stamps_endicia/partner_portals/login_page_steps.rb
+++ features/step_definitions/web_dev/stamps_endicia/partner_portals/login_page_steps.rb
@@ -115,7 +115,7 @@ Then /^[Pp]P: [Ee]xpect login page [Pp]assword tooltip index (\d+) to be (.*)$/ 
 end
 
 Then /^[Pp]P: [Ee]xpect login page error message to be$/ do |str|
-  expect(PartnerPortal.login_page.error_message.text).to eql(str)
+  expect(PartnerPortal.login_page.error_message.text.strip).to eql(str)
 end
 
 
