2d24fe7533ad3d17acaf95edaa3f4a003adececf
Rob Cruz <robert.cruz@gmail.com>
2018-04-30 11:07:32 -0700

    Implemented LOG_LEVEL

--- features/step_definitions/common/sdc_web_apps_steps.rb
+++ features/step_definitions/common/sdc_web_apps_steps.rb
@@ -156,7 +156,7 @@ Then /^sign-in to Orders(?: as (.+), (.+)|)$/ do |usr, pw|
       SdcWebsite.orders.loading_popup.safe_wait_until_present(timeout: 5)
       SdcWebsite.orders.loading_popup.safe_wait_while_present(timeout: 5)
       SdcWebsite.navigation.user_drop_down.signed_in_user.safe_wait_until_present(timeout: 5)
-      expect(SdcWebsite.navigation.user_drop_down.signed_in_user.text_value).to eql(TestData.store[:username])
+      expect(SdcWebsite.navigation.user_drop_down.signed_in_user.text_value).to eql(usr)
     end
 
   elsif SdcEnv.ios
