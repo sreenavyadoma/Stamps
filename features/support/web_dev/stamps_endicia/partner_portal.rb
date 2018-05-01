bdf585dbe3c5dc68d0c230528344ddb4fd60bd67
mhossain <mhossain@stamps.com>
2018-05-01 12:16:22 -0700

    Login Update

--- features/support/web_dev/stamps_endicia/partner_portal.rb
+++ features/support/web_dev/stamps_endicia/partner_portal.rb
@@ -55,7 +55,10 @@ module PartnerPortal
 
   class PPDashboardPage < SdcPage
     #welcome header
-    page_object(:dashboard_header) { {class: "dashboard__header"} }
+    page_object(:dashboard_header) { {xpath: "//h1[contains(text(), 'Dashboard')]"} }
+
+    #Contract header
+    page_object(:contract_header) { {class: "dashboard__contract-header"} }
 
     #hamburger button
     button(:hamburger, required: true) { {class: "navbar-toggle collapsed"} }
