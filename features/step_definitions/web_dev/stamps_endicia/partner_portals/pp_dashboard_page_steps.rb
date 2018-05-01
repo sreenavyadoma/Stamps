c6ae9521437ab5eb3d54c1ba92f4484e008c3f8f
mhossain <mhossain@stamps.com>
2018-05-01 15:56:55 -0700

    Login Update

--- features/step_definitions/web_dev/stamps_endicia/partner_portals/pp_dashboard_page_steps.rb
+++ features/step_definitions/web_dev/stamps_endicia/partner_portals/pp_dashboard_page_steps.rb
@@ -1,30 +1,4 @@
 
-Then /^[Pp]P: set dashboard page to small viewport$/ do
-  if SdcEnv.browser
-    SdcDriver.driver.window.resize_to(320, 480)
-  else
-    #ignore
-  end
-end
-
-Then /^[Pp]P: set dashboard page to medium viewport$/ do
-  if SdcEnv.browser
-    SdcDriver.driver.window.resize_to(768, 1024)
-  else
-    #ignore
-  end
-end
-
-Then /^[Pp]P: set dashboard page to large viewport$/ do
-  if SdcEnv.browser
-    SdcDriver.driver.window.resize_to(1920, 1080)
-     $SdcPage.browser.window.resize_to(1920, 1080)
-  else
-    #ignore
-  end
-end
-
-
 Then /^[Pp]P: [Ee]xpect user is redirected to the Dashboard Page$/ do
    PartnerPortal.dashboard_page.dashboard_header.wait_until_present(timeout: 30)
    expect(PartnerPortal.dashboard_page.dashboard_header). to be_present, "User was not redirected to the dashboard page"
--- features/support/web_dev/stamps_endicia/pp_common.rb
+++ features/support/web_dev/stamps_endicia/pp_common.rb
@@ -1,7 +1,7 @@
   module PartnerPortal
     class Common < SdcPage
-      page_objects(:sdc_endicia_logo_browser, index: 0)  { {xpath: "//img[@alt='Stamps.com and Endicia']"} }
-      page_objects(:sdc_endicia_logo_mobile, index: 1)  { {xpath: "//img[@alt='Stamps.com and Endicia']"} }
+      page_objects(:sdc_endicia_logo_browser, index: 1)  { {xpath: "//img[@alt='Stamps.com Endicia | Partner Portal logo']"} }
+      page_objects(:sdc_endicia_logo_mobile, index: 0)  { {xpath: "//img[@alt='Stamps.com Endicia | Partner Portal logo']"} }
     end
 
   end
