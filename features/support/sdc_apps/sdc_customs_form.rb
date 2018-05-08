c5aeefaad8f948d0fe82b9cd5773fda1deb69e3a
abaranovskyi <nhfdrf1989ptktyttn!>
2018-05-07 18:00:59 -0700

    ORDERSAUTO-4038 - New Framework: Orders International BVT

--- features/support/sdc_apps/sdc_customs_form.rb
+++ features/support/sdc_apps/sdc_customs_form.rb
@@ -1,19 +1,21 @@
 module SdcWebsite
   class SdcCustomsForm < SdcPage
+    page_object(:text_field) { {xpath: '//*[contains(@class, "x-toolbar-white")]//*[contains(@id, "btnWrap")]'} }
+
     def package_contents
     end
 
     def non_delivery
-
     end
 
     def internal_transaction
-
     end
 
     def item(num)
+      self.class.page_object(:item_description) { {xpath: "(//*[contains(@id, 'singlecustomsitem')]//*[@name='Description'])[#{num}]"} }
+      self.class.page_object(:hs_tariff) { {xpath: "(//*[contains(@id, 'singlecustomsitem')]//*[@name='TariffNo'])[#{num}]"} }
 
+      self.class.page_object(:delete) { {xpath: "(//*[contains(@id, 'singlecustomsitem')]//a[contains(@class, 'x-btn-transparent-small')])[#{num}]"} }
     end
-
   end
 end
