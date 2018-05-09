6c337877457a265a18654f643d0593d00448ab95
mhossain <mhossain@stamps.com>
2018-05-09 09:05:16 -0700

    Browser Emulator

--- features/step_definitions/web_dev/stamps_endicia/partner_portals/pp_dashboard_page_steps.rb
+++ features/step_definitions/web_dev/stamps_endicia/partner_portals/pp_dashboard_page_steps.rb
@@ -1,7 +1,7 @@
 
 Then /^[Pp]P: [Ee]xpect [Dd]ashboard [Pp]age header exist$/ do
    PartnerPortal.dashboard_page.dashboard_header.wait_until_present(timeout: 30)
-   expect(PartnerPortal.dashboard_page.dashboard_header). to be_present, 'User was not redirected to the dashboard page'
+   expect(PartnerPortal.dashboard_page.dashboard_header). to be_present, 'Dashboard header DOES NOT exist on dashboard page'
 end
 
 Then /^[Pp]P: expect Preferred Rates Qualified Postage to exists$/ do
