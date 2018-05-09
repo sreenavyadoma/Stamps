58bff2e04b1f810e5dd99c902fd00235aa3967c2
mhossain <mhossain@stamps.com>
2018-05-09 09:02:49 -0700

    Browser Emulator

--- features/support/sdc_core/sdc_core.rb
+++ features/support/sdc_core/sdc_core.rb
@@ -2,7 +2,7 @@
 module SdcEnv
   TEST_ENVIRONMENTS = %i[stg qacc cc qasc sc rating].freeze unless Object.const_defined?('SdcEnv::TEST_ENVIRONMENTS')
   BROWSERS = %i[ff firefox gc chrome safari edge chromeb ie iexplorer].freeze unless Object.const_defined?('SdcEnv::BROWSERS')
-  BROWSER_MOBILE_EMULATORS = ['iPhone X' 'iPhone 4'].freeze unless Object.const_defined?('SdcEnv::BROWSER_MOBILE_EMULATORS')
+  BROWSER_MOBILE_EMULATORS = ['iPhone X', 'iPhone 4', 'Pixel 2', 'Pixel 2 XL'].freeze unless Object.const_defined?('SdcEnv::BROWSER_MOBILE_EMULATORS')
   HEALTH_CHECK_APPS = ['address book', 'orders', 'or reports', 'postage tools'].freeze unless Object.const_defined?('SdcEnv::HEALTH_CHECK_APPS')
   IOS = %i[iphone6 iphone7 iphone8 iphonex].freeze unless Object.const_defined?('SdcEnv::IOS')
   ANDROID = %i[samsung_galaxy nexus_5x].freeze unless Object.const_defined?('SdcEnv::ANDROID')
