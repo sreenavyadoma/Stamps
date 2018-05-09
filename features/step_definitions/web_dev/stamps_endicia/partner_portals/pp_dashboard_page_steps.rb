cb7af6b3cefe5c5736746d071777d8cf0d9420ec
mhossain <mhossain@stamps.com>
2018-05-09 09:04:36 -0700

    Browser Emulator

--- features/step_definitions/web_dev/stamps_endicia/partner_portals/pp_dashboard_page_steps.rb
+++ features/step_definitions/web_dev/stamps_endicia/partner_portals/pp_dashboard_page_steps.rb
@@ -1,13 +1,13 @@
 
 Then /^[Pp]P: [Ee]xpect [Dd]ashboard [Pp]age header exist$/ do
    PartnerPortal.dashboard_page.dashboard_header.wait_until_present(timeout: 30)
-   expect(PartnerPortal.dashboard_page.dashboard_header). to be_present, "User was not redirected to the dashboard page"
+   expect(PartnerPortal.dashboard_page.dashboard_header). to be_present, 'User was not redirected to the dashboard page'
 end
 
 Then /^[Pp]P: expect Preferred Rates Qualified Postage to exists$/ do
-  expect(PartnerPortal.dashboard_page.preferred_rates_qualified_postage). to be_present "PreFered rates Qualified Postage canvas DOES NOT exist on dashboard page"
+  expect(PartnerPortal.dashboard_page.preferred_rates_qualified_postage). to be_present 'PreFered rates Qualified Postage canvas DOES NOT exist on dashboard page'
 end
 
 Then /^[Pp]P: expect dashboard header on left panel exists$/ do
-  expect(PartnerPortal.dashboard_page.panel_dashboard). to be_present "Dashboard header on left panel  DOES NOT exist on dashboard page"
+  expect(PartnerPortal.dashboard_page.panel_dashboard). to be_present 'Dashboard header on left panel  DOES NOT exist on dashboard page'
 end
