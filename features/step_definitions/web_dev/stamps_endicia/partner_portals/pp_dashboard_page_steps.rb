4a831c4ef4b41460d1379331fcf4e94e3d47197a
mhossain <mhossain@stamps.com>
2018-05-10 13:00:02 -0700

    Dashboard UI Validation

--- features/step_definitions/web_dev/stamps_endicia/partner_portals/pp_dashboard_page_steps.rb
+++ features/step_definitions/web_dev/stamps_endicia/partner_portals/pp_dashboard_page_steps.rb
@@ -1,16 +1,16 @@
 
 Then /^[Pp]P: [Ee]xpect [Dd]ashboard [Pp]age header exist$/ do
    PartnerPortal.dashboard_page.dashboard_header.wait_until_present(timeout: 10)
-   expect(PartnerPortal.dashboard_page.dashboard_header). to be_present, 'Dashboard header DOES NOT exist on dashboard page'
+   expect(PartnerPortal.dashboard_page.dashboard_header).to be_present, 'Dashboard header DOES NOT exist on dashboard page'
 end
 
 Then /^[Pp]P: expect dashboard header on left panel exists$/ do
-  expect(PartnerPortal.dashboard_page.panel_dashboard). to be_present 'Dashboard header on left panel  DOES NOT exist on dashboard page'
+  expect(PartnerPortal.dashboard_page.panel_dashboard).to be_present 'Dashboard header on left panel  DOES NOT exist on dashboard page'
 end
 
 Then /^[Pp]P: [Ee]xpect [Dd]ashboard [Pp]age the [Cc]ontract header exists$/ do
   PartnerPortal.dashboard_page.contract_header.wait_until_present(timeout: 5)
-  expect(PartnerPortal.dashboard_page.contract_header). to be_present, 'Contract header DOES NOT exist on dashboard page'
+  expect(PartnerPortal.dashboard_page.contract_header).to be_present, 'Contract header DOES NOT exist on dashboard page'
 end
 
 
@@ -23,7 +23,7 @@ Then /^[Pp]P: [Ee]xpect [Dd]ashboard [Pp]age the correct contract is being displ
   TestData.hash[:contract_display_name] = PartnerPortal.common_page.contract_table_query(TestData.hash[:partner_account_id], 'DisplayName')
   step 'Close partner portal db connection'
 
-  expect(TestData.hash[:contract_display_name]). to eql(TestData.hash[:contract_display_name_actual])
+  expect(TestData.hash[:contract_display_name]).to eql(TestData.hash[:contract_display_name_actual])
 end
 
 Then /^[Pp]P: [Ee]xpect [Dd]ashboard [Pp]age the Last updated on: field to be present$/ do
@@ -40,16 +40,20 @@ Then /^[Pp]P: [Ee]xpect [Dd]ashboard [Pp]age the Last updated on: field displays
   TestData.hash[:last_update_on] = PartnerPortal.common_page.contract_table_query(TestData.hash[:partner_account_id], 'DateLastUpdated')
   step 'Close partner portal db connection'
 
-  expect(TestData.hash[:last_update_on].to_date). to eql(TestData.hash[:last_update_on_actual])
+  expect(TestData.hash[:last_update_on].to_date).to eql(TestData.hash[:last_update_on_actual])
 end
 
 Then /^[Pp]P: expect [Dd]ashboard page the Preferred Rates Qualified Transactions USD Chart to exist$/ do
-  expect(PartnerPortal.dashboard_page.preferred_rates_qualified_transactions_usd_chart). to be_present 'Preferred Rates Qualified Transactions USD(S) chart DOES NOT exist on dashboard page'
+  expect(PartnerPortal.dashboard_page.preferred_rates_qualified_transactions_usd_chart).to be_present 'Preferred Rates Qualified Transactions USD(S) chart DOES NOT exist on dashboard page'
 end
 
 
-Then /^PP: expect dashboard page the Preferred Rates Qualified Transactions USD Chart title to be (.*)$/ do |str|
+Then /^[Pp]P: expect [Dd]ashboard page the Preferred Rates Qualified Transactions USD Chart title to be (.*)$/ do |str|
   expect(PartnerPortal.dashboard_page.preferred_rates_qualified_transactions_usd_chart_title.text_value.strip). to eql(str)
+end
+
+Then /^[Pp]P: expect [Dd]ashboard page the Preferred Rates Qualified Transactions USD Chart Y-axis label to be (.*)$/ do |str|
+  expect(PartnerPortal.dashboard_page.preferred_rates_qualified_transactions_usd_y_axis.text_value.strip). to eql(str)
 end
 
 
--- features/support/web_dev/stamps_endicia/partner_portal.rb
+++ features/support/web_dev/stamps_endicia/partner_portal.rb
@@ -65,13 +65,20 @@ module PartnerPortal
     page_objects(:preferred_rates_qualified_transactions_usd_chart, index: 0 ) { {xpath: '//canvas'} }
     page_objects(:preferred_rates_qualified_transactions_packages_chart, index: 1 ) { {xpath: '//canvas'} }
     page_objects(:active_customers_customers_chart, index: 2 ) { {xpath: '//canvas'} }
-    page_objects(:active_customers_customers_usd_chart, index: 3 ) { {xpath: '//canvas'} }
+    page_objects(:revenue_share_chart, index: 3 ) { {xpath: '//canvas'} }
 
     #charts title
     page_objects(:preferred_rates_qualified_transactions_usd_chart_title, index: 0 ) { {xpath: '//h4'} }
     page_objects(:preferred_rates_qualified_transactions_packages_chart_title, index: 1 ) { {xpath: '//h4'} }
     page_objects(:active_customers_customers_chart_title, index: 2 ) { {xpath: '//h4'} }
-    page_objects(:active_customers_customers_usd_chart_title, index: 3 ) { {xpath: '//h4'} }
+    page_objects(:revenue_share_chart_title, index: 3 ) { {xpath: '//h4'} }
+
+    #chart Y-axis
+    page_objects(:preferred_rates_qualified_transactions_usd_y_axis, index: 0 ) { {xpath: '//div[contains(text(), "USD ($)")]'} }
+    page_objects(:revenue_share_chart_y_axis, index: 1 ) { {xpath: '//div[contains(text(), "USD ($)")]'} }
+
+    page_object(:preferred_rates_qualified_transactions_packages_chart_y_axis ) { {xpath: '//div[contains(text(), "# of packages")]'} }
+    page_object(:active_customers_customers_chart ) { {xpath: '//div[contains(text(), "# of customers")]'} }
 
     #submit
     button(:submit) { {xpath: '//button[@label="Submit"]'} }
