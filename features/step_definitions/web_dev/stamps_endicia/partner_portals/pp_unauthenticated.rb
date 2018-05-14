999925535c2081405841d9cdcb42daff2a25c4bf
mhossain <mhossain@stamps.com>
2018-05-14 16:58:06 -0700

    unauntheticated

--- features/step_definitions/web_dev/stamps_endicia/partner_portals/pp_unauthenticated.rb
+++ features/step_definitions/web_dev/stamps_endicia/partner_portals/pp_unauthenticated.rb
@@ -1,9 +1,10 @@
-Then /^[Pp]P: [Dd]e-[Aa]uthenticate (?:user|(.*))$/ do
-  step 'Establish Partner Portal db connection'
+Then /^[Pp]P: [Dd]e-[Aa]uthenticate user$/ do
   PartnerPortal.common_page.user_session_timeout
   SdcPage.browser.refresh
 end
 
 Then /^[Pp]P: [Ee]xpect user is redirect to the login page$/ do
-
+  step 'PP: Expect login page "Welcome to the" content to exist'
+  step 'PP: Expect login page "stamps.com endicia" logo to exist'
+  step 'PP: Expect login page "Partner Portal" content to exist'
 end
