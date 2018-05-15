5e00a282897c2774f5530adec67f08b35608be0c
Rob Cruz <robert.cruz@gmail.com>
2018-05-15 08:44:58 -0700

    ORDERSAUTO-4207 Create Mail page object framework

--- features/support/web_dev/stamps_endicia/pp_common.rb
+++ features/support/web_dev/stamps_endicia/pp_common.rb
@@ -1,39 +1,39 @@
 module PartnerPortal
   class Common < SdcPage
     #Global header
-    page_object(:global_header) { {xpath: '//a[@class="header ng-star-inserted"]'} }
+    page_object(:global_header) { { xpath: '//a[@class="header ng-star-inserted"]' } }
 
     #stamps endicia logo
-    page_objects(:sdc_endicia_logo_mobile, index: 0)  { {xpath: '//img[@alt="Stamps.com Endicia | Partner Portal logo"]'} }
-    page_objects(:sdc_endicia_logo_browser, index: 1)  { {xpath: '//img[@alt="Stamps.com Endicia | Partner Portal logo"]'} }
+    page_objects(:sdc_endicia_logo_mobile, index: 0)  { { xpath: '//img[@alt="Stamps.com Endicia | Partner Portal logo"]' } }
+    page_objects(:sdc_endicia_logo_browser, index: 1)  { { xpath: '//img[@alt="Stamps.com Endicia | Partner Portal logo"]' } }
 
     #Partner Portal logo
-    page_object(:partner_portal_logo)  { {xpath:  '//h3[contains(text(), "Partner Portal")]'} }
+    page_object(:partner_portal_logo)  { { xpath:  '//h3[contains(text(), "Partner Portal")]' } }
 
     #Panel Dashboard
-    page_object(:panel_dashboard) { {xpath: '//a[contains(text(), "Dashboard")]'} }
-    page_object(:panel_logout) { {xpath: '//a[contains(text(), "Logout")]'} }
+    page_object(:panel_dashboard) { { xpath: '//a[contains(text(), "Dashboard")]' } }
+    page_object(:panel_logout) { { xpath: '//a[contains(text(), "Logout")]' } }
 
     #xs viewport
     #hamburger button
-    page_object(:hamburger_button_xs)  { {class:  'hamburger hamburger-slider visible-xs'} }
+    page_object(:hamburger_button_xs)  { { class:  'hamburger hamburger-slider visible-xs' } }
 
     #x button
-    page_object(:x_button)  { {class:  'hamburger hamburger-slider is-active visible-xs'} }
+    page_object(:x_button)  { { class:  'hamburger hamburger-slider is-active visible-xs' } }
 
     #Left Panel
-    page_object(:panel_collapsed_xs)  { {class:  'sidebar'} }
-    page_object(:panel_expanded_xs)  { {class:  'sidebar sidebar_expanded-xs'} }
+    page_object(:panel_collapsed_xs)  { { class:  'sidebar' } }
+    page_object(:panel_expanded_xs)  { { class:  'sidebar sidebar_expanded-xs' } }
 
     #Normal viewport left panel
-    page_object(:panel_hamburger) { {class: 'hamburger hamburger-slider hamburger-arrow hamburger-sidebar'} }
-    page_object(:panel_arrow) { {class: 'hamburger hamburger-slider is-active hamburger-arrow hamburger-sidebar'} }
+    page_object(:panel_hamburger) { { class: 'hamburger hamburger-slider hamburger-arrow hamburger-sidebar' } }
+    page_object(:panel_arrow) { { class: 'hamburger hamburger-slider is-active hamburger-arrow hamburger-sidebar' } }
 
-    page_object(:panel_collapsed_lg)  { {class:  'sidebar sidebar_collapsed'} }
-    page_object(:panel_expanded_lg)  { {class:  'sidebar'} }
+    page_object(:panel_collapsed_lg)  { { class:  'sidebar sidebar_collapsed' } }
+    page_object(:panel_expanded_lg)  { { class:  'sidebar' } }
 
     #footer
-    page_object(:footer)  { {class:  'footer'} }
+    page_object(:footer)  { { class:  'footer' } }
 
 
     def user_table_query(user, column)
