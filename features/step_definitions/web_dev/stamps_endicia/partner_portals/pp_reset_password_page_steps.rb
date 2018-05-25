8e7a30e04ba9621f578348f4ca458da14488cca6
Rob Cruz <robert.cruz@gmail.com>
2018-05-25 07:12:50 -0700

    bug fix

--- features/step_definitions/web_dev/stamps_endicia/partner_portals/pp_reset_password_page_steps.rb
+++ features/step_definitions/web_dev/stamps_endicia/partner_portals/pp_reset_password_page_steps.rb
@@ -1,9 +1,3 @@
-Given /^[Pp]P: [Aa] user navigates to the Reset Password Page$/ do
-  step 'Start test driver'
-  step 'PP: A user navigates to the login page'
-  step 'PP: User clicks Forgot Password link'
-  step 'PP: expect user is redirected to Reset Password Page'
-end
 
 Then /^[Pp]P: [Aa] user navigates to Reset Password Page$/ do
  PPResetPasswordPage.visit
