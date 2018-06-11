529087654fcf80df645739a853f42fa6a729b416
mhossain <mhossain@stamps.com>
2018-06-11 10:03:26 -0700

    Export Data

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -11,11 +11,13 @@ include PartnerPortal
 
 Before do |scenario|
   Dir.mkdir("#{Dir.getwd}/download") unless Dir.exist?("#{Dir.getwd}/download")
+  system 'C:\Stamps\binaries\edge_rdp_unlock.bat"' if SdcEnv.browser = 'edge'
   SdcEnv.scenario = scenario
   SdcTest.start
 end
 
 After do
+  system 'C:\Stamps\binaries\edge_rdp_lock.bat' if SdcEnv.browser = 'edge'
   SdcTest.teardown
   # Old framework
   @stamps = nil
