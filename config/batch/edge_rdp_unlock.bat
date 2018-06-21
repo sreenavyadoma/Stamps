cca0fc3e34c6686bf2aee7a5a33f28556d6612fd
mhossain <mhossain@stamps.com>
2018-06-21 10:49:36 -0700

    Export Data UI validation

--- config/batch/edge_rdp_unlock.bat
+++ config/batch/edge_rdp_unlock.bat
@@ -1,7 +1,3 @@
-::for /f "skip=1 tokens=3" %%s in ('query user %USERNAME%') do (
-::  %windir%\System32\tscon.exe %%s /dest:console
-::)
-
-for /f 'skip=1 tokens=3' %%s in ('c:\\windows\\sysnative\\query.exe user') do (
- %windir%\sysnative\tscon.exe %%s /dest:console
+for /f "skip=1 tokens=3" %%s in ('query user %USERNAME%') do (
+ %windir%\System32\tscon.exe %%s /dest:console
 )
