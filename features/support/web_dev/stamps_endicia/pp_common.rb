021c8937c1e61dbb40ae80ce31e900f3f90eb9ee
mhossain <mhossain@stamps.com>
2018-06-25 12:04:53 -0700

    Export Data UI validation

--- features/support/web_dev/stamps_endicia/pp_common.rb
+++ features/support/web_dev/stamps_endicia/pp_common.rb
@@ -140,5 +140,19 @@ module PartnerPortal
       end
     end
 
+    def tansaction_data(user,start_date,end_date )
+      "select pp_trans_data.AccountNumber, pp_trans_data.TransactionDateTime,  pp_trans_data.TransactionType,
+        pp_trans_data.RetailRate, pp_trans_data.PayoutAmount, pp_trans_data.TrackingNumber, pp_trans_data.Weight,
+        pp_trans_data.Zone, pp_trans_data.PackageLength, pp_trans_data.PackageWidth,  pp_trans_data.PackageHeight,
+        pp_trans_data.PackageType, pp_trans_data.MailClass, pp_trans_data.InternationalCountryGroup, pp_trans_data.OriginationAddressZip,
+        pp_trans_data.DestinationAddressZip, pp_trans_data.DestinationCountry, pp_trans_data.ExtraServiceFee, pp_trans_data.TransactionID,
+        pp_trans_data.ContainerType, pp_trans_data.IsCubic, pp_trans_data.CubicValue, pp_trans_data.IsApfAfo, pp_trans_data.CreditCardFee
+        from [dbo].[sdct_PartnerPortal_TransactionData]  as pp_trans_data
+        inner join [dbo].[sdct_PartnerPortal_Contract] as pp_contract on pp_trans_data.ContractId = pp_contract.ContractId
+        inner join [dbo].[sdct_PartnerPortal_User] as pp_user on pp_contract.PartnerAccountId = pp_user.PartnerAccountId
+        where pp_user.EmailAddress = '#{user}' and pp_trans_data.TransactionDateTime >= '#{2017-07-10}' and  pp_trans_data.TransactionDateTime < DATEADD(DAY, 1 , CONVERT(DATE, '#{2017-07-10}'))
+        ORDER BY  pp_trans_data.TransactionDateTime ASC"
+    end
+
   end
 end
