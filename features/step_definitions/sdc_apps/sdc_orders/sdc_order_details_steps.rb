88ab41b45ea766b21a4b295f3f8d7a56f86d1f2a
Rob Cruz <robert.cruz@gmail.com>
2018-07-06 13:53:04 -0700

    ORDERSAUTO-4610 - Changes to Orders Grid

--- features/step_definitions/sdc_apps/sdc_orders/sdc_order_details_steps.rb
+++ features/step_definitions/sdc_apps/sdc_orders/sdc_order_details_steps.rb
@@ -222,9 +222,7 @@ Then /^blur out on order details form$/ do
   order_details.weight_label.double_click
   order_details.service_label.double_click
   order_details.reference_no.double_click
-  if order_details.ship_to_label.present?
-    order_details.ship_to_label.double_click
-  end
+  order_details.ship_to_label.safe_double_click
   order_details.order_id.double_click
   order_details.title.double_click
 end
