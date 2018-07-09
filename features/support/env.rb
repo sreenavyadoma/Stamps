5d5b80d580a5dc7f5d343a60fe2a03379bcc4c58
Rob Cruz <robert.cruz@gmail.com>
2018-07-09 10:38:15 -0700

    ORDERSAUTO-4610 - Changes to Orders Grid

--- features/support/env.rb
+++ features/support/env.rb
@@ -30,21 +30,4 @@ require_relative 'sdc_apps/sdc_navigation'
 require_relative 'web_dev/stamps_endicia/partner_portal'
 
 
-# Old Framework
-#require_relative 'lib/core/stamps_element'
-#require_relative 'lib/apps/registration_app'
-#require_relative 'lib/apps/pam_app'
-#require_relative 'lib/pam/pam_page_header'
-#require_relative 'lib/apps/stores_app'
-require_relative 'helpers/test_helpers'
-#require_relative 'lib/core/old_web_apps_core'
-#require_relative 'lib/apps/web_apps'
-#require_relative 'lib/common/stamps_dot_com'
-#require_relative 'lib/orders/orders_modals'
-#require_relative 'lib/orders/orders'
-#require_relative 'lib/mail/mail_sign_in'
-#require_relative 'lib/orders/store_management'
-#require_relative 'lib/mail/print_form_panel_elements'
-#require_relative 'lib/mail/print_form_panel'
-
-World Stamps, SdcWebsite, SdcOrders, SdcMail, SdcNavigation, PartnerPortal
+World SdcWebsite, SdcOrders, SdcMail, SdcNavigation, PartnerPortal
