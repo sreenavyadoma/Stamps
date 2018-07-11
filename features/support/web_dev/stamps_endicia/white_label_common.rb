85a0045d8b0ce9a6479ebd66968150b0e1287811
mhossain <mhossain@stamps.com>
2018-07-11 15:00:32 -0700

    Stamps WebReg Normal Workflow

--- features/support/web_dev/stamps_endicia/white_label_common.rb
+++ features/support/web_dev/stamps_endicia/white_label_common.rb
@@ -8,13 +8,11 @@ module WhiteLabel
     #security questions
     page_object(:sq_header) {{xpath: '//h1[(contains(text(), "Before you start printing postage, make sure your account is protected."))]'}}
     page_object(:security_question) {{xpath: '//h2[(contains(text(), "To protect your account, please answer these security questions:"))]'}}
-    page_object(:first_security_question) {{xpath: '//button[contains(@class, "dropdown-toggle")][@title="1ST QUESTION"]'}}
+    page_objects(:first_security_question, index: 0) {{xpath: '//div[contains(@class, "secretQuestion")]/button]'}}
     text_field(:first_secret_answer, tag: :text_field, required: true) { { id: 'secretAnswer1' } }
     page_object(:second_security_question) {{xpath: '//button[contains(@class, "dropdown-toggle")][@title="2ND QUESTION"]'}}
     text_field(:second_secret_answer, tag: :text_field, required: true) { { id: 'secretAnswer2' } }
     button(:sq_get_started) { { id: ['startPrinting'] } }
-
-
 
     def dropdown_selection(str, index, name = :dropdown_element)
       page_objects(name, index: index) { { xpath: "//span[contains(text(), \"#{str}\")]" } }
