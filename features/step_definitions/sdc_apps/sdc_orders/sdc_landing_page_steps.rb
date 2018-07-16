3aaa54b03114bd1ed0a4de366e85498b589e8c44
robcruz <rcruz@stamps.com>
2018-07-16 13:05:54 -0700

    ORDERSAUTO-3830 Create Mobile test framework

--- features/step_definitions/sdc_apps/sdc_orders/sdc_landing_page_steps.rb
+++ features/step_definitions/sdc_apps/sdc_orders/sdc_landing_page_steps.rb
@@ -134,8 +134,8 @@ end
 Then /^click Orders landing page sign-in button$/ do
   landing_page = SdcWebsite.landing_page
   landing_page.sign_in.wait_until_present(timeout: 3)
+  landing_page.sign_in.click
   3.times do
-    landing_page.sign_in.click
     if landing_page.invalid_username_password.present?
       landing_page.sign_in.click
     else
