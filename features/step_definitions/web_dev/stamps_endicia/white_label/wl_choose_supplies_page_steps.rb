49cce5e5ba96a4d6af02f8f2c6ffd9d313287de1
mhossain <mhossain@stamps.com>
2018-07-17 08:56:35 -0700

    Stamps WebReg Normal Workflow

--- features/step_definitions/web_dev/stamps_endicia/white_label/wl_choose_supplies_page_steps.rb
+++ features/step_definitions/web_dev/stamps_endicia/white_label/wl_choose_supplies_page_steps.rb
@@ -1,6 +1,6 @@
 Then /^WL: check choose supplies page is present then verify the page and click place order button$/ do
   if TestData.hash[:atg_promotion] == ''
-    #ignore
+    expect(WhiteLabel.choose_supplies.cs_header).not_to be_present, 'Choose Supplies Page is PRESENT when there is no ATG Promotion'
   else
     step 'WL: expect choose supplies page customize your welcome kit is present'
     step 'WL: click choose supplies page place order button'
@@ -10,7 +10,7 @@ end
 Then /^WL: expect choose supplies page customize your welcome kit is present$/ do
   cs_header =  WhiteLabel.choose_supplies.cs_header
   cs_header.wait_until_present(timeout: 50)
-  expect(cs_header).to be_present, 'Customize your Welcome Kit is NOT present'
+  expect(cs_header).to be_present, "Customize your Welcome Kit is NOT present, ATG Promotion: #{TestData.hash[:atg_promotion]}"
 end
 
 Then /^WL: click choose supplies page place order button$/ do
