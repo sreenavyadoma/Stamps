33d7cd167c0c54059c4b351c69a5249d4e6fdcfa
ksathavara <ksathavara@stamps.com>
2018-07-20 13:34:27 -0700

    Profile page UI validation

--- features/features/web_dev/stamps_endicia/stamps_webreg/stamps_webreg_profile_page.feature
+++ features/features/web_dev/stamps_endicia/stamps_webreg/stamps_webreg_profile_page.feature
@@ -9,63 +9,65 @@ Feature: Stamps WebReg: Profile Page
     Then WL: navigates to default registration page for stamps with the following source id 100-TES-WB001
 
     #Verifying all content present
-    #Then WL: expect Registration navigation bar Stamps logo exists
-    #Then WL: expect Registration navigation bar USPS logo exists
-    #Then WL: expect Profile bread crumbs is Profile
-    #Then WL: expect Profile page header contain Sign up and avoid trips to the Post Office
-    #Then WL: expect Profile page paragraph contain The USPS requires you to register your name, telephone number and physical address from which your postage will be printed. This information will be used to create your Stamps.com account.
-    #Then WL: expect Profile page SideContent Side Account header is Why do I need to create an account?
-    #Then WL: expect Profile page Money-saving offers and new products header is "Money-saving offers and new products"
-    #Then WL: expect Profile page content under Money-saving offers and new products Send me special money-saving offers and information on new products and services from Stamps.com and its partners.
+    Then pause for 5 seconds
+    Then WL: expect Registration navigation bar Stamps logo exists
+    Then WL: expect Registration navigation bar USPS logo exists
+    Then WL: expect Profile bread crumbs is Profile
+    Then WL: expect Profile page header contain Sign up and avoid trips to the Post Office
+    Then WL: expect Profile page paragraph contain The USPS requires you to register your name, telephone number and physical address from which your postage will be printed. This information will be used to create your Stamps.com account.
+    Then WL: expect Profile page SideContent Side Account header is Why do I need to create an account?
+    Then WL: expect Profile page Money-saving offers and new products header is "Money-saving offers and new products"
+    Then WL: expect Profile page content under Money-saving offers and new products Send me special money-saving offers and information on new products and services from Stamps.com and its partners.
       ###Then WL: expect Profile page Money-saving offers and new products is checked
       ###Then WL: check Profile page Money-saving offers and new products
       ###Then WL: uncheck Profile page Money-saving offers and new products
       ###Then WL: expect Profile page Money-saving offers and new products is unchecked
 
     # Validate Single Help block tooltips
-      #Then continue to Membership page
+    Then WL: click profile page continue button
     Then WL: expect Profile page Email tooltip index 1 to be This field is required
     Then WL: expect Profile page username tooltip index 1 to be This field is required
     Then WL: expect Profile page password tooltip index 1 to be This field is required
     Then WL: expect Profile page Re-Password tooltip index 1 to be This field is required
 
   #Verifying all fields present and set Negative values for tooltip Validation
-    Then expect Profile page email exists
-    Then set Profile page Email to kk
-    Then expect Profile page username exists
-    Then set Profile page Username to r
-    Then expect Profile page password exists
-    Then set Profile page Password to !
-    Then expect Profile page retype password exists
-    Then set Profile page Re-type password to k
-    Then expect Profile page Survey Question exists
-    Then expect Profile page promo code link exists
-    Then show profile page promo code textbox
-    Then expect Profile page promo code textbox exists
-    Then set Profile page promo code to PR
-    Then expect Profile page CONTINUE button exists
-    Then continue to Membership page
+    Then WL: expect Profile page email exists
+    Then WL: set profile page email to k
+    Then WL: expect Profile page username exists
+    Then WL: set profile page username to r
+    Then WL: expect Profile page password exists
+    Then WL: set profile page password to !
+    Then WL: expect Profile page retype password exists
+    Then WL: set profile page re-type password to k
+    Then WL: expect Profile page Survey Question exists
+    Then WL: expect Profile page promo code link exists
+    Then WL: show profile page promo code
+    Then WL: expect Profile page promo code exists
+    Then WL: expect profile page promo code to equal source id promo code
+    Then WL: set profile page promo code to p
+    Then WL: expect Profile page CONTINUE button exists
+    Then WL: click profile page continue button
 
    # Validate tooltips for various values
    #Validate Email tooltips
-    Then expect Profile page Email tooltip count is 2
-    Then expect Profile page Email tooltip index 1 to be 5 character minimum
-    Then expect Profile page Email tooltip index 2 to be Valid email address required
+    Then WL: expect Profile page Email tooltip count is 2
+    Then WL: expect Profile page Email tooltip index 1 to be 5 character minimum
+    Then WL: expect Profile page Email tooltip index 2 to be Valid email address required
 
     #Validate Username tooltips
-    Then expect Profile page Username tooltip count is 1
-    Then expect Profile page username tooltip index 1 to be 2 character minimum
+    Then WL: expect Profile page Username tooltip count is 1
+    Then WL: expect Profile page username tooltip index 1 to be 2 character minimum
     # Validate Password tooltips
-    Then expect Profile page Password tooltip count is 3
-    Then expect Profile page Password tooltip index 1 to be 6 character minimum
-    Then expect Profile page Password tooltip index 2 to be At least 1 number required
-    Then expect Profile page Password tooltip index 3 to be At least 1 letter required
+    Then WL: expect Profile page Password tooltip count is 3
+    Then WL: expect Profile page Password tooltip index 1 to be 6 character minimum
+    Then WL: expect Profile page Password tooltip index 2 to be At least 1 number required
+    Then WL: expect Profile page Password tooltip index 3 to be At least 1 letter required
 
     # Validate Re-type password tooltips
-    Then expect Profile page Re-Password tooltip index 1 to be Passwords don't match
+    Then WL: expect Profile page Re-Password tooltip index 1 to be Passwords don't match
 
    #Validate promo code tooltips
-    Then expect Profile page promo code tooltip 1 to be Invalid promo code
+    Then WL: expect Profile page promo code tooltip 1 to be Invalid promo code
 
     # Validating all fields with correct values
     Then set Profile page Email to random value
--- features/step_definitions/web_dev/stamps_endicia/white_label/wl_profile_page_steps.rb
+++ features/step_definitions/web_dev/stamps_endicia/white_label/wl_profile_page_steps.rb
@@ -1,4 +1,8 @@
 
+Then /^WL: [Ee]xpect [Pp]rofile [Pp]age email exists$/ do
+  expect(WhiteLabel.profile_page.email).to be_present, "Email textbox DOES NOT exists on profile page"
+end
+
 Then /^WL: set profile page email to (?:random value|(.*))$/ do |str|
   email = WhiteLabel.profile_page.email
   email.wait_until_present(timeout: 30)
@@ -14,9 +18,17 @@ Then /^WL: set profile page email to (?:random value|(.*))$/ do |str|
   print "Email = #{TestData.hash[:email]}\n"
 end
 
+Then /^WL: [Ee]xpect [Pp]rofile [Pp]age username exists$/ do
+  expect(WhiteLabel.profile_page.username).to be_present, "Username textbox DOES NOT exist on profile page"
+end
+
 Then /^WL: set profile page username to (?:random value|(.*))$/ do |str|
   WhiteLabel.profile_page.username.set ((TestData.hash[:username]=(str.nil?)?(TestHelper.rand_usr) : str))
   print "UserName = #{TestData.hash[:username]}\n"
+end
+
+Then /^WL: [Ee]xpect [Pp]rofile [Pp]age password exists$/ do
+  expect(WhiteLabel.profile_page.password).to be_present, "Password textbox DOES NOT exist on profile page"
 end
 
 Then /^WL: set profile page password to (?:random value|(.*))$/ do |str|
@@ -28,8 +40,16 @@ Then /^WL: set profile page password to (?:random value|(.*))$/ do |str|
   print "Password = #{TestData.hash[:account_password]}\n"
 end
 
+Then /^WL: [Ee]xpect [Pp]rofile [Pp]age retype password exists$/ do
+  expect(WhiteLabel.profile_page.confirm_password).to be_present, "Retype password textbox DOES NOT exist on profile page"
+end
+
 Then /^WL: set profile page re-type password to (?:same as previous password|(.*))$/ do |str|
   WhiteLabel.profile_page.confirm_password.set(TestData.hash[:retype_password]=(str.nil?)?(TestData.hash[:account_password]) : str)
+end
+
+Then /^WL: [Ee]xpect [Pp]rofile [Pp]age [Ss]urvey [Qq]uestion exists$/ do
+  expect(WhiteLabel.profile_page.survey).to be_present, "Survey Question DOES NOT exist on profile page"
 end
 
 Then /^WL: set profile page survey question to (.*)$/ do |str|
@@ -50,21 +70,33 @@ Then /^WL: set profile page how did you hear about us\? to (.*)$/ do |str|
   expect(profile_page.referrer_name.attribute_value('title').strip).to eql str
 end
 
+Then /^WL: [Ee]xpect [Pp]rofile [Pp]age promo code link exists$/ do
+  expect(WhiteLabel.profile_page.promo_code_link).to be_present, "Promo code link DOES NOT exist on profile page"
+end
+
+Then /^WL: [Ee]xpect [Pp]rofile [Pp]age promo code exists$/ do
+  expect(WhiteLabel.profile_page.promo_code_textbox).to be_present, "Profile promo code textbox DOES NOT exist"
+end
+
 Then /^WL: expect profile page promo code to equal source id promo code$/ do
-  step 'WL: show profile page promo code textbox'
+  step 'WL: show profile page promo code'
   profile_page = WhiteLabel.profile_page
   expect(profile_page.promo_code_textbox.text_value.strip).to eql(TestData.hash[:promo_code])
 end
 
 Then /^WL: set profile page promo code to (?:an empty string|(.*))$/ do |str|
-  step 'WL: show profile page promo code textbox'
+  step 'WL: show profile page promo code'
   profile_page  = WhiteLabel.profile_page
   profile_page.promo_code_textbox.set(TestData.hash[:promo_code]=(str.nil?)? '' : str)
 end
 
-Then /^WL: show profile page promo code textbox$/ do
+Then /^WL: show profile page promo code$/ do
   WhiteLabel.profile_page.promo_code_link.click if  WhiteLabel.profile_page.promo_code_link.present?
   expect(WhiteLabel.profile_page.promo_code_textbox).to be_present, 'Unable to show Promo Code textbox upon clicking Show Promo Code link.'
+end
+
+Then /^WL: [Ee]xpect [Pp]rofile [Pp]age CONTINUE button exists$/ do
+  expect(WhiteLabel.profile_page.continue).to be_present, "CONTINUE button doesn't exists on Profile Page"
 end
 
 Then /^WL: click profile page continue button$/ do
@@ -72,10 +104,11 @@ Then /^WL: click profile page continue button$/ do
 end
 
 
-
 ##---------UI Validation------##
 
 Then /^WL: [Ee]xpect [Rr]egistration navigation bar Stamps logo exists$/ do
+  stamps_logo = WhiteLabel.profile_page.stamps_logo
+  stamps_logo.wait_until_present(timeout: 60)
   expect(WhiteLabel.profile_page.stamps_logo).to be_present, "Stamps logo doesn't exists on navigation bar"
 end
 
@@ -123,18 +156,51 @@ Then /^WL: [Ee]xpect [Pp]rofile [Pp]age [Mm]oney-saving offers and new products 
   expect(WhiteLabel.profile_page.money_saving_offers_checkbox.checked?).not_to be(true), "Expected Profile Money-saving offers and new products is checked. Got unchecked"
 end
 
+Then /^WL: [Ee]xpect Profile page Email tooltip count is (.*)$/ do |count|
+  email_tooltip_count = WhiteLabel.profile_page.email_tooltip
+  email_tooltip_count.wait_until_present(timeout: 5)
+  TestData.hash[:email_tooltip_count] = email_tooltip_count.text_value.split("\n")
+  expect(TestData.hash[:email_tooltip_count].size).to eql(count.to_i)
+end
+
 Then /^WL: [Ee]xpect Profile page Email tooltip index (\d+) to be (.*)$/ do |index, str|
-  expect(WhiteLabel.profile_page.email_tooltip1.text(index.to_i - 1)).to eql(str)
+  email_tooltip = WhiteLabel.profile_page.email_tooltip
+  email_tooltip.wait_until_present(timeout: 5)
+  TestData.hash[:email_tooltip] = email_tooltip.text_value.split("\n")
+  expect(TestData.hash[:email_tooltip][index.to_i - 1]).to eql(str)
+end
+
+Then /^WL: [Ee]xpect Profile page [Uu]sername tooltip count is (.*)$/ do |count|
+  username_tooltip_count = WhiteLabel.profile_page.username_tooltip
+  username_tooltip_count.wait_until_present(timeout: 5)
+  TestData.hash[:username_tooltip_count] = username_tooltip_count.text_value.split("\n")
+  expect(TestData.hash[:username_tooltip_count].size).to eql(count.to_i)
 end
 
 Then /^WL: [Ee]xpect Profile page [Uu]sername tooltip index (\d+) to be (.*)$/ do |index, str|
-  expect(WhiteLabel.profile_page.username_tooltip1.text(index.to_i - 1)).to eql(str)
+  username_tooltip = WhiteLabel.profile_page.username_tooltip
+  username_tooltip.wait_until_present(timeout: 5)
+  TestData.hash[:username_tooltip] = username_tooltip.text_value.split("\n")
+  expect(TestData.hash[:username_tooltip][index.to_i - 1]).to eql(str)
+end
+
+Then /^WL: [Ee]xpect Profile page Password tooltip count is (.*)$/ do |count|
+  password_tooltip_count = WhiteLabel.profile_page.password_tooltip
+  password_tooltip_count.wait_until_present(timeout: 5)
+  TestData.hash[:password_tooltip_count] = password_tooltip_count.text_value.split("\n")
+  expect(TestData.hash[:password_tooltip_count].size).to eql(count.to_i)
 end
 
 Then /^WL: [Ee]xpect Profile page [Pp]assword tooltip index (\d+) to be (.*)$/ do |index, str|
-  expect(WhiteLabel.profile_page.password_tooltip1.text(index.to_i - 1)).to eql(str)
+  password_tooltip = WhiteLabel.profile_page.password_tooltip
+  password_tooltip.wait_until_present(timeout: 5)
+  TestData.hash[:password_tooltip] = password_tooltip.text_value.split("\n")
+  expect(TestData.hash[:password_tooltip][index.to_i - 1]).to eql(str)
 end
 
 Then /^WL: [Ee]xpect Profile page Re-Password tooltip index (\d+) to be (.*)$/ do |index, str|
-  expect(WhiteLabel.profile_page.confirm_password_tooltip1.text(index.to_i - 1)).to eql(str)
+  confirm_password_tooltip = WhiteLabel.profile_page.confirm_password_tooltip
+  confirm_password_tooltip.wait_until_present(timeout: 5)
+  TestData.hash[:confirm_password_tooltip] = confirm_password_tooltip.text_value.split("\n")
+  expect(TestData.hash[:confirm_password_tooltip][index.to_i - 1]).to eql(str)
 end
