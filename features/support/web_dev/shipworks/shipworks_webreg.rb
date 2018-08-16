dcb0479b342d00306d0940ae0d7401b4517963e4
mhossain <mhossain@stamps.com>
2018-08-16 15:04:34 -0700

    Shipworks Webreg Create Your Account UI Validation

--- features/support/web_dev/shipworks/shipworks_webreg.rb
+++ features/support/web_dev/shipworks/shipworks_webreg.rb
@@ -1,7 +1,27 @@
 module SWWR
 
   class CreateYourAccount < SdcPage
-    page_object(:create_your_account_header) { {xpath: '//div[@id="page"]/div/div/h1'} }
+    page_object(:create_your_account_header) { {xpath: '//div[@class="webreg-form-case-1"]/h1'} }
+    page_Object(:create_your_account_header_p) { {xpath: '//div[@class="webreg-form-case-1"]/p'} }
+    text_field(:email, tag: :text_field, required: true) { { id: 'email' } }
+    page_object(:email_help_block,  index: 0) { {xpath: '//li[@class="module webreg_email"]/div/div/div/span/span[2]'} }
+    text_field(:password, tag: :text_field, required: true) { { id: 'passwordEle' }}
+    page_object(:password_help_block,  index: 0) { {xpath: '//li[@class="module webreg_password"]/div/div/div/span/span[2]'} }
+    text_field(:first_name, tag: :text_field, required: true) { { id: 'firstName' } }
+    page_object(:first_name_help_block) { {xpath: '//div[@class="col-xs-12 col-sm-6 gut-sm-form-r-half"]/div/div/span'} }
+    text_field(:last_name, tag: :text_field, required: true) { { id: 'lastName' } }
+    page_object(:last_name_help_block) { {xpath: '//li[@class="module webreg_personalinfo"]/div/div[@class="col-xs-12 col-md-6 gut-md-l-half"]/div/span/span[2]'} }
+    text_field(:company, tag: :text_field) { { id: 'companyName' } }
+    text_field(:phone, tag: :text_field, required: true) { { id: 'phone' } }
+    page_object(:phone_help_block) { {xpath: '//li[@class="module webreg_personalinfo"]/div/div[@class="col-xs-12"]/div/span/span[2]'} }
+    page_object(:promo_code_label) {{xpath: 'radio-group-label hidden-xs pull-left'}}
+    page_object(:promo_code_yes, index: 0) {{xpath: '//label[@class="md-radio"]/input'}}
+    page_object(:promo_code_checked) {{class: ['form-group show'] }}
+    page_object(:promo_code_no, index: 1) {{xpath: '//label[@class="md-radio"]/input'}}
+    page_object(:promo_code_checked) {{class: ['form-group hide'] }}
+
+
+
   end
 
   class MembershipPage < SdcPage
