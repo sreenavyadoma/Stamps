d7aaad2874d498a6735bd399e93c4456b34132e1
mhossain <mhossain@stamps.com>
2018-08-20 09:58:38 -0700

    Stamps Website Regression

--- features/step_definitions/web_dev/stamps_endicia/stamps_website/sdcw_postage_online_page_steps.rb
+++ features/step_definitions/web_dev/stamps_endicia/stamps_website/sdcw_postage_online_page_steps.rb
@@ -1,5 +1,5 @@
 Then /^SDCW: Navigate to postage online page$/ do
-  PostageOnlinePage.visit
+  StampsWebsite::PostageOnlinePage.visit
   stamps_logo = StampsWebsite.common_page.stamps_logo
   stamps_logo.wait_until_present(timeout: 10)
   expect(stamps_logo).to be_present
--- features/support/web_dev/stamps_endicia/stamps_website.rb
+++ features/support/web_dev/stamps_endicia/stamps_website.rb
@@ -18,6 +18,7 @@ module StampsWebsite
                 # ignore
             end)
     end
+
   end
 
 
