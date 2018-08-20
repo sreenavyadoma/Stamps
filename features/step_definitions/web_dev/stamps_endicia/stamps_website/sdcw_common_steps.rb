e609339c54aae1ba3ffefe19044add73391ac053
mhossain <mhossain@stamps.com>
2018-08-20 10:44:51 -0700

    Stamps Website Regression Postage Online Page

--- features/step_definitions/web_dev/stamps_endicia/stamps_website/sdcw_common_steps.rb
+++ features/step_definitions/web_dev/stamps_endicia/stamps_website/sdcw_common_steps.rb
@@ -1,8 +1,17 @@
-Then /^SDCW: navigate to home page$/ do
-  StampsWebsite::SDCWWebsite.visit
-  stamps_logo = StampsWebsite.common_page.stamps_logo
-  stamps_logo.wait_until_present(timeout: 10)
-  expect(stamps_logo).to be_present
+Then /^SDCW: navigate back$/ do
+  SdcPage.browser.back
 end
 
-
+Then /^SDCW: click stamps website logo$/ do
+  StampsWebsite.common_page.stamps_logo.click
+  case TestSession.env.url
+    when :qacc
+      expect(SdcPage.browser.url).to eql('https://sdcwebsite.qacc.stamps.com/')
+    when :stg
+      expect(SdcPage.browser.url).to eql('https://sdcwebsite.staging.stamps.com/')
+    when :prod
+      expect(SdcPage.browser.url).to eql('https://www.stamps.com/')
+    else
+      # ignore
+  end
+end
