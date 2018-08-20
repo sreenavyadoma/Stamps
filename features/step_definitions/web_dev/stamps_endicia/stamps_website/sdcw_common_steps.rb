68e02c44cf0777fa0d13549b8ecce90c10d023cc
ksathavara <ksathavara@stamps.com>
2018-08-20 11:35:29 -0700

    home page ui validation

--- features/step_definitions/web_dev/stamps_endicia/stamps_website/sdcw_common_steps.rb
+++ features/step_definitions/web_dev/stamps_endicia/stamps_website/sdcw_common_steps.rb
@@ -1,6 +1,2 @@
-Then /^SDCW: navigate to home page$/ do
-  StampsWebsite::SDCWWebsite.visit
-  stamps_logo = StampsWebsite.common_page.stamps_logo
-  stamps_logo.wait_until_present(timeout: 10)
-  expect(stamps_logo).to be_present
-end
+
+
--- features/step_definitions/web_dev/stamps_endicia/stamps_website/sdcw_postage_online_page_steps.rb
+++ features/step_definitions/web_dev/stamps_endicia/stamps_website/sdcw_postage_online_page_steps.rb
@@ -1,5 +1,5 @@
 Then /^SDCW: navigate to postage online page$/ do
-  StampsWebsite::PostageOnlinePage.visit
+  StampsWebsite::HomePage.visit
   stamps_logo = StampsWebsite.common_page.stamps_logo
   stamps_logo.wait_until_present(timeout: 10)
   expect(stamps_logo).to be_present
