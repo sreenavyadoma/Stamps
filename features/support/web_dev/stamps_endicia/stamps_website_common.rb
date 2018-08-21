49d91a7bb3453cc9b57a45631cbc9c05217fbf38
mhossain <mhossain@stamps.com>
2018-08-21 14:35:01 -0700

    Stamps Website Regression Postage Online Page

--- features/support/web_dev/stamps_endicia/stamps_website_common.rb
+++ features/support/web_dev/stamps_endicia/stamps_website_common.rb
@@ -11,6 +11,15 @@ module StampsWebsite
     page_objects(:corporate_postage_solutions, index: 0) { {xpath: '//a[contains(text(),"Corporate Postage Solutions")]' } }
     page_object(:faq) { {xpath: '//a[contains(text(),"FAQ")]'} }
 
+    #mobile
+    button(:get_started_xs) {{class: ['btn btn-success btn-xs register visible-xs-block']}}
+    button(:menu_xs) {{id: 'menu-top'}}
+    link(:menu_get_started_xs) {{class: ['list-group-item list-group-item-success register']}}
+    link(:menu_learn_more_xs) {{xpath: '//a[@href="#learn-more-menu"]'}}
+    page_objects(:small_office_mailers_xs, index: 1) { {xpath: '//a[contains(text(),"Small Office Mailers")]' } }
+    page_objects(:online_sellers_xs, index: 1) { {xpath: '//a[contains(text(),"Online Sellers")]' } }
+    page_objects(:warehouse_shippers_xs, index: 1) { {xpath: '//a[contains(text(),"Warehouse Shippers")]' } }
+    page_objects(:corporate_postage_solutions, index: 1) { {xpath: '//a[contains(text(),"Corporate Postage Solutions")]' } }
 
     def dropdown(str, name = :element)
       page_object(name) { {xpath: "//span[contains(text(), \" #{str} \")]" } }
