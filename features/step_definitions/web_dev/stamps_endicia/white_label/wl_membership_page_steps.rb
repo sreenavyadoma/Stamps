4c7b1ef16a780ac6c96c93a8ece5781de20d5e97
mhossain <mhossain@stamps.com>
2018-09-07 09:48:24 -0700

    Stamps Website Regression Postage Online Page

--- features/step_definitions/web_dev/stamps_endicia/white_label/wl_membership_page_steps.rb
+++ features/step_definitions/web_dev/stamps_endicia/white_label/wl_membership_page_steps.rb
@@ -127,7 +127,7 @@ end
 Then /WL: select membership page address autocomplete index (\d+)$/ do |index|
   address_auto_complete = WhiteLabel.membership_page.address_auto_complete[index-1]
   address_auto_complete.wait_until_present(timeout: 2)
-  address_auto_complete.hover if TestSession.env.local_browser == :ff || :firefox
+  address_auto_complete.hover if TestSession.env.local_browser == :ff || TestSession.env.local_browser == :firefox
   address_auto_complete.click
   step 'WL: blur_out on membership page'
 end
