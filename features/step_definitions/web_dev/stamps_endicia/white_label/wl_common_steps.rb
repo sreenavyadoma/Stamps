ef00a6242a129292bebc9c9ce7efcafcbc8baefd
mhossain <mhossain@stamps.com>
2018-09-18 09:25:29 -0700

    Stamps Website Regression Postage Online Page

--- features/step_definitions/web_dev/stamps_endicia/white_label/wl_common_steps.rb
+++ features/step_definitions/web_dev/stamps_endicia/white_label/wl_common_steps.rb
@@ -78,7 +78,7 @@ Then /^WL: navigates to default registration page for stamps with the following 
   end
   step 'WL: close stamps website db connection'
 
-  SDCWWebsite.visit(source_id)
+  WhiteLabel::SDCWWebsite.visit(source_id)
   common_page.stamps_logo.wait_until_present(timeout: 10)
   expect(SdcPage.browser.url).to include(target_url.to_s)
 
@@ -105,7 +105,7 @@ Then /^WL: navigates to default registration page for endicia with the following
   end
   step 'WL: close stamps website db connection'
 
-  EWWebsite.visit
+  WhiteLabel::EWWebsite.visit
 
   SdcLogger.info "VendorId = #{vendor_id}"
   SdcLogger.info "OfferId = #{offer_id}"
