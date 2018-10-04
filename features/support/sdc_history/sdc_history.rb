d04e79387cc68a090bb985657c8fa0f2ffd302ac
Rob Cruz <robert.cruz@gmail.com>
2018-10-04 11:38:56 -0700

    ORDERSAUTO-5336 Migrate and create new tests for mail_bvt_stamps

--- features/support/sdc_history/sdc_history.rb
+++ features/support/sdc_history/sdc_history.rb
@@ -4,5 +4,9 @@ module SdcHistory
     def toolbar
       SdcHistoryToolbar.new
     end
+
+    def filter_panel
+      SdcHistoryFilterPanel.new
+    end
   end
 end
---
+++ features/support/sdc_history/sdc_history_filter_panel.rb
@@ -0,0 +1,7 @@
+module SdcHistory
+  class SdcHistoryFilterPanel < SdcPage
+
+  end
+
+  #all your toolbar modals goes here
+end
