f40a021025195bd2eccc1db1fce09df516a88708
Rob Cruz <robert.cruz@gmail.com>
2018-10-08 12:23:34 -0700

    ORDERSAUTO-5336 Migrate and create new tests for mail_bvt_stamps

--- features/features/history/history_filter_panel.feature
+++ features/features/history/history_filter_panel.feature
@@ -8,7 +8,12 @@ Feature:  History Filter Panel
     Then sign-in to orders
     Then navigate to History
     Then expand date printed on history filter panel
-    Then select date printer today on history filter panel
-    Then expect date printed today is selected on history filter panel
-    Then expect history filter panel date printed count for today is greater than 0
+
+    Then select today on history filter panel
+    Then expect today is selected on history filter panel
+    Then expect today count on history filter panel is greater than 0
+
+    Then select past 30 days on history filter panel
+    Then expect past 30 days is selected on history filter panel
+    Then expect past 30 days count on history filter panel is greater than 0
 
--- features/step_definitions/history/history_filter_panel_steps.rb
+++ features/step_definitions/history/history_filter_panel_steps.rb
@@ -5,18 +5,46 @@ Then /^expand date printed on history filter panel$/ do
   expect(date_printed.today.text).to eql 'Today'
 end
 
-Then /^select date printer today on history filter panel$/ do
+Then /^select today on history filter panel$/ do
   date_printed = SdcHistory.filter_panel.date_printed
-  date_printed.today.click
-  step 'expect date printed today is selected on history filter panel'
+  date_printed.today.select
+  step 'wait while loading history grid'
+  begin
+    SdcPage.browser.wait_until(timeout: 5) { date_printed.today.selected? }
+  rescue
+    # ignore
+  end
+  expect(date_printed.today.selected?).to be true
 end
 
-Then /^expect date printed today is selected on history filter panel$/ do
+Then /^expect today is selected on history filter panel$/ do
   date_printed = SdcHistory.filter_panel.date_printed
-  expect(date_printed.today.attribute_value('class')).to include 'selected'
+  expect(date_printed.today.selected?).to be true
 end
 
-Then /^expect history filter panel date printed count for today is greater than (\d+)$/ do |num|
+Then /^expect today count on history filter panel is greater than (\d+)$/ do |num|
   date_printed = SdcHistory.filter_panel.date_printed
   expect(date_printed.today_count.text.to_i).to be > num.to_i
+end
+
+Then /^select past 30 days on history filter panel$/ do
+  date_printed = SdcHistory.filter_panel.date_printed
+  date_printed.past_30_days.select
+  step 'wait while loading history grid'
+  begin
+    SdcPage.browser.wait_until(timeout: 5) { date_printed.past_30_days.selected? }
+  rescue
+    # ignore
+  end
+  expect(date_printed.past_30_days.selected?).to be true
+end
+
+Then /^expect past 30 days is selected on history filter panel$/ do
+  date_printed = SdcHistory.filter_panel.date_printed
+  expect(date_printed.past_30_days.selected?).to be true
+end
+
+Then /^expect past 30 days count on history filter panel is greater than (\d+)$/ do |num|
+  date_printed = SdcHistory.filter_panel.date_printed
+  expect(date_printed.past_30_days_count.text.to_i).to be > num.to_i
 end
