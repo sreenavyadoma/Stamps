e8838d9f903bf39cf45baf5bb515a057cd44f79d
Rob Cruz <robert.cruz@gmail.com>
2018-10-19 13:12:15 -0700

    ORDERSAUTO-5336 Migrate and create new tests for mail_bvt_stamps

--- features/step_definitions/history/history_grid_steps.rb
+++ features/step_definitions/history/history_grid_steps.rb
@@ -10,8 +10,8 @@ Then /^uncheck row (\d+) on history grid$/ do |row|
   grid = SdcHistory.grid
   checkbox = grid.grid_column(:checkbox)
   row = checkbox.checkbox_row(row)
-  row.check
-  expect(row.checked?).to be_truthy
+  row.uncheck
+  expect(row.checked?).to be false
 end
 
 Then /^check row for saved tracking number on history grid$/ do
